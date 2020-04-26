36717e97e194c945e5c4f907294804a7a90b5542
Stella Laurenzo <stellaraccident@gmail.com>
2020-04-26 16:26:45 -0700

    Adapt to use installed MLIR

---
+++ tools/cmake_configure.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# Setup directories.
+td="$(realpath $(dirname $0)/..)"
+build_dir="$td/build"
+install_mlir="$td/install-mlir"
+
+if ! [ -d "$install_mlir/include/mlir" ]; then
+  echo "MLIR install path does not appear valid: $install_mlir"
+  exit 1
+fi
+mkdir -p "$build_dir"
+
+# Make sure we are using python3.
+python_exe="$(which python3)"
+echo "Using python: $python_exe"
+if [ -z "$python_exe" ]; then
+  echo "Could not find python3"
+  exit 1
+fi
+
+set -x
+cmake -GNinja \
+  "-H$td" \
+  "-B$build_dir" \
+  "-DPYTHON_EXECUTABLE=$python_exe" \
+  "-DMLIR_DIR=$install_mlir/lib/cmake/mlir" \
+  "-DLLVM_EXTERNAL_LIT=$build_dir/bin/llvm-lit" \
+  "$@"
