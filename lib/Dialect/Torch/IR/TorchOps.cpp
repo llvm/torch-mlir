3cd60f273f31b957a80c257051b92e3cb357a5fc
Stella Laurenzo <stellaraccident@gmail.com>
2020-09-29 14:17:34 -0700

    Add a torch.kernel_call op and associated predicates.

--- lib/Dialect/Torch/IR/TorchOps.cpp
+++ lib/Dialect/Torch/IR/TorchOps.cpp
@@ -8,8 +8,16 @@
 
 #include "npcomp/Dialect/Torch/IR/TorchOps.h"
 
+#include "mlir/IR/Builders.h"
+#include "npcomp/Dialect/Basicpy/IR/BasicpyDialect.h"
+#include "npcomp/Dialect/Basicpy/IR/BasicpyOps.h"
+#include "npcomp/Dialect/Numpy/IR/NumpyDialect.h"
+#include "npcomp/Dialect/Numpy/IR/NumpyOps.h"
+
 using namespace mlir;
 using namespace mlir::NPCOMP;
+using namespace mlir::NPCOMP::Basicpy;
+using namespace mlir::NPCOMP::Numpy;
 using namespace mlir::NPCOMP::Torch;
 
 #define GET_OP_CLASSES
