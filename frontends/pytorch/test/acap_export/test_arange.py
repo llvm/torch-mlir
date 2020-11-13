3c460925082607cc342ea4c821fbf80f06cb3eff
Stella Laurenzo <stellaraccident@gmail.com>
2020-11-12 17:42:19 -0800

    Fix dispatch of arange.
    * Fixes #107
    * I wouldn't say I love what had to be done here. Worth a conversation with the PT devs (probably as part of a rollup of a bunch of this stuff).

---
+++ frontends/pytorch/test/acap_export/test_arange.py
@@ -0,0 +1,21 @@
+# -*- Python -*-
+# This file is licensed under a pytorch-style license
+# See frontends/pytorch/LICENSE for license information.
+
+import torch
+import torch_mlir
+
+# RUN: %PYTHON %s | npcomp-opt | FileCheck %s
+torch_mlir.debug_trace_to_stderr()
+
+mb = torch_mlir.ModuleBuilder()
+N = 10
+
+with mb.capture_function("arange_test", []) as f:
+  x = torch.arange(N)
+  f.returns([x])
+
+# CHECK: %[[CST:.*]] = constant dense<[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]> : tensor<10xi64>
+# CHECK: %[[R:.*]] = numpy.create_array_from_tensor %[[CST]]
+# CHECK: return %[[R]]
+mb.module.operation.print()
