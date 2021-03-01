df4c5764da38b55c86f5b5421d6acdec7104376c
Sean Silva <silvasean@google.com>
2021-03-01 15:26:57 -0800

    Add support for `prim::unchecked_cast`.
    This arises when casting optionals, which happens a lot especially
    around handling of default arguments (python `if arg is None` idiom).
    
    In this case, the offending code for the model is in max_pool2d:
    [code link](https://github.com/pytorch/pytorch/blob/b3bf08e67f5cf29cbfd197e3c9e9a2f45e0b5c9d/torch/nn/functional.py#L657)

--- frontends/pytorch/test/node_import/prim.py
+++ frontends/pytorch/test/node_import/prim.py
@@ -5,6 +5,8 @@
 import torch
 import torch_mlir
 
+import typing
+
 # RUN: %PYTHON %s | npcomp-opt | FileCheck %s
 
 mb = torch_mlir.ModuleBuilder()
@@ -39,6 +41,26 @@ def prim_Print(x):
 @torch.jit.script
 def prim_RaiseException():
     raise Exception("Error")
+
+# CHECK-LABEL:   func @prim_unchecked_cast(
+# CHECK-SAME:                              %[[VAL_0:.*]]: !torch.optional<i64>) -> i64 {
+# CHECK:           %[[NONE:.*]] = basicpy.singleton : !basicpy.NoneType
+# CHECK:           %[[C3:.*]] = constant 3 : i64
+# CHECK:           %[[IS_NONE:.*]] = torch.kernel_call "aten::__is__" %[[VAL_0]], %[[NONE]] : (!torch.optional<i64>, !basicpy.NoneType) -> !basicpy.BoolType
+# CHECK:           %[[COND:.*]] = basicpy.bool_cast %[[IS_NONE]] : !basicpy.BoolType -> i1
+# CHECK:           %[[RESULT:.*]] = scf.if %[[COND]] -> (i64) {
+# CHECK:             scf.yield %[[C3]] : i64
+# CHECK:           } else {
+# CHECK:             %[[CASTED:.*]] = torch.prim.unchecked_cast %[[VAL_0]] : !torch.optional<i64> -> i64
+# CHECK:             scf.yield %[[CASTED]] : i64
+# CHECK:           }
+# CHECK:           return %[[RESULT:.*]] : i64
+@mb.import_function
+@torch.jit.script
+def prim_unchecked_cast(i: typing.Optional[int]):
+    if i is None:
+        return 3
+    return i
 
 mb.module.operation.print()
 print()
