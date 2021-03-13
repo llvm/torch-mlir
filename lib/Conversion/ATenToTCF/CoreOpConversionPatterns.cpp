4fd9b4afb526692ec4943633019eaa864315240b
Aaron Arthurs <34066482+ajarthurs@users.noreply.github.com>
2021-03-12 19:21:16 -0600

    Import ATen conv2d conversion and test (#180)
    * Import ATen conv2d conversion and test
    
    This is a first attempt at expanding ATen-to-TCF conversion for the
    conv2d operator. Eventually, this will come in use when lowering a
    high-level conv-based model.

--- lib/Conversion/ATenToTCF/CoreOpConversionPatterns.cpp
+++ lib/Conversion/ATenToTCF/CoreOpConversionPatterns.cpp
@@ -12,6 +12,7 @@
 #include "mlir/IR/Matchers.h"
 #include "mlir/IR/PatternMatch.h"
 #include "npcomp/Dialect/ATen/IR/ATenDialect.h"
+#include "npcomp/Dialect/Basicpy/IR/BasicpyOps.h"
 #include "npcomp/Dialect/TCF/IR/TCFOps.h"
 
 using namespace mlir;
@@ -66,6 +67,86 @@ public:
   }
 };
 
+/// The ATen Conv2dOp has seven arguments:
+///   input, weight, bias, stride, padding, dilation, groups
+
+class ConvertATenConv2d : public OpRewritePattern<aten::Conv2dOp> {
+  using OpRewritePattern::OpRewritePattern;
+  LogicalResult matchAndRewrite(aten::Conv2dOp srcConv2dOp,
+                                PatternRewriter &rewriter) const override {
+    auto results = srcConv2dOp.getOperation()->getResults();
+    assert(srcConv2dOp.getNumOperands() == 7 && "expected seven (7) operands");
+    assert(results.size() == 1 && "expected single result op");
+    // TODO: Replace constant int-list constraints for stride, padding, and dilation; and, constant int constraint for groups.
+    auto strideOp = srcConv2dOp.stride().getDefiningOp<Basicpy::BuildListOp>();
+    if (!strideOp) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "expected basicpy.build_list to drive stride input");
+    }
+    if (strideOp.getNumOperands() != 2) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "expected stride length of 2");
+    }
+    auto *strideOperand0Op = strideOp.getOperand(0).getDefiningOp();
+    auto *strideOperand1Op = strideOp.getOperand(1).getDefiningOp();
+    if (!matchPattern(strideOperand0Op, m_One())
+      || !matchPattern(strideOperand1Op, m_One())
+      ) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "aten.conv2d to tcf.conv_2d_nchw currently only supports stride == [1, 1]");
+    }
+    auto paddingOp = srcConv2dOp.padding().getDefiningOp<Basicpy::BuildListOp>();
+    if (!paddingOp) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "expected basicpy.build_list to drive padding input");
+    }
+    if (paddingOp.getNumOperands() != 2) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "expected padding length of 2");
+    }
+    auto *paddingOperand0Op = paddingOp.getOperand(0).getDefiningOp();
+    auto *paddingOperand1Op = paddingOp.getOperand(1).getDefiningOp();
+    if (!matchPattern(paddingOperand0Op, m_Zero())
+      || !matchPattern(paddingOperand1Op, m_Zero())
+      ) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "aten.conv2d to tcf.conv_2d_nchw currently only supports padding == [0, 0]");
+    }
+    auto dilationOp = srcConv2dOp.dilation().getDefiningOp<Basicpy::BuildListOp>();
+    if (!dilationOp) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "expected basicpy.build_list to drive dilation input");
+    }
+    if (dilationOp.getNumOperands() != 2) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "expected dilation length of 2");
+    }
+    auto *dilationOperand0Op = dilationOp.getOperand(0).getDefiningOp();
+    auto *dilationOperand1Op = dilationOp.getOperand(1).getDefiningOp();
+    if (!matchPattern(dilationOperand0Op, m_One())
+      || !matchPattern(dilationOperand1Op, m_One())
+      ) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "aten.conv2d to tcf.conv_2d_nchw currently only supports dilation == [1, 1]");
+    }
+    if (!matchPattern(srcConv2dOp.groups(), m_One())
+      ) {
+      return rewriter.notifyMatchFailure(
+          srcConv2dOp, "aten.conv2d to tcf.conv_2d_nchw currently only supports groups == 1");
+    }
+    auto tcfConvNCHWOp = rewriter.create<tcf::ConvNCHWOp>(
+        srcConv2dOp.getLoc(), srcConv2dOp.getResult().getType(), srcConv2dOp.input(),
+        srcConv2dOp.weight());
+    // TODO: Reference Torch Conv2D's bias flag to conditionally create TCF Add.
+    // (see https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html#torch.nn.Conv2d)
+    auto tcfConvNCHWBiasOp = rewriter.create<tcf::AddOp>(
+        srcConv2dOp.getLoc(), srcConv2dOp.getResult().getType(), tcfConvNCHWOp.getResult(),
+        srcConv2dOp.bias());
+    rewriter.replaceOp(srcConv2dOp, tcfConvNCHWBiasOp.getResult());
+    return success();
+  }
+};
+
 } // namespace
 
 void mlir::NPCOMP::populateCoreATenToTCFPatterns(
@@ -75,4 +156,5 @@ void mlir::NPCOMP::populateCoreATenToTCFPatterns(
   patterns.insert<ConvertBinaryElementwise<aten::MaximumOp, tcf::MaxOp>>(
       context);
   patterns.insert<ConvertBinaryElementwise<aten::MmOp, tcf::MatmulOp>>(context);
+  patterns.insert<ConvertATenConv2d>(context);
 }
--- test/Conversion/ATenToTCF/core_op_conversions.mlir
+++ test/Conversion/ATenToTCF/core_op_conversions.mlir
@@ -1,5 +1,18 @@
 // RUN: npcomp-opt <%s -convert-aten-to-tcf | FileCheck %s --dump-input=fail
 
+// CHECK-LABEL: @conv2d
+func @conv2d(%arg0: tensor<?x?x?x?xf32>, %arg1: tensor<?x?x?x?xf32>, %arg2: tensor<?x?x?x?xf32>) -> tensor<?x?x?x?xf32> {
+  // CHECK: %[[CONV2D_RESULT:.*]] = tcf.conv_2d_nchw %arg0, %arg1 : (tensor<?x?x?x?xf32>, tensor<?x?x?x?xf32>) -> tensor<?x?x?x?xf32>
+  // CHECK: tcf.add %[[CONV2D_RESULT]], %arg2 : (tensor<?x?x?x?xf32>, tensor<?x?x?x?xf32>) -> tensor<?x?x?x?xf32>
+  %c0_i64 = constant 0 : i64
+  %c1_i64 = constant 1 : i64
+  %0 = basicpy.build_list %c1_i64, %c1_i64 : (i64, i64) -> !basicpy.ListType
+  %1 = basicpy.build_list %c0_i64, %c0_i64 : (i64, i64) -> !basicpy.ListType
+  %2 = basicpy.build_list %c1_i64, %c1_i64 : (i64, i64) -> !basicpy.ListType
+  %3 = "aten.conv2d"(%arg0, %arg1, %arg2, %0, %1, %2, %c1_i64) : (tensor<?x?x?x?xf32>, tensor<?x?x?x?xf32>, tensor<?x?x?x?xf32>, !basicpy.ListType, !basicpy.ListType, !basicpy.ListType, i64) -> tensor<?x?x?x?xf32>
+  return %3 : tensor<?x?x?x?xf32>
+}
+
 // CHECK-LABEL: @binary_elementwise_ops
 // NOTE: These are all template expanded, so just testing an examplar op and
 // special cases.
