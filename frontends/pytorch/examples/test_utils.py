2ab62aec12fd0ea0fbe75df2bfb52307e160a478
Sean Silva <silvasean@google.com>
2021-04-06 15:58:37 -0700

    MILESTONE: TorchScript unary tanh runs on RefBackend
    This revamps the TORCH_TO_TCF_PASSES to reflect the new layering that we
    are doing in the compiler. See comments there for the layering.
    
    Also adds `frontends/pytorch/examples/torchscript_tanh_e2e.py` as an
    "example". E2E testing story TBD (want to get IREE working first).

--- frontends/pytorch/examples/test_utils.py
+++ frontends/pytorch/examples/test_utils.py
@@ -15,15 +15,16 @@ def _indent(value):
 
 
 def compare_outputs(torch_func, jit_func, *args):
-  print('—' * 80)
+  print('-' * 80)
 
   print(f"Input args:\n{_indent(args)}", file=sys.stderr)
   result = torch_func(*args)
   jit_result = jit_func(*args)
 
-  np.testing.assert_allclose(result.numpy(), jit_result)
+  np.testing.assert_allclose(result.numpy(), jit_result, rtol=1e-05, atol=1e-08)
 
   # Only print these if the test passes, as np.testing will print them if it
   # fails.
+  print("SUCCESS", file=sys.stderr)
   print(f"PyTorch Result:\n{_indent(result.numpy())}", file=sys.stderr)
   print(f"JIT Result:\n{_indent(jit_result)}", file=sys.stderr)
