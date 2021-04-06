ee14cca0253c8f7f28f28c979e83754f3e033f05
Sean Silva <silvasean@google.com>
2021-04-06 15:58:37 -0700

    MILESTONE: TorchScript unary tanh runs on RefBackend
    This revamps the TORCH_TO_TCF_PASSES to reflect the new layering that we
    are doing in the compiler. See comments there for the layering.
    
    Also adds `frontends/pytorch/examples/torchscript_tanh_e2e.py` as an
    "example". E2E testing story TBD (want to get IREE working first).

---
+++ frontends/pytorch/examples/torchscript_tanh_e2e.py
@@ -0,0 +1,49 @@
+# -*- Python -*-
+# This file is licensed under a pytorch-style license
+# See frontends/pytorch/LICENSE for license information.
+
+import typing
+
+import torch
+import torch_mlir
+
+import npcomp
+from npcomp.compiler.pytorch.backend import refjit
+from npcomp.compiler.utils import logging
+
+import test_utils
+
+#logging.enable()
+
+# RUN: %PYTHON %s | npcomp-opt | FileCheck %s
+
+mb = torch_mlir.ModuleBuilder()
+
+class TestModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        return torch.tanh(x)
+
+test_module = TestModule()
+class_annotator = torch_mlir.ClassAnnotator()
+recursivescriptmodule = torch.jit.script(test_module)
+torch.jit.save(recursivescriptmodule, '/tmp/foo.pt')
+
+class_annotator.exportNone(recursivescriptmodule._c._type())
+class_annotator.exportPath(recursivescriptmodule._c._type(), ['forward'])
+class_annotator.annotateShapesAndDtypes(recursivescriptmodule._c._type(), ['forward'], [
+    None,
+    ([2, 3, -1], torch.float32)
+])
+# TODO: Automatically handle unpacking Python class RecursiveScriptModule into the underlying ScriptModule.
+mb.import_module(recursivescriptmodule._c, class_annotator)
+#mb.module.operation.print()
+
+backend = refjit.CompilerBackend()
+compiled = backend.compile_object_graph(mb.module)
+jit_module = backend.load(compiled)
+
+torch.manual_seed(0)
+input = torch.rand(2, 3, 1)
+test_utils.compare_outputs(test_module.forward, jit_module.forward, input)
--- python/npcomp/compiler/pytorch/backend/refjit.py
+++ python/npcomp/compiler/pytorch/backend/refjit.py
@@ -20,6 +20,9 @@ __all__ = [
 # to a module semantics where symbols correspond to dotted paths into the
 # module.
 OBJECT_GRAPH_LOWERING_PASSES = (
+    # Globalize the program. The rest of the compiler assumes a globalized
+    # program, which makes all analyses and transforms significantly easier
+    # to write.
     "torch-globalize-pipeline",
     # symbol-dce is currently needed for correctness, as we don't have a lowering
     # in the backend for torch.global_slot's.
@@ -27,14 +30,42 @@ OBJECT_GRAPH_LOWERING_PASSES = (
     # bothersome because we don't currently have a lowering for them.
     # TODO: Support global slots in backends.
     "symbol-dce",
+    # Incorporate user annotations and remove signature Python-isms.
     "torch-adjust-calling-conventions",
 )
 
+# TODO: Replace this with lowering to "TCP + guards" -- that's the real
+# backend interface. Put differently, add "TCF to TCP" to the end of this
+# pipeline.
 TORCH_TO_TCF_PASSES = (
+    # Recognize ATen kernels.
     "func(aten-recognize-kernels)",
-    "func(convert-aten-to-tcf)",
+
+    # Convert the bulk of the program to ranked tensors with known dtype.
+    # This is the input to the backend layer that we are aiming for.
+
+    # First, unilaterally convert public functions to tensor.
+    # The way this pass is currently written, this implies that
+    # as pipeline authors, we are restricting our users to not be able to see
+    # updates to "out params" on their public functions.
+    # This is deemed ok for now.
     "numpy-public-functions-to-tensor",
-    "canonicalize",
+    # Convert the bulk of non-ABI-visible arrays to tensors.
+    "func(numpy-array-to-tensor)",
+    # Do shape and dtype refinement.
+    # We could do it sooner, but the pass currently doesn't have transfer
+    # functions for array ops.
+    "func(torch-refine-types)",
+    # Propagate to ABI return types the shape/dtype information discovered by
+    # the previous pass. Doing this is ABI-compatible for our backends.
+    "numpy-refine-public-return",
+    # Clean up a few stray array/tensor conversion remnants.
+    "func(numpy-array-to-tensor)",
+
+    # Lower to TCF which is the input to RefBackend.
+    # Most of this pass should be subsumed by aten->linalg+guards conversions.
+    # (the guard generation will be automated from the linalg Op DSL)
+    "func(convert-aten-to-tcf)",
 )
 
 # Re-export.
