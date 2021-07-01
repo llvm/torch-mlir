b1c03f13ba7a1b3ef24e6271d80a5e89ca001b33
Sean Silva <silvasean@google.com>
2021-07-01 13:44:04 -0700

    Add E2E tests to CI
    This includes IREE and RefBackend.
    
    This includes a fixup to torchscript_e2e_test.sh for handling the
    situation where PYTHONPATH was not already exported.

--- tools/torchscript_e2e_test.sh
+++ tools/torchscript_e2e_test.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 src_dir="$(realpath $(dirname $0)/..)"
 
 cd "$src_dir"
+
+# Ensure PYTHONPATH is set for export to child processes, even if empty.
+export PYTHONPATH=${PYTHONPATH-}
 source .env
+
 python -m frontends.pytorch.e2e_testing.torchscript.main "$@"
