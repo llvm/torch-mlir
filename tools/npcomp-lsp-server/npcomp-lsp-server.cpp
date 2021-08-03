c464cb107fff332cee973180b9b783e0ac98c382
Sean Silva <silvasean@google.com>
2021-08-03 16:47:38 -0700

    Add npcomp-lsp-server.
    To use, do `ninja npcomp-lsp-server`, copy `build/bin/npcomp-lsp-server`
    into your PATH somewhere, and then add
    ```
    "mlir.server_path": "npcomp-lsp-server",
    ```
    to your settings.json.
    
    Also bump llvm-project to 2d9759c7902c5cbc9a7e3ab623321d5578d51687 to
    bring in latest `mlir-lsp-server` changes.

---
+++ tools/npcomp-lsp-server/npcomp-lsp-server.cpp
@@ -0,0 +1,22 @@
+//===- npcomp-lsp-server.cpp - MLIR Language Server -------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
+#include "npcomp/InitAll.h"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+  DialectRegistry registry;
+  registerAllDialects(registry);
+  mlir::NPCOMP::registerAllDialects(registry);
+  return failed(MlirLspServerMain(argc, argv, registry));
+}
