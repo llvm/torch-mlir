1f00f95d2e943355bf758b6cab3481ffba79c2d9
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2021-09-22 12:27:55 -0500

    WIP implementation of torchfx (#304)
    Implements a python package for taking a `torch.fx.GraphModule`
    and turning it into MLIR in the `torch` dialect that can then
    be further compiled by `npcomp`. This is a WIP, so the coverage
    of PyTorch operations is very small.

---
+++ examples/torchfx/examples/example_add_tanh_sigmoid.py
@@ -0,0 +1,53 @@
+# -*- Python -*-
+# This file is licensed under a pytorch-style license
+# See frontends/pytorch/LICENSE for license information.
+
+import torch
+from torch.fx.experimental.fx_acc import acc_tracer
+import npcomp
+from npcomp.compiler.pytorch.backend import refjit
+from npcomp.passmanager import PassManager
+
+from torchfx2iree.builder import build_module
+from torchfx2iree.annotator import annotate_forward_args
+from torchfx2iree.torch_mlir_types import TorchTensorType
+
+
+class MyModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, y):
+        return torch.tanh(x) + torch.sigmoid(y)
+
+
+module = MyModule()
+traced_module = acc_tracer.trace(module, [torch.Tensor(2,2),
+                                          torch.Tensor(2,2)])
+
+print("TRACE")
+arg_type = TorchTensorType(shape=[None, None], dtype=torch.float)
+traced_module = annotate_forward_args(traced_module, [arg_type, arg_type])
+print(traced_module.graph)
+torch_mlir_module = build_module(traced_module)
+
+print("\n\nTORCH MLIR")
+torch_mlir_module.dump()
+
+with npcomp.ir.Context() as ctx:
+    npcomp.register_all_dialects(ctx)
+    lowered_mlir_module = npcomp.ir.Module.parse(str(torch_mlir_module))
+    pm = PassManager.parse('torchscript-to-npcomp-backend-pipeline')
+pm.run(lowered_mlir_module)
+
+print("\n\nLOWERED MLIR")
+lowered_mlir_module.dump()
+
+backend = refjit.RefjitNpcompBackend()
+compiled = backend.compile(lowered_mlir_module)
+jit_module = backend.load(compiled)
+
+print("\n\nRunning Forward Function")
+t = torch.rand((2, 2), dtype=torch.float)
+print("Compiled result:\n", jit_module.forward(t, t))
+print("\nExpected result:\n", module.forward(t, t))
---
+++ examples/torchfx/loc.py
@@ -0,0 +1,46 @@
+# -*- Python -*-
+# This file is licensed under a pytorch-style license
+# See frontends/pytorch/LICENSE for license information.
+#
+# pylint: disable=no-member, no-name-in-module, invalid-name, missing-function-docstring, fixme
+
+from typing import Mapping
+import inspect
+import ast
+import torch.fx
+
+class Annotation:
+    def __init__(self, name: str, row: int, col: int):
+        self.name = name
+        self.row = row
+        self.col = col
+
+Annotations = Mapping[torch.fx.Node, Annotation]
+
+class LocInspector:
+     #TODO: type of module?
+    def __init__(self, graph: torch.fx.Graph, module: torch.nn.Module):
+        self.annotations = {}
+        self.graph = graph
+        self.module = module
+        module_lines, self.module_start_lineno = \
+            inspect.getsourcelines(type(module))
+        module_src = "".join(module_lines)
+        self.src_file = inspect.getsourcefile(type(module))
+        self.module_ast = ast.parse(module_src)
+
+    def __str__(self):
+        newline = "\n\n"
+        values = ["Annotations: ", str(self.annotations), newline,
+                  "Src File: ", self.src_file, newline,
+                  "Module AST: ", ast.dump(self.module_ast)]
+        return "".join(values)
+
+    def annotate_defs(self) -> None:
+        for node in ast.walk(self.module_ast):
+            if isinstance(node, (ast.ClassDef,
+                                 ast.FunctionDef)):
+                # subtract 1 because lineno's begin on 1
+                lineno = node.lineno + self.module_start_lineno - 1
+                self.annotations[node.name] = (self.src_file, lineno,
+                                               node.col_offset)
