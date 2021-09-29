5b6902e31c1bf61859ec03e77750407dbb011e44
Sean Silva <silvasean@google.com>
2021-09-29 16:03:40 +0000

    Dual license the torch-mlir project.
    This commit (with approval from all contributors) dual licenses
    the torch-mlir project under both the standard LLVM license and the
    standard PyTorch license. This will facilitate moving code between
    torch-mlir and the two upstream projects.
    
    The standard file comment is now:
    
    ```
    // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
    // See https://llvm.org/LICENSE.txt for license information.
    // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
    // Also available under a BSD-style license. See LICENSE.
    ```
    
    See `LICENSE` in the project root for the terms of both licenses.

--- python/torch_mlir/dialects/torch/importer/jit_ir/__init__.py
+++ python/torch_mlir/dialects/torch/importer/jit_ir/__init__.py
@@ -1,6 +1,7 @@
-#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-#  See https://llvm.org/LICENSE.txt for license information.
-#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Also available under a BSD-style license. See LICENSE.
 
 # This is a trampoline module which loads the _torch_mlir native module
 # and binds names locally. It exists to allow for customization of behavior
