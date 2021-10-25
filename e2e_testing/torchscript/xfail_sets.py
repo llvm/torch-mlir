3797ac96745a69a8d3f395b22061b347d5f732e4
Stella Laurenzo <stellaraccident@gmail.com>
2021-10-25 18:43:21 +0000

    Bump llvm-project to f1b922188ead5ca492c8d8edd47921b013a22ae0.
    Includes a fix to use `add_mlir_public_c_api_library` for Torch-MLIR's CAPI library, which is now required (note: upstream sample has it the right way).
    
    Disabled a TOSA test per discussion: https://github.com/llvm/torch-mlir/issues/379

--- e2e_testing/torchscript/xfail_sets.py
+++ e2e_testing/torchscript/xfail_sets.py
@@ -21,4 +21,7 @@ REFBACKEND_XFAIL_SET = COMMON_TORCH_MLIR_LOWERING_XFAILS
 
 # Write the TOSA set as a "passing" set as it is very early in development
 # and very few tests work yet.
-TOSA_PASS_SET = {"ElementwiseUnaryModule_basic"}
+TOSA_PASS_SET = {
+    # See: https://github.com/llvm/torch-mlir/issues/379
+    #"ElementwiseUnaryModule_basic",
+}
