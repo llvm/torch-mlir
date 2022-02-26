58338f79a1030a9eaa8a17c6a8cdd63a50be564e
Jae Hoon (Antonio) Kim <17433012+antoniojkim@users.noreply.github.com>
2022-02-25 19:50:09 -0500

    Torch-MLIR LTC Backend Lowering Codegen (#621)
    * Codegen and build LTC lowering
    
    * Add LazyShapeInference header

--- python/torch_mlir/csrc/backend/backend_impl.h
+++ python/torch_mlir/csrc/backend/backend_impl.h
@@ -1,3 +1,18 @@
+//===- backend_impl.h -----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+// The Torch-MLIR backend class API that handles lowering LTC ATen ops to MLIR
+// using the Torch-MLIR ATen dialect
+//
+// This file is adapted from pytorch/pytorch
+// https://github.com/pytorch/pytorch/blob/lazy_tensor_staging/lazy_tensor_core/lazy_tensor_core/csrc/ts_backend/backend_impl.h
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <torch/csrc/lazy/backend/backend_data.h>
--- python/torch_mlir/csrc/utils/exception.h
+++ python/torch_mlir/csrc/utils/exception.h
@@ -1,3 +1,12 @@
+//===- exception.h --------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <exception>
