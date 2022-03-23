9f9d4b6f50a6ff3641ea3f1f37af3cfbfca40d89
Antonio Kim <antonio.kim@cerebras.net>
2022-03-23 12:01:25 -0700

    Got LTC working until compile

--- python/torch_mlir/csrc/backend/aten_eager_fallback.cpp
+++ python/torch_mlir/csrc/backend/aten_eager_fallback.cpp
@@ -10,29 +10,49 @@
 // https://github.com/pytorch/pytorch/blob/lazy_tensor_staging/lazy_tensor_core/lazy_tensor_core/csrc/ts_backend/aten_eager_fallback.cpp
 //===----------------------------------------------------------------------===//
 
+#include <iostream>
 #include <unordered_map>
 
 #include <torch/csrc/lazy/backend/backend_interface.h>
 #include <torch/csrc/lazy/core/metrics.h>
 
-#include "aten_eager_fallback.h"
 #include "../utils/exception.h"
+#include "aten_eager_fallback.h"
 
 namespace torch_lazy_tensors {
 
-static std::unordered_map<std::string, ::torch::lazy::Counter*> _eager_fallback_counters;
+static std::unordered_map<std::string, ::torch::lazy::Counter*>
+    _eager_fallback_counters;
 
 bool force_eager_fallback(c10::Symbol op) {
-    return false;  // Never force eager fallback
+  static char* force_str = std::getenv("LTC_FORCE_FALLBACK");
+  if (force_str != nullptr) {
+    static auto force_sym = c10::Symbol::fromQualString(std::string(force_str));
+    if (op == force_sym) {
+      std::cout << "MLIR force_eager_fallback(" << force_str << "): true"
+                << std::endl;
+      return true;
+    }
+  }
+  std::cout << "MLIR force_eager_fallback(" << op.toQualString() << "): false"
+            << std::endl;
+  return false;
 }
 
-void ltc_eager_fallback(const c10::OperatorHandle& op,
-                        torch::jit::Stack* stack) {
-    UNSUPPORTED_ERROR("ltc_eager_fallback is not supported");
+void ltc_eager_fallback(
+    const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+  const auto name = c10::toString(op.operator_name());
+  UNSUPPORTED_ERROR(
+      "MLIR ltc_eager_fallback is not supported for op: " << name);
 }
+
+std::function<void(void)> register_mlir_ltc_eager_fallback;
 
 TORCH_LIBRARY_IMPL(_, Lazy, m) {
-  m.fallback(torch::CppFunction::makeFromBoxedFunction<&ltc_eager_fallback>());
+  register_mlir_ltc_eager_fallback = [&]() {
+    m.fallback(
+        torch::CppFunction::makeFromBoxedFunction<&ltc_eager_fallback>());
+  };
 }
 
-}  // namespace torch_lazy_tensors
+} // namespace torch_lazy_tensors
