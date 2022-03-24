c3b20e444c01d1884978f402da056cddf632171d
Jae Hoon (Antonio) Kim <17433012+antoniojkim@users.noreply.github.com>
2022-03-24 10:15:43 -0400

    Got LTC working until compile (#689)

--- python/torch_mlir/csrc/backend/mlir_lowering_context.h
+++ python/torch_mlir/csrc/backend/mlir_lowering_context.h
@@ -10,60 +10,58 @@
 // https://github.com/pytorch/pytorch/blob/lazy_tensor_staging/torch/csrc/lazy/ts_backend/ts_lowering_context.h
 //===----------------------------------------------------------------------===//
 
-
 #pragma once
 
 #include <vector>
 
 #include <torch/csrc/lazy/backend/lowering_context.h>
 
-
 namespace torch {
 namespace lazy {
 
-class MlirComputation : public torch::lazy::Computation {
-  public:
-    int parameters_size() const override;
+class TORCH_API MlirComputation : public torch::lazy::Computation {
+public:
+  int parameters_size() const override;
 
-    virtual const std::vector<torch::lazy::Shape>& parameter_shapes() const override;
+  virtual const std::vector<torch::lazy::Shape>&
+  parameter_shapes() const override;
 
-    virtual const std::vector<std::string>& parameter_names() const override;
+  virtual const std::vector<std::string>& parameter_names() const override;
 
-    virtual const torch::lazy::Shape& result_shape() const override;
+  virtual const torch::lazy::Shape& result_shape() const override;
 };
 
-class MlirLoweringContext : public torch::lazy::LoweringContext {
-  public:
+class TORCH_API MlirLoweringContext : public torch::lazy::LoweringContext {
+public:
+  MlirLoweringContext(
+      const std::string& name, torch::lazy::BackendDevice device);
+  MlirLoweringContext(
+      const std::string& name, torch::lazy::BackendDevice device,
+      c10::ArrayRef<torch::lazy::Node*> post_order,
+      torch::lazy::Util::EmissionMap emit_status);
 
-    MlirLoweringContext(const std::string& name, torch::lazy::BackendDevice device);
-    MlirLoweringContext(const std::string& name,
-                        torch::lazy::BackendDevice device,
-                        c10::ArrayRef<torch::lazy::Node*> post_order,
-                        torch::lazy::Util::EmissionMap emit_status);
-    
-    // Get the shape of the result tuple component, given by index.
-    virtual torch::lazy::Shape GetResultShape(size_t index) const override;
+  // Get the shape of the result tuple component, given by index.
+  virtual torch::lazy::Shape GetResultShape(size_t index) const override;
 
-    // Adds the given output as a component of the result tuple and returns its
-    // assigned position within the tuple.
-    virtual size_t AddResult(const torch::lazy::Output& output) override;
+  // Adds the given output as a component of the result tuple and returns its
+  // assigned position within the tuple.
+  virtual size_t AddResult(const torch::lazy::Output& output) override;
 
-    // Associates the given output with the input parameter of the given index and
-    // shape. Only used for the operator-by-operator execution, mostly for
-    // debugging purposes.
-    virtual void AddParameter(const torch::lazy::Output& output,
-                              size_t index,
-                              const torch::lazy::Shape& shape,
-                              const std::string& name) override;
+  // Associates the given output with the input parameter of the given index and
+  // shape. Only used for the operator-by-operator execution, mostly for
+  // debugging purposes.
+  virtual void AddParameter(
+      const torch::lazy::Output& output, size_t index,
+      const torch::lazy::Shape& shape, const std::string& name) override;
 
-    // Build the computation capturing all the operations created with the
-    // embedded builder (returned by the builder() API).
-    virtual torch::lazy::ComputationPtr Build() override;
+  // Build the computation capturing all the operations created with the
+  // embedded builder (returned by the builder() API).
+  virtual torch::lazy::ComputationPtr Build() override;
 
-  private:
-    std::vector<const torch::lazy::Node*> result_tuple_;
-    torch::lazy::OutputMap<const torch::lazy::Node*> emitted_outputs_;
+private:
+  std::vector<const torch::lazy::Node*> result_tuple_;
+  torch::lazy::OutputMap<const torch::lazy::Node*> emitted_outputs_;
 };
 
-}  // namespace lazy
-}  // namespace torch
+} // namespace lazy
+} // namespace torch
--- python/torch_mlir/csrc/utils/exception.h
+++ python/torch_mlir/csrc/utils/exception.h
@@ -13,17 +13,20 @@
 #include <sstream>
 #include <string>
 
-#define UNIMPLEMENTED_ERROR(msg)                \
-    {                                           \
-        std::ostringstream err;                 \
-        err << "Unimplemented Error: " << msg;  \
-        throw std::runtime_error(err.str());    \
-    }
+#define UNIMPLEMENTED_ERROR(msg)                                               \
+  {                                                                            \
+    std::ostringstream err;                                                    \
+    err << "Unimplemented Error: " << msg;                                     \
+    throw std::runtime_error(err.str());                                       \
+  }
 
+#define UNIMPLEMENTED_FUNCTION_ERROR()                                         \
+  UNIMPLEMENTED_ERROR(                                                         \
+      "\n\t" << __FILE__ << ":" << __LINE__ << "    " << __PRETTY_FUNCTION__)
 
-#define UNSUPPORTED_ERROR(msg)                  \
-    {                                           \
-        std::ostringstream err;                 \
-        err << "Unsupported Error: " << msg;    \
-        throw std::runtime_error(err.str());    \
-    }
+#define UNSUPPORTED_ERROR(msg)                                                 \
+  {                                                                            \
+    std::ostringstream err;                                                    \
+    err << "Unsupported Error: " << msg;                                       \
+    throw std::runtime_error(err.str());                                       \
+  }
