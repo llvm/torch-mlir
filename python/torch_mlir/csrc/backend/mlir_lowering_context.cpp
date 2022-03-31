e797d89674c60bfabc22b4687d6e7baf459ee805
Henry Tu <henry.tu@cerebras.net>
2022-03-31 10:01:37 -0400

    Added JIT to MLIR lowering
    Lowering to JIT is performed in a way similar to how it's done in the TS LTC backend. After a jit::Graph is constructed, it gets converted to a jit::Function, which is fed into the existing utility to generate an MlirModule in torch-mlir.

--- python/torch_mlir/csrc/backend/mlir_lowering_context.cpp
+++ python/torch_mlir/csrc/backend/mlir_lowering_context.cpp
@@ -12,16 +12,93 @@
 
 #include <iostream>
 
+#include <torch/csrc/jit/api/compilation_unit.h>
+#include <torch/csrc/lazy/core/lazy_graph_executor.h>
+
+#include "../../dialects/torch/importer/jit_ir/csrc/function_importer.h"
 #include "../utils/debug.h"
 #include "../utils/exception.h"
+#include "backend_impl.h"
+#include "mlir-c/Registration.h"
 #include "mlir_lowering_context.h"
+#include "mlir_node.h"
+#include "torch-mlir-c/Registration.h"
 
 namespace torch {
 namespace lazy {
 
+///////////////////////////////////////////////////////////////////////////////
+// TorchMlir Computation
+///////////////////////////////////////////////////////////////////////////////
+
+TorchMlirComputation::TorchMlirComputation(
+    MlirOperation func_op, MlirContext mlir_context,
+    const std::shared_ptr<torch::jit::Graph>& graph)
+    : func_op_(std::move(func_op)), mlir_context_(std::move(mlir_context)),
+      graph_(graph), num_results_(graph_->outputs().size()) {
+
+  // TODO(henrytu): Save parameter shape information.
+
+  for (torch::jit::Value* input : graph_->inputs()) {
+    parameter_names_.push_back(input->debugName());
+  }
+}
+
+int TorchMlirComputation::parameters_size() const {
+  return parameter_names_.size();
+}
+
+const std::vector<torch::lazy::Shape>&
+TorchMlirComputation::parameter_shapes() const {
+  throw std::runtime_error(
+      "todo(whc) implement ts computation shapes or change interface");
+  return parameter_shapes_;
+}
+
+const std::vector<std::string>& TorchMlirComputation::parameter_names() const {
+  return parameter_names_;
+}
+
+const torch::lazy::Shape& TorchMlirComputation::result_shape() const {
+  throw std::runtime_error(
+      "todo(whc) implement ts computation shapes or change interface");
+  return result_shape_;
+}
+
+unsigned TorchMlirComputation::num_results() const { return num_results_; }
+
+MlirOperation TorchMlirComputation::func_op() const { return func_op_; }
+
+std::string TorchMlirComputation::to_string() const {
+  // Since we use the C-MLIR API, we need to use a callback to print.
+  MlirStringCallback print_callback = [](MlirStringRef part, void* user_data) {
+    // user_data is a void ptr to some data structure of our choice -- in this
+    // case, the string stream where we'll be accumulating the strings.
+    std::stringstream* ss_ptr = static_cast<std::stringstream*>(user_data);
+    *ss_ptr << std::string(part.data, part.length);
+  };
+
+  std::stringstream ss;
+  ss << "JIT Graph: \n"
+     << graph_->toString() << "\n\n"
+     << "MLIR: \n";
+  mlirOperationPrint(func_op_, print_callback, &ss);
+
+  return ss.str();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// TorchMlir Lowering Context
+///////////////////////////////////////////////////////////////////////////////
+
 TorchMlirLoweringContext::TorchMlirLoweringContext(
     const std::string& name, BackendDevice device)
-    : LoweringContext(name, std::forward<BackendDevice>(device)) {}
+    : LoweringContext(name, std::forward<BackendDevice>(device)),
+      graph_(std::make_shared<torch::jit::Graph>()),
+      mlir_context_(mlirContextCreate()) {
+  lowering_ = TorchMlirNodeLoweringInterface::Create(this);
+  RegisterMlirDialects();
+}
 
 TorchMlirLoweringContext::TorchMlirLoweringContext(
     const std::string& name, BackendDevice device,
@@ -29,53 +106,28 @@ TorchMlirLoweringContext::TorchMlirLoweringContext(
     : LoweringContext(
           name, std::forward<BackendDevice>(device),
           std::forward<c10::ArrayRef<torch::lazy::Node*>>(post_order),
-          std::forward<Util::EmissionMap>(emit_status)) {}
+          std::forward<Util::EmissionMap>(emit_status)),
+      graph_(std::make_shared<torch::jit::Graph>()),
+      mlir_context_(mlirContextCreate()) {
+  lowering_ = TorchMlirNodeLoweringInterface::Create(this);
+  for (auto node : post_order) {
+    bool ok = lowering_->Lower(node);
+    CHECK(ok) << "Failed to lower: " << *node;
+  }
 
-int TorchMlirComputation::parameters_size() const { UNIMPLEMENTED_FUNCTION_ERROR(); }
-
-const std::vector<torch::lazy::Shape>&
-TorchMlirComputation::parameter_shapes() const {
-  UNIMPLEMENTED_FUNCTION_ERROR();
-}
-
-const std::vector<std::string>& TorchMlirComputation::parameter_names() const {
-  UNIMPLEMENTED_FUNCTION_ERROR();
-}
-
-const torch::lazy::Shape& TorchMlirComputation::result_shape() const {
-  UNIMPLEMENTED_FUNCTION_ERROR();
-}
-
-std::string TorchMlirComputation::to_string() const {
-  UNIMPLEMENTED_FUNCTION_ERROR();
+  RegisterMlirDialects();
 }
 
 // Get the shape of the result tuple component, given by index.
-torch::lazy::Shape TorchMlirLoweringContext::GetResultShape(size_t index) const {
+torch::lazy::Shape
+TorchMlirLoweringContext::GetResultShape(size_t index) const {
   UNIMPLEMENTED_FUNCTION_ERROR();
 }
 
-// Adds the given output as a component of the result tuple and returns its
-// assigned position within the tuple.
-size_t TorchMlirLoweringContext::AddResult(const torch::lazy::Output& output) {
+size_t TorchMlirLoweringContext::AddResult(const Output& output) {
   PRINT_FUNCTION();
-  const torch::lazy::Node* node;
-  auto it = emitted_outputs_.find(output);
-  if (it == emitted_outputs_.end()) {
-    node = output.node;
 
-    auto post_order = Util::ComputePostOrder(node, &emit_status_);
-    for (auto po_node : post_order) {
-      // TODO: uncomment after lowering is implemented
-      // bool ok = lowering_->Lower(node);
-      // TORCH_CHECK(ok, "Failed to lower: ", node->ToString());
-    }
-    emitted_outputs_[output] = node;
-  } else {
-    node = it->second;
-  }
-  result_tuple_.emplace_back(node);
-  return result_tuple_.size() - 1;
+  return AddResult(GetOutputOp(output));
 }
 
 // Associates the given output with the input parameter of the given index and
@@ -90,17 +142,113 @@ void TorchMlirLoweringContext::AddParameter(
 // Build the computation capturing all the operations created with the
 // embedded builder (returned by the builder() API).
 ComputationPtr TorchMlirLoweringContext::Build() {
-  PRINT_FUNCTION()
-  for (const torch::lazy::Node* output : result_tuple_) {
+  PRINT_FUNCTION();
+
+  for (torch::jit::Value* output : root_tuple_) {
+    graph_->block()->registerOutput(output);
   }
-  return std::make_shared<TorchMlirComputation>();
+
+  // Create jit::Function from jit::Graph.
+  c10::QualifiedName name("graph");
+  auto cu = std::make_shared<torch::jit::CompilationUnit>();
+  // IMPORTANT: We pass in a COPY of the graph into create_function, since it
+  //            may get mutated in the process.
+  auto jit_fn = cu->create_function(std::move(name), std::move(graph_->copy()));
+
+  // Generate MLIR.
+  MlirOperation func_op =
+      torch_mlir::importJitFunctionAsFuncOp(mlir_context_, jit_fn);
+
+  // TODO(henrytu): Inject tensor shapes into func_op
+  return std::make_shared<TorchMlirComputation>(func_op, mlir_context_, graph_);
 }
 
-// Retrieves the lowered operation for an output. If the requested output is
-// not available yet, the graph behind the output's Node is lowered, and the
-// corresponding MLIR operation returned.
-torch::jit::Value* GetOutputOp(const Output& output) {
-  UNIMPLEMENTED_FUNCTION_ERROR();
+torch::jit::Value* TorchMlirLoweringContext::GetOutputOp(const Output& output) {
+  PRINT_FUNCTION();
+
+  auto it = emitted_outputs_.find(output);
+  if (it == emitted_outputs_.end()) {
+    auto post_order = Util::ComputePostOrder(output.node, &emit_status_);
+    for (auto node : post_order) {
+      bool ok = lowering_->Lower(node);
+      TORCH_CHECK(ok, "Failed to lower: ", node->ToString());
+    }
+    // At this point the output better be present, otherwise there is an issue
+    // with the lowering code.
+    it = emitted_outputs_.find(output);
+    TORCH_CHECK(
+        it != emitted_outputs_.end(),
+        "No MLIR operation emitted for output: ", output.ToString());
+  }
+  return it->second;
+}
+
+void TorchMlirLoweringContext::AssignOutputOp(
+    const Output& output, torch::jit::Value* op) {
+  PRINT_FUNCTION();
+
+  auto torch_mlir_node =
+      NodeCast<TorchMlirNode>(output.node, output.node->op());
+  if (!torch_mlir_node->getPythonStacktrace().empty()) {
+    op->node()->s_(
+        c10::Symbol::attr("source"), torch_mlir_node->getPythonStacktrace());
+  }
+  emitted_outputs_[output] = std::move(op);
+}
+
+torch::jit::Value* TorchMlirLoweringContext::GetParameter(BackendDataPtr data) {
+  PRINT_FUNCTION();
+
+  if (!dynamic_cast<TorchMlirBackendData*>(data.get())) {
+    TORCH_CHECK(
+        false,
+        "Expected TorchMlirBackendData. Got some other BackendData type");
+  }
+  const auto mlir_data = std::static_pointer_cast<TorchMlirBackendData>(data);
+
+  BackendData::Handle handle = mlir_data->GetHandle();
+  auto it = parameters_map_.find(handle);
+
+  if (it == parameters_map_.end()) {
+    torch::jit::Value* param =
+        graph_->addInput(c10::str("p", parameters_.size()));
+
+    auto info = mlir_data->mlir_info();
+    if (info->scalar.has_value()) {
+      auto& scalar = info->scalar.value();
+      if (scalar.isFloatingPoint()) {
+        param->setType(c10::FloatType::get());
+      } else if (scalar.isIntegral(true)) {
+        param->setType(c10::IntType::get());
+      } else {
+        TORCH_CHECK(
+            false, "Unhandled scalar type: ", c10::toString(scalar.type()));
+      }
+    }
+
+    it = parameters_map_.emplace(handle, Parameter{param, parameters_.size()})
+             .first;
+    parameters_.push_back(mlir_data);
+  }
+
+  parameter_sequence_.push_back(it->second.index);
+  return it->second.param;
+}
+
+std::shared_ptr<torch::jit::Graph> TorchMlirLoweringContext::graph() const {
+  return graph_;
+}
+
+size_t TorchMlirLoweringContext::AddResult(torch::jit::Value* op) {
+  PRINT_FUNCTION();
+  root_tuple_.push_back(std::move(op));
+  return root_tuple_.size() - 1;
+}
+
+void TorchMlirLoweringContext::RegisterMlirDialects() {
+  // https://reviews.llvm.org/D88162
+  mlirRegisterAllDialects(mlir_context_);
+  torchMlirRegisterAllDialects(mlir_context_);
 }
 
 } // namespace lazy
--- python/torch_mlir/csrc/backend/mlir_lowering_context.h
+++ python/torch_mlir/csrc/backend/mlir_lowering_context.h
@@ -7,28 +7,67 @@
 //
 //===----------------------------------------------------------------------===//
 // This file is adapted from pytorch/pytorch
-// https://github.com/pytorch/pytorch/blob/torch/csrc/lazy/ts_backend/ts_lowering_context.h
+// https://github.com/pytorch/pytorch/blob/master/torch/csrc/lazy/ts_backend/ts_lowering_context.h
 //===----------------------------------------------------------------------===//
 
 #pragma once
 
 #include <vector>
 
+#include <torch/csrc/api/include/torch/jit.h>
 #include <torch/csrc/lazy/backend/lowering_context.h>
+
+#include "mlir-c/IR.h"
+#include "mlir_node_lowering.h"
 
 namespace torch {
 namespace lazy {
 
+class TORCH_API TorchMlirNodeLoweringInterface {
+  /**
+   * This interface is only needed for legacy ops, and can be removed once all
+   * ops implement LtcMlirNode->lower().
+   * */
+public:
+  TorchMlirNodeLoweringInterface() = default;
+
+  virtual ~TorchMlirNodeLoweringInterface() = default;
+
+  virtual bool Lower(const Node* node) = 0;
+
+  static std::unique_ptr<TorchMlirNodeLoweringInterface>
+  Create(LoweringContext* loctx);
+};
+
 class TORCH_API TorchMlirComputation : public torch::lazy::Computation {
 public:
+  TorchMlirComputation(
+      MlirOperation func_op, MlirContext mlir_context,
+      const std::shared_ptr<torch::jit::Graph>& graph);
+
   int parameters_size() const override;
 
-  virtual const std::vector<torch::lazy::Shape>&
-  parameter_shapes() const override;
+  const std::vector<torch::lazy::Shape>& parameter_shapes() const override;
 
-  virtual const std::vector<std::string>& parameter_names() const override;
+  const std::vector<std::string>& parameter_names() const override;
 
-  virtual const torch::lazy::Shape& result_shape() const override;
+  const torch::lazy::Shape& result_shape() const override;
+
+  unsigned num_results() const;
+
+  MlirOperation func_op() const;
+
+  std::string to_string() const;
+
+private:
+  std::vector<std::string> parameter_names_;
+  std::vector<Shape> parameter_shapes_;
+  Shape result_shape_;
+
+  MlirOperation func_op_;
+  MlirContext mlir_context_;
+  std::shared_ptr<torch::jit::Graph> graph_;
+  unsigned num_results_;
 };
 
 class TORCH_API TorchMlirLoweringContext : public torch::lazy::LoweringContext {
@@ -41,31 +80,56 @@ public:
       torch::lazy::Util::EmissionMap emit_status);
 
   // Get the shape of the result tuple component, given by index.
-  virtual torch::lazy::Shape GetResultShape(size_t index) const override;
+  torch::lazy::Shape GetResultShape(size_t index) const override;
 
   // Adds the given output as a component of the result tuple and returns its
   // assigned position within the tuple.
-  virtual size_t AddResult(const torch::lazy::Output& output) override;
+  size_t AddResult(const torch::lazy::Output& output) override;
 
   // Associates the given output with the input parameter of the given index and
   // shape. Only used for the operator-by-operator execution, mostly for
   // debugging purposes.
-  virtual void AddParameter(
+  void AddParameter(
       const torch::lazy::Output& output, size_t index,
       const torch::lazy::Shape& shape, const std::string& name) override;
 
   // Build the computation capturing all the operations created with the
   // embedded builder (returned by the builder() API).
-  virtual torch::lazy::ComputationPtr Build() override;
+  torch::lazy::ComputationPtr Build() override;
 
   // Retrieves the lowered operation for an output. If the requested output is
   // not available yet, the graph behind the output's Node is lowered, and the
-  // corresponding MLIR operation returned.
+  // corresponding TS operation returned.
   torch::jit::Value* GetOutputOp(const Output& output);
 
+  // Assigns the given TS operation to the specified output. As outputs are
+  // lowered in a post-order fashion, later nodes should always find their
+  // operands among the emitted outputs.
+  void AssignOutputOp(const Output& output, torch::jit::Value* op);
+
+  // If a parameter associated with data has already been declared, it will be
+  // returned. Otherwise a new one will be created, associated with the tensor
+  // held in data.
+  torch::jit::Value* GetParameter(BackendDataPtr data);
+
+  std::shared_ptr<torch::jit::Graph> graph() const;
+
 private:
-  std::vector<const torch::lazy::Node*> result_tuple_;
-  torch::lazy::OutputMap<const torch::lazy::Node*> emitted_outputs_;
+  struct Parameter {
+    torch::jit::Value* param;
+    size_t index = 0;
+  };
+
+  size_t AddResult(torch::jit::Value* op);
+
+  void RegisterMlirDialects();
+
+  std::shared_ptr<torch::jit::Graph> graph_;
+  MlirContext mlir_context_;
+  std::unordered_map<BackendData::Handle, Parameter> parameters_map_;
+  std::vector<torch::jit::Value*> root_tuple_;
+  OutputMap<torch::jit::Value*> emitted_outputs_;
+  std::unique_ptr<TorchMlirNodeLoweringInterface> lowering_;
 };
 
 } // namespace lazy
