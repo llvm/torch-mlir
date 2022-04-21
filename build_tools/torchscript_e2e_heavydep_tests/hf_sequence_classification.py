f950233aca1e31536bc3f4788a3291d7a720475c
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2022-04-21 18:19:24 +0530

    [MLIR][TORCH] Add heavydep tests for torch benchmarks
    This commit adds e2e heavydep tests for the torch benchmarks.
    
    Signed-Off By: Vivek Khandelwal <vivek@nod-labs.com>

--- build_tools/torchscript_e2e_heavydep_tests/hf_sequence_classification.py
+++ build_tools/torchscript_e2e_heavydep_tests/hf_sequence_classification.py
@@ -3,9 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # Also available under a BSD-style license. See LICENSE.
 
-# A pretrained model to classify the input sentence.
-# https://huggingface.co/philschmid/MiniLM-L6-H384-uncased-sst2
-
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
@@ -14,6 +11,7 @@ from torch_mlir_e2e_test.torchscript.registry import register_test_case
 from torch_mlir_e2e_test.torchscript.annotations import annotate_args, export
 
 torch.manual_seed(0)
+
 
 def prepare_sentence_tokens(hf_model: str, sentence: str):
     tokenizer = AutoTokenizer.from_pretrained(hf_model)
@@ -24,17 +22,16 @@ def getTracedRecursiveScriptModule(module, trace_input):
     traced_module = torch.jit.trace_module(module, trace_input)
     script_module = traced_module._actual_script_module
     export(script_module.forward)
-    annotate_args_decorator = annotate_args(
-        [
-            None,
-            ([-1, -1], torch.int64, True),
-        ]
-    )
+    annotate_args_decorator = annotate_args([
+        None,
+        ([-1, -1], torch.int64, True),
+    ])
     annotate_args_decorator(script_module.forward)
     return script_module
 
 
 class HfSequenceClassification(torch.nn.Module):
+
     def __init__(self, model_name: str):
         super().__init__()
         self.model = AutoModelForSequenceClassification.from_pretrained(
@@ -50,12 +47,10 @@ class HfSequenceClassification(torch.nn.Module):
         self.model.eval()
 
     @export
-    @annotate_args(
-        [
-            None,
-            ([-1, -1], torch.int64, True),
-        ]
-    )
+    @annotate_args([
+        None,
+        ([-1, -1], torch.int64, True),
+    ])
     def forward(self, tokens):
         return self.model.forward(tokens)[0]
 
@@ -65,37 +60,122 @@ class HfSequenceClassification(torch.nn.Module):
 hf_minilm_model = "philschmid/MiniLM-L6-H384-uncased-sst2"
 
 trace_input = {
-    "forward": prepare_sentence_tokens(hf_minilm_model, "how do you like the project")
+    "forward":
+    prepare_sentence_tokens(hf_minilm_model, "how do you like the project")
 }
-test_input = prepare_sentence_tokens(
-    hf_minilm_model, "this project is very interesting")
+test_input = prepare_sentence_tokens(hf_minilm_model,
+                                     "this project is very interesting")
 
 
-@register_test_case(
-    module_factory=lambda: getTracedRecursiveScriptModule(
-        HfSequenceClassification(hf_minilm_model), trace_input
-    )
-)
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_minilm_model), trace_input))
 def MiniLMSequenceClassification_basic(module, tu: TestUtils):
     module.forward(test_input)
+
 
 # ==============================================================================
 
 hf_albert_model = "albert-base-v2"
 
 trace_input = {
-    "forward": prepare_sentence_tokens(hf_albert_model, "how do you like the project")
+    "forward":
+    prepare_sentence_tokens(hf_albert_model, "how do you like the project")
 }
-test_input = prepare_sentence_tokens(
-    hf_albert_model, "this project is very interesting")
+test_input = prepare_sentence_tokens(hf_albert_model,
+                                     "this project is very interesting")
 
 
-@register_test_case(
-    module_factory=lambda: getTracedRecursiveScriptModule(
-        HfSequenceClassification(hf_albert_model), trace_input
-    )
-)
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_albert_model), trace_input))
 def AlbertSequenceClassification_basic(module, tu: TestUtils):
     module.forward(test_input)
 
+
 # ==============================================================================
+
+hf_bart_model = "facebook/bart-base"
+
+trace_input = {
+    "forward":
+    prepare_sentence_tokens(hf_bart_model, "how do you like the project")
+}
+test_input = prepare_sentence_tokens(hf_bart_model,
+                                     "this project is very interesting")
+
+
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_bart_model), trace_input))
+def BartSequenceClassification_basic(module, tu: TestUtils):
+    module.forward(test_input)
+
+
+# ==============================================================================
+
+hf_bert_model = "bert-base-uncased"
+
+trace_input = {
+    "forward":
+    prepare_sentence_tokens(hf_bert_model, "how do you like the project")
+}
+test_input = prepare_sentence_tokens(hf_bert_model,
+                                     "this project is very interesting")
+
+
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_bert_model), trace_input))
+def BertSequenceClassification_basic(module, tu: TestUtils):
+    module.forward(test_input)
+
+
+# ==============================================================================
+
+hf_bigbird_model = "google/bigbird-roberta-base"
+
+trace_input = {
+    "forward":
+    prepare_sentence_tokens(hf_bigbird_model, "how do you like the project")
+}
+test_input = prepare_sentence_tokens(hf_bigbird_model,
+                                     "this project is very interesting")
+
+
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_bigbird_model), trace_input))
+def BigBirdSequenceClassification_basic(module, tu: TestUtils):
+    module.forward(test_input)
+
+
+# ==============================================================================
+
+hf_distilbert_model = "distilbert-base-uncased"
+
+trace_input = {
+    "forward":
+    prepare_sentence_tokens(hf_distilbert_model, "how do you like the project")
+}
+test_input = prepare_sentence_tokens(hf_distilbert_model,
+                                     "this project is very interesting")
+
+
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_distilbert_model), trace_input))
+def DistilBertSequenceClassification_basic(module, tu: TestUtils):
+    module.forward(test_input)
+
+
+# ==============================================================================
+
+hf_gpt2_model = "gpt2"
+
+trace_input = {
+    "forward":
+    prepare_sentence_tokens(hf_gpt2_model, "how do you like the project")
+}
+test_input = prepare_sentence_tokens(hf_gpt2_model,
+                                     "this project is very interesting")
+
+
+@register_test_case(module_factory=lambda: getTracedRecursiveScriptModule(
+    HfSequenceClassification(hf_gpt2_model), trace_input))
+def GPT2SequenceClassification_basic(module, tu: TestUtils):
+    module.forward(test_input)
