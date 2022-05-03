6ab5f01fb899ab99275a827e20584222ed81a26d
Jae Hoon (Antonio) Kim <17433012+antoniojkim@users.noreply.github.com>
2022-05-03 09:35:44 -0400

    Fix LTC Decoupling (#815)
    * Initial changes
    
    * Fix up native functions
    
    * Further fix decoupling
    
    * Remove unnecessary ops
    
    * Formatting and copyright banners:
    
    * Add pytorch submodule

--- python/torch_mlir/csrc/base_lazy_backend/LazyShapeInference.cpp
+++ python/torch_mlir/csrc/base_lazy_backend/LazyShapeInference.cpp
@@ -13,5 +13,25 @@
 namespace torch {
 namespace lazy {
 
+std::vector<Shape> compute_shape_native_batch_norm(
+    const at::Tensor& input, const c10::optional<at::Tensor>& weight,
+    const c10::optional<at::Tensor>& bias,
+    const c10::optional<at::Tensor>& running_mean,
+    const c10::optional<at::Tensor>& running_var, bool training,
+    double momentum, double eps) {
+  std::vector<Shape> shapes;
+  shapes.reserve(3);
+  shapes.emplace_back(input.scalar_type(), input.sizes().vec());
+  if (running_mean.has_value()) {
+    shapes.emplace_back(
+        running_mean.value().scalar_type(), running_mean.value().sizes().vec());
+    if (running_var.has_value()) {
+      shapes.emplace_back(
+          running_var.value().scalar_type(), running_var.value().sizes().vec());
+    }
+  }
+  return shapes;
+}
+
 } // namespace lazy
 } // namespace torch
