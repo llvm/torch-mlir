83fb59bf1102a6b4742b15aa4cbe0261c941db47
Jae Hoon (Antonio) Kim <17433012+antoniojkim@users.noreply.github.com>
2022-05-03 09:35:44 -0400

    Fix LTC Decoupling (#815)
    * Initial changes
    
    * Fix up native functions
    
    * Further fix decoupling
    
    * Remove unnecessary ops
    
    * Formatting and copyright banners:
    
    * Add pytorch submodule

---
+++ python/torch_mlir/csrc/base_lazy_backend/ops/generic.cpp
@@ -0,0 +1,28 @@
+//===- generic.cpp --------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+// This file is adapted from pytorch/pytorch
+// https://github.com/pytorch/pytorch/blob/master/torch/csrc/lazy/ts_backend/generic.cpp
+//===----------------------------------------------------------------------===//
+
+#include "generic.h"
+
+namespace torch {
+namespace lazy {
+
+Generic::Generic(
+    OpKind op,
+    OpList operands,
+    Shape shape,
+    size_t num_outputs,
+    hash_t hash_seed)
+    : TorchMlirNode(op, operands, {std::move(shape)}, num_outputs, hash_seed),
+      hash_seed_(hash_seed) {}
+
+} // namespace lazy
+} // namespace torch
