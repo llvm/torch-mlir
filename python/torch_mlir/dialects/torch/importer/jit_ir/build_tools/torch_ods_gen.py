b20679e1b80524d56b217e5feb79cd81f444f24e
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2022-05-05 23:29:25 +0530

    [MLIR][TORCH] Modify aten::dropout op description
    Signed-Off By: Vivek Khandelwal vivek@nod-labs.com

--- python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
+++ python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
@@ -443,7 +443,7 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
     emit("aten::tensor.float : (float, int?, Device?, bool) -> (Tensor)")
     emit("aten::Int.Tensor : (Tensor) -> (int)", has_folder=True)
     emit("aten::Float.Tensor : (Tensor) -> (float)", has_folder=True)
-    emit("aten::dropout : (Tensor, float, bool) -> (Tensor)")
+    emit_with_mutating_variants("aten::dropout : (Tensor, float, bool) -> (Tensor)")
     emit("aten::t : (Tensor) -> (Tensor)")
     emit("aten::full : (int[], Scalar, int?, int?, Device?, bool?) -> (Tensor)")
     emit("aten::full_like : (Tensor, Scalar, int?, int?, Device?, bool?, int?) -> (Tensor)")
