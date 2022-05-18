074be5d81028c30aaad417ceb8c8d3aab2867634
Prashant Kumar <prashant@nod-labs.com>
2022-05-18 11:48:51 +0000

    Add simple neural_net and bert_training scripts.
    1. With the help of `make_fx` we are able to get the full training graph
       with weight updates.
    2. NeuralNet_training passes. Bert_training passes after cherry-picking
       https://github.com/llvm/torch-mlir/pull/844.
    3. TODO: Remove the functorch's dependency after make_fx moves to
       pytorch core.

--- build_tools/torchscript_e2e_heavydep_tests/generate_serialized_tests.sh
+++ build_tools/torchscript_e2e_heavydep_tests/generate_serialized_tests.sh
@@ -27,7 +27,7 @@ python3 -m pip install --upgrade -r "$torch_mlir_src_root/requirements.txt"
 # For minilm_seq_classification.py
 python3 -m pip install 'transformers[torch]'
 
-# For functorch dependent models.
+# TODO: Remove functorch after make_fx makes into pytorch core.
 python3 -m pip install "git+https://github.com/pytorch/functorch.git"
 python3 -m pip install networkx
 
@@ -37,6 +37,5 @@ python3 -m pip install timm
 cd "$torch_mlir_src_root"
 export PYTHONPATH=${PYTHONPATH-}
 source "$torch_mlir_src_root/.env"
-# For the functorch utils.py
-export PYTHONPATH="$torch_mlir_src_root/build_tools/torchscript_e2e_heavydep_tests:$PYTHONPATH"
+
 python3 -m build_tools.torchscript_e2e_heavydep_tests.main --output_dir=$serialized_test_dir
