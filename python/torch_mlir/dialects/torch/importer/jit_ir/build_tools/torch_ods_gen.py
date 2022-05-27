513f93a6935479497017854326a55617fbc5e255
Henry Tu <henry.tu@cerebras.net>
2022-05-27 14:14:49 -0400

    Added support for native_layer_norm_backward

--- python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
+++ python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
@@ -535,6 +535,7 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
     emit("aten::tanh_backward : (Tensor, Tensor) -> (Tensor)")
     emit("aten::gelu_backward : (Tensor, Tensor, str) -> (Tensor)")
     emit("aten::_log_softmax_backward_data : (Tensor, Tensor, int, int) -> (Tensor)")
+    emit("aten::native_layer_norm_backward : (Tensor, Tensor, int[], Tensor, Tensor, Tensor?, Tensor?, bool[]) -> (Tensor, Tensor, Tensor)")
 
     # ==========================================================================
     # `prim::` namespace.
