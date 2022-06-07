8504fd7f9649f60dfa5efbbda2c2eea89a0963e5
Jae Hoon (Antonio) Kim <17433012+antoniojkim@users.noreply.github.com>
2022-06-07 14:38:50 -0400

    E2E HuggingFace Bert using LTC Backend (#912)
    * Update native function definitions
    
    * Add ops to support bert lowering
    
    - Add empty_strided and as_strided
    
    - Restore zeros_like to op blacklist (Without this, tensors will be unintentionally created with a CPU device rather than lazy)
    
    - Check for composite implicit ops and add device data IR
    
    - Also fix codegen for functionalization
    
    * Add autogen to CMakeList
    
    * Remove PyTorch submodule
    
    * Reduced BERT model size
    
    * Print Mark Step status in Torch MLIR LTC debug string
    
    * Apply fixes to work with latest upstream/main
    
    - Pass importOptions into getMlirTypeFromTorchType during NodeImporter::importNode
    
      Without this, the tensor type created may have a mismatched type as ImportOptions may cause vtensor to be used instead of tensor
    
    * Update shape inference functions
    
    - Fixed compute_shape_native_batch_norm when mean and var are uninitialized
    
      Previously, the number of shapes returned would be <3 if either mean or val was didn't exist. Instead, we now initialize them with a vector matching the number of channels.
    
    - Implemented compute_shape_mul
    
    - Fixed bug in reshape shape inference error message
    
    * Get MLIR backend more consistent with TS backend
    
    - Remove LazyNativeFunctions::_unsafe_view from autogen
    
    - Blacklist ops to make JIT graph more like output of TS backend
    
    - Print graph when SSA value has mismatch of types and results
    
    - Remove normalize_index from LazyShapeInference
    
    - Fix seeds for LTC example models
    
    * Update and clean up shape inference functions
    
    - Prune shape inference functions
    
    - Add shape inference function for GenerateSlice
    
    - Add shape inference function for GenerateCopy
    
    Co-authored-by: Henry Tu <henry.tu@cerebras.net>

--- python/torch_mlir/csrc/base_lazy_backend/mlir_node_lowering.cpp
+++ python/torch_mlir/csrc/base_lazy_backend/mlir_node_lowering.cpp
@@ -14,6 +14,7 @@
 #include "generated/LazyNonNativeIr.h"
 #include "mlir_lowering_context.h"
 #include "mlir_node.h"
+#include "ops/device_data.h"
 
 #include <ATen/Functions.h>
 #include <c10/core/ScalarType.h>
@@ -61,8 +62,8 @@ TorchMlirOpVector LowerTorchMlirBuiltin(
   for (jit::Value* value : results) {
     if (value->type()->kind() == c10::TypeKind::TensorType) {
       TORCH_CHECK(
-          tensor_type_idx < tensor_types.size(),
-          "Tensor corresponding to JIT SSA value %", value->debugName(),
+          tensor_type_idx < tensor_types.size(), function->graph()->toString(),
+          "\nTensor corresponding to JIT SSA value %", value->debugName(),
           " corresponds to result #", tensor_type_idx, ", but we only have ",
           tensor_types.size(), " known types!");
 
@@ -98,6 +99,79 @@ TorchMlirOpVector LowerTorchMlirBuiltin(
 
   return LowerTorchMlirBuiltin(
       function, sym, tensor_types, arguments, kwarguments);
+}
+
+c10::TensorType& cast_tensor_type(c10::TypePtr value_type) {
+  auto tensor_type = value_type->cast<c10::TensorType>();
+  TORCH_CHECK(tensor_type, "Unable to cast Value type to TensorType!");
+
+  return *tensor_type.get();
+}
+
+c10::optional<std::vector<int64_t>>
+get_tensor_type_shape(c10::TensorType& tensor_type) {
+  auto& symbolic_shape = tensor_type.symbolic_sizes();
+  if (!symbolic_shape.rank()) {
+    return c10::nullopt;
+  }
+
+  // Get current tensor shape.
+  std::vector<int64_t> dims;
+  dims.resize(*symbolic_shape.rank());
+  for (size_t i = 0; i < dims.size(); ++i) {
+    auto shape_symbol = symbolic_shape[i];
+    dims[i] = shape_symbol.is_static() ? shape_symbol.static_size() : -1;
+  }
+
+  return dims;
+}
+
+std::vector<torch::lazy::Shape> compute_shape_copy(c10::TypePtr value_type) {
+  c10::TensorType& tensor_type = cast_tensor_type(value_type);
+
+  auto maybe_dims = get_tensor_type_shape(tensor_type);
+  TORCH_CHECK(maybe_dims.has_value(), "Cannot copy unranked tensor!");
+
+  auto scalar_type = tensor_type.scalarType();
+  TORCH_CHECK(
+      scalar_type.has_value(), "Unable to copy due to lack of scalar type!");
+  return {Shape(scalar_type.value(), maybe_dims.value())};
+}
+
+std::vector<torch::lazy::Shape> compute_shape_slice(
+    c10::TypePtr value_type, int64_t dim, int64_t start, int64_t end,
+    int64_t step) {
+  c10::TensorType& tensor_type = cast_tensor_type(value_type);
+
+  auto maybe_dims = get_tensor_type_shape(tensor_type);
+  TORCH_CHECK(maybe_dims.has_value(), "Cannot slice unranked tensor!");
+
+  std::vector<int64_t> dims = maybe_dims.value();
+  int64_t num_dims = dims[dim];
+
+  // Index may be negative, so we must normalize it.
+  auto normalize_index = [](int64_t index, unsigned num_dims) {
+    return index < 0 ? (int64_t)num_dims + index : index;
+  };
+  start = normalize_index(start, num_dims);
+  end = normalize_index(end, num_dims);
+
+  if (start >= end || start >= num_dims || end <= 0) {
+    // Slice is out of bounds, nothing in range.
+    dims[dim] = 0;
+  } else {
+    // Clamp upper and lower bound to valid indices.
+    start = std::max((int64_t)0, start);
+    end = std::min(num_dims, end);
+
+    // Final size is determined by step and interval size.
+    dims[dim] = std::ceil((double)(end - start) / (double)step);
+  }
+
+  auto scalar_type = tensor_type.scalarType();
+  TORCH_CHECK(
+      scalar_type.has_value(), "Unable to slice due to lack of scalar type!");
+  return {Shape(scalar_type.value(), dims)};
 }
 
 class TorchMlirNodeLowering : public TorchMlirNodeLoweringInterface {
@@ -213,14 +287,14 @@ public:
       const torch::lazy::DeviceData* device_data_node =
           torch::lazy::NodeCast<torch::lazy::DeviceData>(
               node, *torch::lazy::ltc_device_data);
-      auto infoptr = device_data_node->data->info();
+      auto infoptr = device_data_node->data()->info();
       auto deviceDataInfoPtr =
           (torch::lazy::LazyGraphExecutor::DeviceDataInfo*)infoptr;
       if (GRAPH_DUMP_ENABLED) {
         LOG(ERROR) << "Lowering device data node, tensor id "
                    << deviceDataInfoPtr->tensor_id << std::endl;
       }
-      return {loctx()->GetParameter(device_data_node->data)};
+      return {loctx()->GetParameter(device_data_node->data())};
     }
 
     std::vector<torch::jit::NamedValue> arguments;
@@ -421,8 +495,8 @@ public:
     arguments.emplace_back(destination);
     arguments.emplace_back(source);
     LowerBuiltin(
-        at::aten::copy_, c10::ArrayRef<Shape>({/*shape goes here*/}),
-        arguments);
+        at::aten::copy_,
+        c10::ArrayRef<Shape>(compute_shape_copy(source->type())), arguments);
   }
 
   torch::jit::Value* GenerateSlice(
@@ -434,8 +508,11 @@ public:
     arguments.emplace_back(start);
     arguments.emplace_back(end);
     arguments.emplace_back(step);
+
     TorchMlirOpVector selected = LowerBuiltin(
-        at::aten::slice, c10::ArrayRef<Shape>({/*shape goes here*/}),
+        at::aten::slice,
+        c10::ArrayRef<Shape>(
+            compute_shape_slice(base->type(), dim, start, end, step)),
         arguments);
     CHECK_EQ(selected.size(), 1);
     return selected.front();
