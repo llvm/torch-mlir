923a9fe8b13f46993b9cd44ac8c7bd4ee46ce775
Henry Tu <henry.tu@cerebras.net>
2022-06-09 11:57:37 -0400

    Added LTC option to torchscript e2e

---
+++ python/torch_mlir_e2e_test/torchscript/configs/lazy_tensor_core.py
@@ -0,0 +1,23 @@
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Also available under a BSD-style license. See LICENSE.
+
+import torch
+
+from torch_mlir_e2e_test.torchscript.framework import TestConfig, Trace
+
+
+class LazyTensorCoreTestConfig(TestConfig):
+    """TestConfig that runs torch.nn.Module thru the Lazy Tensor Core frontend for Torch MLIR"""
+
+    def __init__(self):
+        super().__init__()
+
+    def compile(self, program: torch.nn.Module) -> torch.nn.Module:
+        return program
+
+    def run(self, artifact: torch.nn.Module, trace: Trace) -> Trace:
+        result: Trace = []
+
+        return result
