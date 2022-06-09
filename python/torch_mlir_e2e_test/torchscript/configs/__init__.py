00578e2cc0cd9c68fa880c5b7b2dd4cc17373c0a
Henry Tu <henry.tu@cerebras.net>
2022-06-09 15:56:01 -0400

    Added e2e LTC tests  (#916)
    * Added e2e LTC Torch MLIR tests
    
    * Fix seed for reproducability
    
    * Check if computation is None before getting debug string
    
    * Updated unit tests, and added numeric tests
    
    * Print name of the model layer that fails numeric validation
    
    * Run LTC e2e test with CI/CD
    
    * Set seed in main function, instead of beginning of execution
    
    * Add comment to specify number of digits of precision
    
    * Fixed typo
    
    * Remove tests for LTC example models
    
    * Added LTC option to torchscript e2e
    
    * Implement compile and run for LTC e2e test
    
    * xfail all tests that use ops that aren't currently supported

--- python/torch_mlir_e2e_test/torchscript/configs/__init__.py
+++ python/torch_mlir_e2e_test/torchscript/configs/__init__.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # Also available under a BSD-style license. See LICENSE.
 
+from .lazy_tensor_core import LazyTensorCoreTestConfig
 from .linalg_on_tensors_backend import LinalgOnTensorsBackendTestConfig
 from .native_torch import NativeTorchTestConfig
 from .torchscript import TorchScriptTestConfig
