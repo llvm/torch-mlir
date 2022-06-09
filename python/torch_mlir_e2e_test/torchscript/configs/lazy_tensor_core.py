890979c9529cd6fd3feeafe6d4260fab441d9076
Henry Tu <henry.tu@cerebras.net>
2022-06-09 15:00:36 -0400

    Implement compile and run for LTC e2e test

--- python/torch_mlir_e2e_test/torchscript/configs/lazy_tensor_core.py
+++ python/torch_mlir_e2e_test/torchscript/configs/lazy_tensor_core.py
@@ -3,9 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # Also available under a BSD-style license. See LICENSE.
 
+import ltc_backend.ltc_backend._EXAMPLE_MLIR_BACKEND as ltc_backend
 import torch
-
-from torch_mlir_e2e_test.torchscript.framework import TestConfig, Trace
+from torch_mlir_e2e_test.torchscript.framework import TestConfig, Trace, TraceItem
 
 
 class LazyTensorCoreTestConfig(TestConfig):
@@ -13,11 +13,22 @@ class LazyTensorCoreTestConfig(TestConfig):
 
     def __init__(self):
         super().__init__()
+        ltc_backend._initialize()
 
     def compile(self, program: torch.nn.Module) -> torch.nn.Module:
-        return program
+        return program.to('lazy')
 
     def run(self, artifact: torch.nn.Module, trace: Trace) -> Trace:
         result: Trace = []
+
+        for item in trace:
+            # We need to move all the inputs to the lazy device before running in LTC.
+            lazy_inputs = [arg.to('lazy') for arg in item.inputs]
+            output = getattr(artifact, item.symbol)(*lazy_inputs)
+
+            result.append(
+                TraceItem(symbol=item.symbol,
+                          inputs=item.inputs,
+                          output=output.to('cpu')))
 
         return result
