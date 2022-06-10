6615add806eaafcfe61478049e3b3998290abbda
powderluv <powderluv@users.noreply.github.com>
2022-06-10 11:57:58 -0700

    Fix the new header location (#926)
    Seems to have moved in the latest nightly

--- python/torch_mlir/dialects/torch/importer/jit_ir/csrc/ivalue_importer.cpp
+++ python/torch_mlir/dialects/torch/importer/jit_ir/csrc/ivalue_importer.cpp
@@ -21,7 +21,7 @@
 #include "mlir-c/Diagnostics.h"
 #include "torch-mlir-c/TorchTypes.h"
 
-#include "ATen/native/quantized/packed_params.h"
+#include "ATen/native/ao_sparse/quantized/cpu/packed_params.h"
 #include "caffe2/core/scope_guard.h"
 
 using namespace torch_mlir;
