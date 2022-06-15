b90837ee243b086bd487119da4e140b2df397498
Bob Adolf <rdadolf@gmail.com>
2022-06-14 18:24:40 -0700

    Temporarily revert support for custom op extensions. (#944)
    The MacOS builders are having linking trouble with the extension library.
    Until it's fixed, all support for op extensions is disabled. It should be
    easy to restore once the issue is resolved.

--- build_tools/update_torch_ods.sh
+++ build_tools/update_torch_ods.sh
@@ -17,16 +17,19 @@ torch_ir_include_dir="${src_dir}/include/torch-mlir/Dialect/Torch/IR"
 python_packages_dir="${build_dir}/tools/torch-mlir/python_packages"
 
 pypath="${python_packages_dir}/torch_mlir"
-if [ ! -z ${TORCH_MLIR_EXT_PYTHONPATH} ]; then
-  pypath="${pypath}:${TORCH_MLIR_EXT_PYTHONPATH}"
-fi
-ext_module="torch_mlir._torch_mlir_custom_op_example"
-if [ ! -z ${TORCH_MLIR_EXT_MODULES} ]; then
-  ext_module="${ext_module},${TORCH_MLIR_EXT_MODULES}"
-fi
+# TODO: Re-enable once custom op support is back.
+#if [ ! -z ${TORCH_MLIR_EXT_PYTHONPATH} ]; then
+#  pypath="${pypath}:${TORCH_MLIR_EXT_PYTHONPATH}"
+#fi
+#ext_module="torch_mlir._torch_mlir_custom_op_example"
+#if [ ! -z ${TORCH_MLIR_EXT_MODULES} ]; then
+#  ext_module="${ext_module},${TORCH_MLIR_EXT_MODULES}"
+#fi
 
 PYTHONPATH="${pypath}" python \
   -m torch_mlir.dialects.torch.importer.jit_ir.build_tools.torch_ods_gen \
   --torch_ir_include_dir="${torch_ir_include_dir}" \
-  --pytorch_op_extensions="${ext_module}" \
   --debug_registry_dump="${torch_ir_include_dir}/JITOperatorRegistryDump.txt"
+
+# TODO: Add back to torch_ods_gen invocation once custom op support is back.
+#  --pytorch_op_extensions="${ext_module}" \
