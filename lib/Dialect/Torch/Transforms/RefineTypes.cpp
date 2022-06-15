4d67a0f59594126322f60ac3a353844e7630ad8f
Bob Adolf <rdadolf@gmail.com>
2022-06-14 18:24:40 -0700

    Temporarily revert support for custom op extensions. (#944)
    The MacOS builders are having linking trouble with the extension library.
    Until it's fixed, all support for op extensions is disabled. It should be
    easy to restore once the issue is resolved.

--- lib/Dialect/Torch/Transforms/RefineTypes.cpp
+++ lib/Dialect/Torch/Transforms/RefineTypes.cpp
@@ -642,7 +642,7 @@ ChangeResult TypeAnalyzer::visitOperation(
           AtenZero_Op, AtenIndexTensorOp, ValsemVariantAtenIndexPutImplOp,
           AtenIndexPutOp, ValsemVariantAtenCopyOp, AtenZeroFunctionalOp,
           AtenIndexPutHackedTwinOp, AtenMaskedFillScalarOp, AtenFlipOp,
-          PrimAbsScalarOp, AtenNumpyTOp>(op)) {
+          PrimAbsScalarOp>(op)) {
     return incorporateKnowledge(op->getResult(0), operands[0]->getValue());
   }
 
