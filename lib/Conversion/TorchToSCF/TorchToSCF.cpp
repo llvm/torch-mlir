829717c96eb09e6393813950532c71c2789718cc
Maksim Levental <maksim.levental@gmail.com>
2022-06-22 22:23:46 -0500

    Bump LLVM (#958)

--- lib/Conversion/TorchToSCF/TorchToSCF.cpp
+++ lib/Conversion/TorchToSCF/TorchToSCF.cpp
@@ -11,7 +11,7 @@
 
 #include "../PassDetail.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
-#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "torch-mlir/Dialect/Torch/IR/TorchDialect.h"
