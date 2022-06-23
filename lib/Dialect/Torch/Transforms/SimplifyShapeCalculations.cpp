a495be1905237df77bb5e0e6f59534348d9cb070
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2022-06-23 20:48:58 +0000

    Add basic support for list of optional tensors in reduce-op-variants
    This commit adds support for lists of type `list<optional<tensor>>`
    where each element in the list is either a `!torch.tensor` or a
    `!torch.none`.

--- lib/Dialect/Torch/Transforms/SimplifyShapeCalculations.cpp
+++ lib/Dialect/Torch/Transforms/SimplifyShapeCalculations.cpp
@@ -269,6 +269,22 @@ public:
 };
 } // namespace
 
+namespace {
+class FoldPrimUncheckedCastOp : public OpRewritePattern<PrimUncheckedCastOp> {
+public:
+  using OpRewritePattern::OpRewritePattern;
+  LogicalResult matchAndRewrite(PrimUncheckedCastOp op,
+                                PatternRewriter &rewriter) const override {
+    if (!isValidSubtype(op.x().getType(), op.result().getType())) {
+      return rewriter.notifyMatchFailure(
+          op, "input tensor type is not a valid subtype of result type");
+    }
+    rewriter.replaceOp(op, op.x());
+    return success();
+  }
+};
+} // namespace
+
 static void refineShapeCalculateResult(ShapeCalculateOp op, int resultNum,
                                        PatternRewriter &rewriter,
                                        bool &madeChange) {
@@ -396,6 +412,7 @@ class SimplifyShapeCalculationsPass
     patterns.insert<AbstractlyInterpretListOpsWithinABlock>(context);
     patterns.insert<DecomposeAtenSizeOp>(context);
     patterns.insert<RefineShapeCalculateOp>(context);
+    patterns.insert<FoldPrimUncheckedCastOp>(context);
 
     PrimIfOp::getCanonicalizationPatterns(patterns, context);
     Aten__Getitem__TOp::getCanonicalizationPatterns(patterns, context);
