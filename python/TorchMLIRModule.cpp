27d8882258d168eb03e79025a6d1abcea7c0ee39
Ashay Rane <ashay@users.noreply.github.com>
2022-07-21 09:14:44 -0700

    Revert "python: trim registration and loading of dialects and passes"
    This reverts commit ad283c104339267fbe11cc7efefc02a6ad4d2d1e, since it's
    causing nightly build failures for all platforms.

--- python/TorchMLIRModule.cpp
+++ python/TorchMLIRModule.cpp
@@ -9,7 +9,6 @@
 
 #include "mlir-c/Bindings/Python/Interop.h"
 #include "mlir/Bindings/Python/PybindAdaptors.h"
-#include "mlir/CAPI/IR.h"
 #include "torch-mlir-c/Dialects.h"
 #include "torch-mlir-c/Registration.h"
 
@@ -20,6 +19,14 @@ PYBIND11_MODULE(_torchMlir, m) {
 
   m.doc() = "torch-mlir main python extension";
 
-  m.def("register_required_dialects", torchMlirRegisterRequiredDialects,
-        py::arg("context"));
+  m.def(
+      "register_dialect",
+      [](MlirContext context, bool load) {
+        MlirDialectHandle handle = mlirGetDialectHandle__torch__();
+        mlirDialectHandleRegisterDialect(handle, context);
+        if (load) {
+          mlirDialectHandleLoadDialect(handle, context);
+        }
+      },
+      py::arg("context"), py::arg("load") = true);
 }
--- python/torch_mlir/_mlir_libs/_site_initialize_0.py
+++
@@ -1,4 +0,0 @@
-from . import _torchMlir
-
-def context_init_hook(context):
-    _torchMlir.register_required_dialects(context)
