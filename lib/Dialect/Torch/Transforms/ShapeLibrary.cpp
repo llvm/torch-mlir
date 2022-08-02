38d8498b21d0892c2c7196aeaab64aeec6852a62
Quinn Dawkins <quinn@nod-labs.com>
2022-08-02 11:39:41 -0400

    add e2e support for aten.atan2 (#1117)
     - Includes math-to-libm pass in refbackend for math::atan2 support

--- lib/Dialect/Torch/Transforms/ShapeLibrary.cpp
+++ lib/Dialect/Torch/Transforms/ShapeLibrary.cpp
@@ -6248,6 +6248,10 @@ module {
     %0 = call @__torch__.torch.jit._shape_functions.broadcast(%arg0, %arg1) : (!torch.list<int>, !torch.list<int>) -> !torch.list<int>
     return %0 : !torch.list<int>
   }
+  func.func @"__torch_mlir_shape_fn.aten.atan2"(%arg0: !torch.list<int>, %arg1: !torch.list<int>) -> !torch.list<int> {
+    %0 = call @__torch__.torch.jit._shape_functions.broadcast(%arg0, %arg1) : (!torch.list<int>, !torch.list<int>) -> !torch.list<int>
+    return %0 : !torch.list<int>
+  }
   func.func @"__torch_mlir_shape_fn.aten.__and__.Tensor"(%arg0: !torch.list<int>, %arg1: !torch.list<int>) -> !torch.list<int> {
     %0 = call @__torch__.torch.jit._shape_functions.broadcast(%arg0, %arg1) : (!torch.list<int>, !torch.list<int>) -> !torch.list<int>
     return %0 : !torch.list<int>
