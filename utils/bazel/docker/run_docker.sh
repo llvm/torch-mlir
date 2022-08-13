4c4b2a72a25ef85eb549abfce263ec16c4f4735b
Sambhav Jain <sambhav.jain@getcruise.com>
2022-08-12 18:56:22 -0700

    Dockerize and Cache Bazel CI and local builds

---
+++ utils/bazel/docker/run_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker build -f utils/bazel/docker/Dockerfile \
+             -t torch-mlir:dev \
+             .
+
+docker run -it \
+           -v "$(pwd)":"/opt/src/torch-mlir" \
+           -v "${HOME}/.cache/bazel":"/root/.cache/bazel" \
+           torch-mlir:dev
