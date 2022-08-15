c6f663d6041b9638989a24a862e8bef188a3183f
Sean Silva <silvasean@google.com>
2022-08-15 21:32:42 +0000

    more debugging

--- lib/Dialect/Torch/Transforms/RefineTypes.cpp
+++ lib/Dialect/Torch/Transforms/RefineTypes.cpp
@@ -1626,6 +1626,7 @@ namespace {
 class RefineTypesPass : public RefineTypesBase<RefineTypesPass> {
   void runOnOperation() override {
     auto func = getOperation();
+    func.dump();
     DataFlowSolver solver;
     solver.load<dataflow::DeadCodeAnalysis>();
     solver.load<dataflow::SparseConstantPropagation>();
