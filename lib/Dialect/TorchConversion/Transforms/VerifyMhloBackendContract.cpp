08832f373ff31243090c8fd10eec274f5c62a88f
TanyoKwok <tianyou.gty@alibaba-inc.com>
2022-08-31 16:42:10 +0800

    [MHLO] add VerifyMhloBackendContract

---
+++ lib/Dialect/TorchConversion/Transforms/VerifyMhloBackendContract.cpp
@@ -0,0 +1,72 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "PassDetail.h"
+
+#include "mlir-hlo/Dialect/mhlo/IR/hlo_ops.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "stablehlo/dialect/ChloOps.h"
+#include "torch-mlir/Dialect/TorchConversion/Transforms/Passes.h"
+
+using namespace mlir;
+using namespace mlir::torch;
+using namespace mlir::torch::TorchConversion;
+
+namespace {
+class VerifyMhloBackendContractPass
+    : public VerifyMhloBackendContractBase<VerifyMhloBackendContractPass> {
+  void runOnOperation() override {
+    MLIRContext *context = &getContext();
+    auto module = getOperation();
+    TypeConverter converter;
+    converter.addConversion([](Type type) -> Type {
+      auto elemTy = type;
+      if (isa<TensorType>(type)) {
+        elemTy = type.cast<TensorType>().getElementType();
+      }
+      if (BaseMemRefType::isValidElementType(elemTy))
+        return type;
+      return nullptr;
+    });
+
+    auto opHasLegalTypes = [&](Operation *op) { return converter.isLegal(op); };
+
+    ConversionTarget target(*context);
+
+    // Structural operations.
+    target.addDynamicallyLegalOp<ModuleOp, func::FuncOp, func::ReturnOp>(
+        opHasLegalTypes);
+    // Basic scalar operations.
+    target.addLegalDialect<mhlo::MhloDialect>();
+    target.addLegalDialect<chlo::ChloDialect>();
+    target.addLegalDialect<tensor::TensorDialect>();
+    target.addLegalDialect<arith::ArithmeticDialect>();
+
+    RewritePatternSet patterns(context);
+    if (failed(applyFullConversion(module, target, std::move(patterns)))) {
+      // We avoid `module.emitError()` so that mlir-print-op-on-diagnostics
+      // doesn't unnecessarily spew out the entire module.
+      emitError(module.getLoc())
+          << "Module does not conform to the MHLO backend contract. "
+             "See dialect conversion legality information above.";
+      return signalPassFailure();
+    }
+  }
+};
+} // namespace
+
+std::unique_ptr<OperationPass<ModuleOp>>
+mlir::torch::TorchConversion::createVerifyMhloBackendContractPass() {
+  return std::make_unique<VerifyMhloBackendContractPass>();
+}
