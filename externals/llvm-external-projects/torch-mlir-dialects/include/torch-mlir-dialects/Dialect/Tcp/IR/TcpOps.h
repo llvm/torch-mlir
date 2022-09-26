0ca9801ed021e68c73722ffa79a78b11c35a9a92
Raghavan Raman <raghavan.raman@getcruise.com>
2022-09-26 11:07:14 -0700

    [Tcp] Add boilerplate for TCP dialect (#1375)
    * Initial boilerplate for TCP with a dummy op.
    
    * Conditional flag to enable TCP

---
+++ externals/llvm-external-projects/torch-mlir-dialects/include/torch-mlir-dialects/Dialect/Tcp/IR/TcpOps.h
@@ -0,0 +1,21 @@
+//===------------------------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TORCH_MLIR_DIALECTS_DIALECT_TCP_IR_TCPOPS_H_
+#define TORCH_MLIR_DIALECTS_DIALECT_TCP_IR_TCPOPS_H_
+
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+
+#define GET_OP_CLASSES
+#include "torch-mlir-dialects/Dialect/Tcp/IR/TcpOps.h.inc"
+
+#endif // TORCH_MLIR_DIALECTS_DIALECT_TCP_IR_TCPOPS_H_
