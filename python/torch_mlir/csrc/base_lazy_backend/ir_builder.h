0701464c47983b0e72bccf45efc1c46f618a6171
Jae Hoon (Antonio) Kim <17433012+antoniojkim@users.noreply.github.com>
2022-10-30 21:42:44 -0400

    Remove view ops from IR builder (#1534)
    * Remove view ops from IR builder
    
    * Update PyTorch requirements

--- python/torch_mlir/csrc/base_lazy_backend/ir_builder.h
+++ python/torch_mlir/csrc/base_lazy_backend/ir_builder.h
@@ -39,20 +39,6 @@ struct TorchMlirIrBuilder : IrBuilder {
   NodePtr MakeTensorList(const OpList& inputs) const override { return MakeNode<TorchMlirTensorList>(inputs); }
   NodePtr MakeGeneric(const OpKind& op, const OpList& operands, const Shape& shape, const size_t& num_outputs = 1, const hash_t& hash_seed = static_cast<uint32_t>(0x5a2d296e9)) const override { return MakeNode<Generic>(op, operands, shape, num_outputs, hash_seed); }
 
-  // view ops
-  NodePtr MakeAsStridedViewUpdate(const Value& input0, const Value& input1, const std::vector<int64_t>& size, const std::vector<int64_t>& stride, const int64_t& storage_offset) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeAsStrided(const Value& input0, const std::vector<int64_t>& size, const std::vector<int64_t>& stride, const int64_t& storage_offset) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeDiagonalViewUpdate(const Value& input0, const Value& input1, const int64_t& offset, const int64_t& dim1, const int64_t& dim2) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeDiagonal(const Value& input0, const int64_t& offset, const int64_t& dim1, const int64_t& dim2) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeNarrowViewUpdate(const Value& input0, const Value& input1, const std::vector<int64_t>& base_indices) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeNarrow(const Value& input0, const std::vector<int64_t>& base_indices, const std::vector<int64_t>& sizes) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakePermute(const Value& input0, const std::vector<int64_t>& dims) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeResize(const Value& input0, const std::vector<int64_t>& size) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeSelectViewUpdate(const Value& input0, const Value& input1, const int64_t& dim, const int64_t& start, const int64_t& end, const int64_t& stride) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeSelect(const Value& input0, const int64_t& dim, const int64_t& start, const int64_t& end, const int64_t& stride) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeSqueeze(const Value& input0, const int& dim) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-  NodePtr MakeUnsqueeze(const Value& input0, const int& dim) const override { UNIMPLEMENTED_FUNCTION_ERROR(); }
-
   // dynamic ir nodes
   NodePtr MakeSizeNode(const Value& input, size_t dim) const override { return MakeNode<SizeNode>(input, dim); }
   NodePtr MakeSizeAdd(const Value& a, const Value& b) const override { return MakeNode<SizeAdd>(a, b); }
