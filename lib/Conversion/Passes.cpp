10f2c0223e99c218cecd757d0795e2dfe6b523be
Raghavan Raman <raghavan.raman@getcruise.com>
2022-11-06 22:42:27 -0800

    [Tcp] Enable Tcp to Linalg conversion pass when TORCH_MLIR_ENABLE_TCP is set

--- lib/Conversion/Passes.cpp
+++ lib/Conversion/Passes.cpp
@@ -21,6 +21,7 @@
 #include "torch-mlir/Conversion/TorchToTMTensor/TorchToTMTensor.h"
 #include "torch-mlir/Conversion/TorchConversionToMLProgram/TorchConversionToMLProgram.h"
 #ifdef TORCH_MLIR_ENABLE_TCP
+#include "torch-mlir-dialects/Conversion/TcpToLinalg/TcpToLinalg.h"
 #include "torch-mlir/Conversion/TorchToTcp/TorchToTcp.h"
 #endif // TORCH_MLIR_ENABLE_TCP
 
@@ -43,4 +44,9 @@ void mlir::torch::registerConversionPasses() {
     return mlir::createSymbolicShapeOptimizationPass();
   });
 #endif // TORCH_MLIR_ENABLE_MHLO
+#if TORCH_MLIR_ENABLE_TCP
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return mlir::tcp::createConvertTcpToLinalgPass();
+  });
+#endif // TORCH_MLIR_ENABLE_TCP
 }
