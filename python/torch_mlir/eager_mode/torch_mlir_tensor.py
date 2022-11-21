5cece7eb31386750a289a90f41765dbde37d9e12
max <maksim.levental@gmail.com>
2022-11-21 15:21:32 -0600

    Fix https://github.com/llvm/torch-mlir/issues/1618 by stripping `requires_grad` from results of view ops.

--- python/torch_mlir/eager_mode/torch_mlir_tensor.py
+++ python/torch_mlir/eager_mode/torch_mlir_tensor.py
@@ -131,6 +131,8 @@ class TorchMLIRTensor(torch.Tensor):
                 if UNSUPPORTED_OPS.match(op_name):
                     raise UnsupportedByTorchMlirEagerMode(op_name)
 
+                requires_grad = requires_grad and "view" not in op_name
+
                 if not hasattr(func, "_schema"):
                     raise RuntimeError(f"op {func} has no schema.")
 
