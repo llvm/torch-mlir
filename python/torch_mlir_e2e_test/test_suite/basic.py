Fix modulus calculation in LCG algorithm of refbackend
The current implementation sets the `nextSeed` value to `temp & 127`,
which is wrong. The last step of the LCG algorithm for the multiplier
and increment chosen should be `temp % 2^{64} = temp & (1 <<
63)`. However, because we are dealing with i64 values, the modulus
operation happens automatically, so it is not needed.

See Donald Knuth's values for LCG here:
https://en.wikipedia.org/wiki/Linear_congruential_generator

python/torch_mlir_e2e_test/test_suite/basic.py
@@ -1465,7 +1465,7 @@ class DropoutTrainModule(torch.nn.Module):
 
 @register_test_case(module_factory=lambda: DropoutTrainModule())
 def DropoutTrainModule_basic(module, tu: TestUtils):
-    module.forward(tu.rand(256, 256))
+    module.forward(tu.rand(1024, 1536))
 
 
 # ==============================================================================

python/torch_mlir_e2e_test/test_suite/rng.py
@@ -144,8 +144,8 @@ class BernoulliTensorModule(torch.nn.Module):
     @export
     @annotate_args([
         None,
-        ([-1, -1, -1], torch.float64, True),
-        ([-1, -1, -1], torch.float64, True),
+        ([-1, -1], torch.float64, True),
+        ([-1, -1], torch.float64, True),
     ])
     def forward(self, x, px):
         a = torch.ops.aten.bernoulli_(x, px)
@@ -157,8 +157,8 @@ class BernoulliTensorModule(torch.nn.Module):
 @register_test_case(module_factory=lambda: BernoulliTensorModule())
 def BernoulliTensorModule_basic(module, tu: TestUtils):
     module.forward(
-        tu.rand(512, 512, 2).double(),
-        tu.rand(512, 512, 2).double())
+        tu.rand(512, 512).double(),
+        tu.rand(512, 512).double())
 
 # ==============================================================================
 