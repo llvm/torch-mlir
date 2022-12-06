485c18bb2fd0b62f9d381bb300b4fb809d465c60
Sean Silva <silvasean@google.com>
2022-12-06 14:36:54 +0000

    [torchdynamo] Add "lockstep" numerical accuracy debugger.
    Thanks to TorchDynamo's great layering and design, this is only about
    100 lines of code for a basic lockstep debugger.
    
    This should allow us to deprecate eager_mode, since AFAIK the only
    interesting use case that it was really supporting is for downstream users to
    write lockstep debuggers.
    
    NOTE: The exact reporting and interface here is subject to change. Please
    try it out and provide feedback (or patches :) ).
    - make_fx should not drop source locations: https://github.com/pytorch/pytorch/issues/90276
    - Report tensors better (huge tensors should be summarized)
    - Maybe don't abort, but just warn?
    - Allow customizing atol/rtol.
    - How best to print the failing node? And include surrounding graph
    context?

---
+++ python/test/debug/lockstep_basic.py
@@ -0,0 +1,46 @@
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Also available under a BSD-style license. See LICENSE.
+
+# RUN: %PYTHON %s | FileCheck %s
+
+from typing import List
+
+import torch
+import torch._dynamo as dynamo
+from torch_mlir.dynamo import make_simple_dynamo_backend
+from torch_mlir_e2e_test.debug.lockstep import make_lockstep_debug_backend
+
+
+@make_simple_dynamo_backend
+@make_lockstep_debug_backend()
+def miscompile_div_as_mul_backend(gm: torch.fx.GraphModule,
+                                  example_inputs: List[torch.Tensor]):
+    # Copy `gm` and rewrite `div` to `mul`.
+    new_g = torch.fx.Graph()
+    new_g.output(new_g.graph_copy(gm.graph, {}))
+    for node in new_g.nodes:
+        if node.op == "call_function":
+            if node.target == torch.ops.aten.div:
+                node.target = torch.ops.aten.mul
+    new_gm = torch.fx.GraphModule(torch.nn.Module(), new_g)
+    return new_gm
+
+
+# TODO: As we get smarter about making this output more readable, we should
+# have more focused tests rather than this "check the exact output" test.
+# CHECK: User result tensor([ 4., 10., 18.]) is not close to golden result tensor([0.2500, 0.4000, 0.5000]) for node div at None
+@dynamo.optimize(miscompile_div_as_mul_backend)
+def f(x, y):
+    a = x * y
+    b = x + y
+    c = x / y
+    return a, b, c
+
+
+args = (torch.tensor([1., 2., 3.]), torch.tensor([4., 5., 6.]))
+try:
+    print(f(*args))
+except AssertionError as e:
+    print(e)
