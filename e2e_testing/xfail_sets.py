d33c71c8cc4a93cbd17719db69152d9a68867d66
Shivam Gupta <shivam98.tkg@gmail.com>
2022-12-21 10:40:33 +0530

    xfail_sets.py for LTC

--- e2e_testing/xfail_sets.py
+++ e2e_testing/xfail_sets.py
@@ -800,6 +800,7 @@ LTC_XFAIL_SET = {
     "ConvolutionModule2DTransposeStrided_basic",
     "ConvolutionModule2DTranspose_basic",
     "ElementwiseClampModule_basic",
+    "ElementwisePreluModule",
     "IouOfModule_basic",
     "MobilenetV3Module_basic",
     "NativeBatchNormNoneWeightModule_basic",
