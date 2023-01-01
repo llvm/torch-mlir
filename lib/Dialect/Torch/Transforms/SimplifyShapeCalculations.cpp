3d50d3d9fefa62c27fb1989f1efa0b8b1ab987e9
powderluv <powderluv@users.noreply.github.com>
2023-01-01 10:56:06 -0800

    Revert "rebase llvm: 5f24f893cac7aaea292c70f8aa83b021499114be (#1760)" (#1765)
    This reverts commit fa356cce50b418269de5a3fa52e95a337cfc78fd.

--- lib/Dialect/Torch/Transforms/SimplifyShapeCalculations.cpp
+++ lib/Dialect/Torch/Transforms/SimplifyShapeCalculations.cpp
@@ -384,7 +384,7 @@ class SimplifyShapeCalculationsPass
     // A single linear scan should suffice.
     GreedyRewriteConfig config;
     config.useTopDownTraversal = true;
-    config.maxIterations = GreedyRewriteConfig::kNoLimit;
+    config.maxIterations = GreedyRewriteConfig::kNoIterationLimit;
     if (failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns),
                                             config))) {
       return signalPassFailure();
