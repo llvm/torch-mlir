159ef0aa1c1238fc1c1d08f248355ac55de95930
AmosLewis <Amos_Lewsi@foxmail.com>
2023-01-03 10:21:32 -0800

    [TOSA] Add aten.Index.Tensor support

--- python/torch_mlir_e2e_test/test_suite/basic.py
+++ python/torch_mlir_e2e_test/test_suite/basic.py
@@ -1788,6 +1788,27 @@ def IndexTensorModule_basic(module, tu: TestUtils):
 
 
 # ==============================================================================
+class IndexTensorStaticModule(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    @export
+    @annotate_args([
+        None,
+        ([4, 5], torch.float32, True),
+        ([2, 3], torch.int64, True),
+    ])
+    def forward(self, x, index):
+        return torch.ops.aten.index(x, (index, ))
+
+
+@register_test_case(module_factory=lambda: IndexTensorStaticModule())
+def IndexTensorStaticModule_basic(module, tu: TestUtils):
+    module.forward(tu.rand(4, 5), tu.randint(2, 3, high=4))
+
+
+# ==============================================================================
 
 
 class IndexTensorModule3dInput(torch.nn.Module):
