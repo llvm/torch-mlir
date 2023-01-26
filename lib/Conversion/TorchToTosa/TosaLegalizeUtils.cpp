67ba7b7dae57f46fcd704368e12d9cddfde3344c
AmosLewis <Amos_Lewsi@foxmail.com>
2023-01-25 21:52:47 -0800

    [TOSA] Add to.dtype i1 to i64

--- lib/Conversion/TorchToTosa/TosaLegalizeUtils.cpp
+++ lib/Conversion/TorchToTosa/TosaLegalizeUtils.cpp
@@ -230,6 +230,7 @@ static LogicalResult checkValidityOfCast(Type src, Type dest) {
       (src.isInteger(32) && dest.isInteger(1)) ||
       (src.isInteger(32) && dest.isF32()) ||
       (src.isInteger(8) && dest.isInteger(1)) ||
+      (src.isInteger(1) && dest.isInteger(64)) ||
       (src.isF32() && dest.isInteger(8)) ||
       (src.isF32() && dest.isInteger(1))) {
     return success();
