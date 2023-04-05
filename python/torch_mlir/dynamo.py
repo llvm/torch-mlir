2213ce0855990faf99487587e9f2195b4caf1935
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2023-04-05 17:02:52 +0530

    [TorchDynamo] Add aten.squeeze op to the decomposition list
    Signed-Off By: Vivek Khandelwal <vivek@nod-labs.com>

--- python/torch_mlir/dynamo.py
+++ python/torch_mlir/dynamo.py
@@ -62,7 +62,8 @@ def _get_decomposition_table():
         aten.native_group_norm_backward,
         aten.sigmoid_backward,
         aten._native_batch_norm_legit,
-        aten._native_batch_norm_legit_no_training
+        aten._native_batch_norm_legit_no_training,
+        aten.squeeze,
     ])
 
 
