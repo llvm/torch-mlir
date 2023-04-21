85916dab331bd854e4c181b3aee1e8129daf8f6d
rahul shrivastava <rahul.shrivastava@cerebras.net>
2023-04-21 08:45:27 -0700

    Add ODS for aten.scatter.src
    Signed-off-by: rahul shrivastava <rahul.shrivastava@cerebras.net>

--- python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
+++ python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
@@ -546,6 +546,7 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
     emit("aten::view_copy : (Tensor, int[]) -> (Tensor)")
     emit("aten::view_copy.dtype : (Tensor, int) -> (Tensor)")
     emit("aten::unfold_copy : (Tensor, int, int, int) -> (Tensor)")
+    emit("aten::scatter.src : (Tensor, int, Tensor, Tensor) -> (Tensor)")
     emit("aten::select_scatter : (Tensor, Tensor, int, int) -> (Tensor)")
     emit("aten::slice_scatter : (Tensor, Tensor, int, int?, int?, int) -> (Tensor)")
     emit("aten::diagonal_scatter : (Tensor, Tensor, int, int, int) -> (Tensor)")
