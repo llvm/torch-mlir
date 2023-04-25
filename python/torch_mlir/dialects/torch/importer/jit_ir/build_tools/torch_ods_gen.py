c9f5265a41df6f6a474feb6e3f55b905af4e8c34
rahul shrivastava <rahul.shrivastava@cerebras.net>
2023-04-25 04:48:37 -0700

    Add ODS for aten.pow.Scalar
    Signed-off-by: rahul shrivastava <rahul.shrivastava@cerebras.net>

--- python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
+++ python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/torch_ods_gen.py
@@ -322,6 +322,7 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
     emit("aten::gelu : (Tensor, str) -> (Tensor)")
     emit("aten::pow.Tensor_Scalar : (Tensor, Scalar) -> (Tensor)")
     emit("aten::pow.Tensor_Tensor : (Tensor, Tensor) -> (Tensor)")
+    emit("aten::pow.Scalar : (Scalar, Tensor) -> (Tensor)")
     emit("aten::threshold_backward : (Tensor, Tensor, Scalar) -> (Tensor)")
     emit("aten::floor_divide : (Tensor, Tensor) -> (Tensor)")
     emit("aten::softplus : (Tensor, Scalar, Scalar) -> (Tensor)")
