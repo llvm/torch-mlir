816880774b8e03ff40307445041273357483043b
Matthias Gehre <93204396+mgehre-amd@users.noreply.github.com>
2023-06-07 10:19:38 +0200

    Fix version comparison against stable (#2209)

--- python/torch_mlir/dynamo.py
+++ python/torch_mlir/dynamo.py
@@ -4,7 +4,7 @@
 # Also available under a BSD-style license. See LICENSE.
 
 from typing import List
-from packaging import version
+from ._version import torch_version_for_comparison, version
 
 import torch
 from torch._functorch.compile_utils import strip_overloads
@@ -66,7 +66,7 @@ def _get_decomposition_table():
         aten.squeeze,
     ]
     # TODO: enable test once 2.1.0 is stable
-    if version.parse(torch.__version__) > version.parse("2.0.1+cpu"):
+    if torch_version_for_comparison() >= version.parse("2.1.0.dev"):
         decomp_list += [aten._native_batch_norm_legit_no_training]
     return get_decompositions(decomp_list)
 
