4e2ba2e0af092bd332fc5958a693f81fe4da1af3
Matthias Gehre <93204396+mgehre-amd@users.noreply.github.com>
2023-06-10 20:45:35 +0200

    Support aten.sign (#2205)

--- python/torch_mlir_e2e_test/test_suite/elementwise.py
+++ python/torch_mlir_e2e_test/test_suite/elementwise.py
@@ -1291,6 +1291,28 @@ def ElementwiseCeilModule_basic(module, tu: TestUtils):
 # ==============================================================================
 
 
+class ElementwiseSignModule(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    @export
+    @annotate_args([
+        None,
+        ([-1, -1], torch.float32, True),
+    ])
+    def forward(self, a):
+        return torch.ops.aten.sign(a)
+
+
+@register_test_case(module_factory=lambda: ElementwiseSignModule())
+def ElementwiseSignModule_basic(module, tu: TestUtils):
+    module.forward(tu.rand(3, 4))
+
+
+# ==============================================================================
+
+
 class ElementwisePowModule(torch.nn.Module):
 
     def __init__(self):
