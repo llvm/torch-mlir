04091185bcbf8e7c94f7840f9de72e04ffedc24b
zhekun.zhang <zhekun.zhang@bytedance.com>
2023-07-06 12:39:01 -0700

    remove cpu check

--- lib/Conversion/TorchToStablehlo/Basic.cpp
+++ lib/Conversion/TorchToStablehlo/Basic.cpp
@@ -1476,15 +1476,11 @@ LogicalResult ConvertAtenOp<AtenEmptyMemoryFormatOp>::matchAndRewrite(
               "memory_format is supported");
   }
 
-  // TODO: Add support for device arg other than cpu.
   if (!op.getDevice().getType().isa<Torch::NoneType>()) {
     std::string device;
     if (!matchPattern(op.getDevice(), m_TorchConstantDevice(device)))
       return rewriter.notifyMatchFailure(
           op, "unimplemented: device must be a constant str");
-    else if (device != "cpu")
-      return rewriter.notifyMatchFailure(
-          op, "unimplemented: device is expected to be cpu");
   }
 
   // TODO: Add support for non-strided layout.
