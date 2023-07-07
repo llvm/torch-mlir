4b74dfc0fc88af8ea816639edbb981648762de52
glebk-cerebras <111300564+glebk-cerebras@users.noreply.github.com>
2023-07-07 15:51:22 -0400

    Tensor[]? support operands type support using partial codegen (#87)
    * aten.index.Tensor support via partial codegen
    
    * Add torch.index_put tracing support
    
    * Added optional tensor list type support for LTC/TorchMLIR lowering
    
    * Added comments
    
    ---------
    
    Co-authored-by: GlebKazantaev <gleb.nnstu@gmail.com>

---
+++ python/torch_mlir/csrc/base_lazy_backend/ops/ivalue.cpp
@@ -0,0 +1,36 @@
+//===- ivalue.cpp
+//----------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "ivalue.h"
+
+#include <ATen/core/ivalue.h>
+
+namespace torch {
+namespace lazy {
+
+IValueConstant::IValueConstant(const c10::IValue& value)
+    : torch::lazy::TorchMlirNode(IValueConstant::ClassOpKind(), OpList{},
+                                 std::vector<Shape>{},
+                                 /* num_outputs */ 1, torch::lazy::MHash()),
+      value(value) {}
+
+std::string IValueConstant::ToString() const {
+  std::stringstream ss;
+  ss << torch::lazy::TorchMlirNode::ToString();
+  return ss.str();
+}
+
+TorchMlirOpVector IValueConstant::Lower(TorchMlirFunction function,
+                                        TorchMlirLoweringContext* loctx) const {
+  return {loctx->graph()->insertConstant(value)};
+}
+
+}  // namespace lazy
+}  // namespace torch
