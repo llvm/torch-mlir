3ca35b4f3cf10b08d6c3f2e19285899f440b3ac2
Matthias Gehre <93204396+mgehre-amd@users.noreply.github.com>
2023-07-21 08:54:19 +0200

    TorchToTosa: aten.embedding: Allow indices with any rank (#2327)
    It's actually fine to not check the rank of the indices, because the conversion anyways flattens the index tensor to be (1, numElements) before applying tosa::gather, and then anyways reshapes the output tensor to the output shape of the aten.embedding.

--- e2e_testing/xfail_sets.py
+++ e2e_testing/xfail_sets.py
@@ -1017,6 +1017,7 @@ TOSA_PASS_SET = {
     "NumpyTRankNStaticModule_basic",
     "NumpyTRankNDynamicModule_basic",
     "EmbeddingModuleI32Static_basic",
+    "EmbeddingModule1DIndices_basic",
     "TModuleRank2_basic",
     "TransposeIntModule_basic",
     "TransposeIntNegDimsModule_basic",
