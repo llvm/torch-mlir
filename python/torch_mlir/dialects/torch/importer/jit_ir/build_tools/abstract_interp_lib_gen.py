610d836fd21f3739ccf67c25dfec4e135367ca9f
Arham Khan <arhammkhan@gmail.com>
2023-08-23 11:04:14 -0500

    impl aten.elu as decomposition

--- python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/abstract_interp_lib_gen.py
+++ python/torch_mlir/dialects/torch/importer/jit_ir/build_tools/abstract_interp_lib_gen.py
@@ -2722,11 +2722,10 @@ def aten〇leaky_relu〡dtype(self_rank_dtype: Tuple[int, int], negative_slope: 
     dtypes = [self_dtype, negative_slope_dtype]
     return promote_dtypes(ranks, dtypes)
 
-# @check_dtype_function(
-#     _check_tensors_with_the_same_dtype(num_of_tensors=1, error_types={torch.bool}, alpha=1, scale=1, input_scale=2) +
-#     _check_tensors_with_the_same_dtype(num_of_tensors=1, error_types={torch.bool, torch.int8, torch.uint8, torch.int16, torch.int32, torch.int64}, alpha=1.0, scale=1.0, input_scale=1.0))
+@check_dtype_function(
+    _check_tensors_with_the_same_dtype(num_of_tensors=1, error_types={torch.bool}, alpha=1, scale=1, input_scale=2) +
+    _check_tensors_with_the_same_dtype(num_of_tensors=1, error_types={torch.bool, torch.int8, torch.uint8, torch.int16, torch.int32, torch.int64}, alpha=1.0, scale=1.0, input_scale=2))
 def aten〇elu〡dtype(self_rank_dtype: Tuple[int, int], alpha: Union[int, float, complex] = 1, scale: Union[int, float, complex] = 1, input_scale: Union[int, float, complex] = 1) -> int:
-    # return torch.float64
     self_rank, self_dtype = self_rank_dtype
     assert self_dtype != torch.bool
     ranks: List[Optional[int]] = [self_rank, None]
@@ -2734,14 +2733,10 @@ def aten〇elu〡dtype(self_rank_dtype: Tuple[int, int], alpha: Union[int, float
     scale_dtype = get_dtype_of_scalar(scale)
     input_scale_dtype = get_dtype_of_scalar(input_scale)
 
-    if is_float_dtype(alpha_dtype):
-        assert not is_integer_dtype(self_dtype)
-    if is_float_dtype(scale_dtype):
-        assert not is_integer_dtype(self_dtype)
-    if is_float_dtype(input_scale_dtype):
+    if any([is_float_dtype(d) for d in [alpha_dtype, scale_dtype, input_scale_dtype]]):
         assert not is_integer_dtype(self_dtype)
 
-    dtypes = [self_dtype, input_scale_dtype] #[self_dtype] + param_dtypes
+    dtypes = [self_dtype, input_scale_dtype]
     return promote_dtypes(ranks, dtypes)
 
 @check_dtype_function(
