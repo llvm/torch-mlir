5a48bf1cad150d812f7f0a19e39d1558843b6f9b
jinchen62 <jinchenye62@gmail.com>
2023-08-24 16:20:36 -0700

    Address the comments

--- lib/Dialect/TorchConversion/Transforms/ConvertCustomQuantOp.cpp
+++ lib/Dialect/TorchConversion/Transforms/ConvertCustomQuantOp.cpp
@@ -40,28 +40,28 @@ public:
       return failure();
     }
 
-    // get inputs: lhs, q_rhs, scales, zps
+    // get inputs: lhs, rhsQuant, scales, zps
     Value lhs = adaptor.getOperands()[0];
     auto lhsType = lhs.getType().cast<RankedTensorType>();
     if (!lhsType) {
       return failure();
     }
     auto lhsShape = lhsType.getShape();
-    int lhs_reduct_dim_size = lhsShape.back();
+    int lhsReductDimSize = lhsShape.back();
 
-    Value q_rhs = adaptor.getOperands()[1];
-    auto rhsType = q_rhs.getType().cast<RankedTensorType>();
+    Value rhsQuant = adaptor.getOperands()[1];
+    auto rhsType = rhsQuant.getType().cast<RankedTensorType>();
     if (!rhsType) {
       return failure();
     }
     auto rhsShape = rhsType.getShape();
-    int rhs_reduct_dim_size = rhsShape.back();
-    Type rhs_elementType = rhsType.getElementType();
+    int rhsReductDimSize = rhsShape.back();
+    Type rhsElementType = rhsType.getElementType();
 
     Value scales = adaptor.getOperands()[2];
     Value zps = adaptor.getOperands()[3];
-    Value unpacked_type_width = adaptor.getOperands()[4];
-    Value group_size = adaptor.getOperands()[5];
+    Value unpackedTypeWidth = adaptor.getOperands()[4];
+    Value groupSize = adaptor.getOperands()[5];
 
     auto getConstantIntegerFromDefiningOp = [](Value operand,
                                                int &extractedInt) {
@@ -79,14 +79,14 @@ public:
     };
 
     int gs;
-    if (failed(getConstantIntegerFromDefiningOp(group_size, gs))) {
+    if (failed(getConstantIntegerFromDefiningOp(groupSize, gs))) {
       return failure();
     }
     int unpackedBitWidth;
-    if (failed(getConstantIntegerFromDefiningOp(unpacked_type_width, unpackedBitWidth))) {
+    if (failed(getConstantIntegerFromDefiningOp(unpackedTypeWidth, unpackedBitWidth))) {
       return failure();
     }
-    if (unpackedBitWidth != rhs_elementType.getIntOrFloatBitWidth()) {
+    if (unpackedBitWidth != rhsElementType.getIntOrFloatBitWidth()) {
       return failure();
     }
 
@@ -100,24 +100,24 @@ public:
     Type elementType = resultType.getElementType();
 
     // expand lhs
-    std::vector<int64_t> lhs_expandedShape = {lhsShape[0], lhsShape[1],
-                                              lhs_reduct_dim_size / gs, gs};
-    RankedTensorType lhs_expandedType = RankedTensorType::get(lhs_expandedShape, elementType);
-    SmallVector<ReassociationIndices, 4> lhs_reassociation = {{0}, {1}, {2, 3}};
-    Value expanded_lhs = rewriter.create<tensor::ExpandShapeOp>(
-      loc, lhs_expandedType, lhs, lhs_reassociation);
+    std::vector<int64_t> lhsExpandedShape = {lhsShape[0], lhsShape[1],
+                                             lhsReductDimSize / gs, gs};
+    RankedTensorType lhsExpandedType = RankedTensorType::get(lhsExpandedShape, elementType);
+    SmallVector<ReassociationIndices, 4> lhsReassociation = {{0}, {1}, {2, 3}};
+    Value lhsExpanded = rewriter.create<tensor::ExpandShapeOp>(
+      loc, lhsExpandedType, lhs, lhsReassociation);
 
     // expand rhs
-    std::vector<int64_t> expandedShape = {rhsShape[0], rhs_reduct_dim_size/gs, gs};
-    RankedTensorType expandedType = RankedTensorType::get(expandedShape, rhs_elementType);
-    SmallVector<ReassociationIndices, 4> reassociation = {{0}, {1, 2}};
-    Value expanded_rhs = rewriter.create<tensor::ExpandShapeOp>(
-      loc, expandedType, q_rhs, reassociation);
-    Value cst_0 = rewriter.create<arith::ConstantOp>(
+    std::vector<int64_t> rhsExpandedShape = {rhsShape[0], rhsReductDimSize/gs, gs};
+    RankedTensorType rhsExpandedType = RankedTensorType::get(rhsExpandedShape, rhsElementType);
+    SmallVector<ReassociationIndices, 4> rhsReassociation = {{0}, {1, 2}};
+    Value rhsExpanded = rewriter.create<tensor::ExpandShapeOp>(
+      loc, rhsExpandedType, rhsQuant, rhsReassociation);
+    Value cst0 = rewriter.create<arith::ConstantOp>(
       loc, FloatAttr::get(elementType, 0.0));
 
-    Value dq_empty = rewriter.create<tensor::EmptyOp>(
-      loc, expandedShape, elementType);
+    Value emptyDequant = rewriter.create<tensor::EmptyOp>(
+      loc, rhsExpandedShape, elementType);
     SmallVector<Value> dynDims;
     for (int i = 0; i < lhsType.getRank(); i++) {
       if (lhsType.isDynamicDim(i)) {
@@ -127,7 +127,7 @@ public:
     Value empty = rewriter.create<tensor::EmptyOp>(
       loc, resultShape, elementType, dynDims);
     Value output = rewriter.create<linalg::FillOp>(
-      loc, cst_0, empty).getResult(0);
+      loc, cst0, empty).getResult(0);
 
     AffineExpr d0, d1, d2, d3, d4;
     bindDims(getContext(), d0, d1, d2, d3, d4);
@@ -137,23 +137,23 @@ public:
     auto map2 = AffineMap::get(5, 0, {d0, d1, d3, d4}, rewriter.getContext());
     auto map3 = AffineMap::get(5, 0, {d2, d3, d4}, rewriter.getContext());
     auto map4 = AffineMap::get(5, 0, {d0, d1, d2}, rewriter.getContext());
-    SmallVector<AffineMap, 4> dq_indexingMaps = {map, map1, map1, map};
-    SmallVector<AffineMap, 4> mat_indexingMaps = {map2, map3, map4};
+    SmallVector<AffineMap, 4> dqIndexingMaps = {map, map1, map1, map};
+    SmallVector<AffineMap, 4> matIndexingMaps = {map2, map3, map4};
 
-    SmallVector<utils::IteratorType> dq_iteratorTypes(3, utils::IteratorType::parallel);
-    SmallVector<utils::IteratorType> mat_iteratorTypes = {
+    SmallVector<utils::IteratorType> dequantIteratorTypes(3, utils::IteratorType::parallel);
+    SmallVector<utils::IteratorType> matmulIteratorTypes = {
       utils::IteratorType::parallel, utils::IteratorType::parallel,
       utils::IteratorType::parallel, utils::IteratorType::reduction,
       utils::IteratorType::reduction
     };
 
-    Value dq_rhs =
+    Value rhsDequant =
         rewriter
             .create<linalg::GenericOp>(
-                loc, dq_empty.getType(),
-                ValueRange{expanded_rhs, scales, zps}, dq_empty,
-                /*indexingMaps=*/dq_indexingMaps,
-                /*iteratorTypes=*/dq_iteratorTypes,
+                loc, emptyDequant.getType(),
+                ValueRange{rhsExpanded, scales, zps}, emptyDequant,
+                /*indexingMaps=*/dqIndexingMaps,
+                /*iteratorTypes=*/dequantIteratorTypes,
                 [&](OpBuilder &b, Location loc, ValueRange args) {
                   Value w = args[0], scale = args[1], zeroPoint = args[2];
                   Value extw = b.create<arith::ExtUIOp>(loc, rewriter.getI32Type(), w);
@@ -164,13 +164,13 @@ public:
                 })
             .getResult(0);
 
-    Value quantMat =
+    Value matmulDequant =
         rewriter
             .create<linalg::GenericOp>(
                 loc, output.getType(),
-                ValueRange{expanded_lhs, dq_rhs}, output,
-                /*indexingMaps=*/mat_indexingMaps,
-                /*iteratorTypes=*/mat_iteratorTypes,
+                ValueRange{lhsExpanded, rhsDequant}, output,
+                /*indexingMaps=*/matIndexingMaps,
+                /*iteratorTypes=*/matmulIteratorTypes,
                 [&](OpBuilder &b, Location loc, ValueRange args) {
                   Value l = args[0], r = args[1], out = args[2];
                   Value pd = b.create<arith::MulFOp>(loc, l, r);
@@ -179,7 +179,7 @@ public:
                 })
             .getResult(0);
 
-    rewriter.replaceOpWithNewOp<tensor::CastOp>(op, resultType, quantMat);
+    rewriter.replaceOpWithNewOp<tensor::CastOp>(op, resultType, matmulDequant);
     return success();
   }
 };
