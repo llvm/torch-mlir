f4b7ac6a73d9cfe2722a009c938216417560d7cc
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2023-09-01 07:24:43 -0700

    copied and pasted from main branch as there's no change to be made in this file

--- python/torch_mlir_e2e_test/test_suite/reshape_like.py
+++ python/torch_mlir_e2e_test/test_suite/reshape_like.py
@@ -19,14 +19,13 @@ class ViewExpandModule(torch.nn.Module):
         None,
         ([6, 4], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 3, 4)
-
 
 @register_test_case(module_factory=lambda: ViewExpandModule())
 def ViewExpandModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(6, 4))
-
 
 # ==============================================================================
 
@@ -39,14 +38,13 @@ class ViewExpandOnesModule(torch.nn.Module):
         None,
         ([1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(1, 1, 1, 1, 1)
-
 
 @register_test_case(module_factory=lambda: ViewExpandOnesModule())
 def ViewExpandOnesModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(1))
-
 
 # ==============================================================================
 
@@ -59,14 +57,13 @@ class ViewExpandOnesBeforeAndAfterModule(torch.nn.Module):
         None,
         ([2, 1, 16, 1, 1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(1, 2, 1, 16, 1, 1, 1, 1)
-
 
 @register_test_case(module_factory=lambda: ViewExpandOnesBeforeAndAfterModule())
 def ViewExpandOnesBeforeAndAfterModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 1, 16, 1, 1))
-
 
 # ==============================================================================
 
@@ -79,16 +76,15 @@ class ViewExpandOnesMiddleModule(torch.nn.Module):
         None,
         ([3, 1, 2], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(3, 1, 1, 1, 1, 2)
-
 
 @register_test_case(module_factory=lambda: ViewExpandOnesMiddleModule())
 def ViewExpandOnesMiddleModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(3, 1, 2))
 
     # ==============================================================================
-
 
 class ViewCollapseOnesMiddleModule(torch.nn.Module):
     def __init__(self):
@@ -99,14 +95,13 @@ class ViewCollapseOnesMiddleModule(torch.nn.Module):
         None,
         ([3, 1, 1, 1, 1, 2], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(3, 1, 2)
-
 
 @register_test_case(module_factory=lambda: ViewCollapseOnesMiddleModule())
 def ViewCollapseOnesMiddleModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(3, 1, 1, 1, 1, 2))
-
 
 # ==============================================================================
 
@@ -119,14 +114,13 @@ class ViewDynamicExpandModule(torch.nn.Module):
         None,
         ([-1, -1, 30, 384], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 4, 5, 6, 12, 32)
-
 
 @register_test_case(module_factory=lambda: ViewDynamicExpandModule())
 def ViewDynamicExpandModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 30, 384))
-
 
 # ==============================================================================
 
@@ -139,14 +133,13 @@ class ViewDynamicExpandWithAtenSizeIntModule(torch.nn.Module):
         None,
         ([-1, -1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(a.size(0), a.size(1), 12, 32)
-
 
 @register_test_case(module_factory=lambda: ViewDynamicExpandWithAtenSizeIntModule())
 def ViewDynamicExpandWithAtenSizeIntModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 384))
-
 
 # ==============================================================================
 
@@ -159,14 +152,13 @@ class ViewCollapseModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(8)
-
 
 @register_test_case(module_factory=lambda: ViewCollapseModule())
 def ViewCollapseModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4))
-
 
 # ==============================================================================
 
@@ -181,14 +173,13 @@ class ViewCollapseDynamicWithAtenSizeIntModule(torch.nn.Module):
         ([], torch.int64, True),
         ([], torch.int64, True),
     ])
+
     def forward(self, a, b, c):
         return a.view(a.size(0), int(b), int(c), a.size(3), 384)
-
 
 @register_test_case(module_factory=lambda: ViewCollapseDynamicWithAtenSizeIntModule())
 def ViewCollapseDynamicWithAtenSizeIntModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 3, 5, 4, 12, 32), torch.tensor(3), torch.tensor(5))
-
 
 # ==============================================================================
 
@@ -201,14 +192,13 @@ class ViewExpandCollapseWithOnesModule(torch.nn.Module):
         None,
         ([2, 4, 8, 8], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 1, 1, 4, 64)
-
 
 @register_test_case(module_factory=lambda: ViewExpandCollapseWithOnesModule())
 def ViewExpandCollapseWithOnesModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 8, 8))
-
 
 # ==============================================================================
 
@@ -221,14 +211,13 @@ class ViewExpandCollapseModule(torch.nn.Module):
         None,
         ([2, 4, 8, 16, 4], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(8, 2, 4, 16, 2, 2)
-
 
 @register_test_case(module_factory=lambda: ViewExpandCollapseModule())
 def ViewExpandCollapseModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 8, 16, 4))
-
 
 # ==============================================================================
 
@@ -241,14 +230,13 @@ class ViewDynamicExpandCollapseModule(torch.nn.Module):
         None,
         ([-1, 4, -1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 1, 4, 64)
-
 
 @register_test_case(module_factory=lambda: ViewDynamicExpandCollapseModule())
 def ViewDynamicExpandCollapseModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 8, 8))
-
 
 # ==============================================================================
 
@@ -261,14 +249,13 @@ class ViewDynamicExpandCollapseWithAtenIntModule(torch.nn.Module):
         None,
         ([-1, -1, -1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 1, a.size(1), 64)
-
 
 @register_test_case(module_factory=lambda: ViewDynamicExpandCollapseWithAtenIntModule())
 def ViewDynamicExpandCollapseWithAtenIntModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 8, 8))
-
 
 # ==============================================================================
 
@@ -281,14 +268,13 @@ class ViewTwoToThreeStaticModule(torch.nn.Module):
         None,
         ([3, 2], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 3)
-
 
 @register_test_case(module_factory=lambda: ViewTwoToThreeStaticModule())
 def ViewTwoToThreeStaticModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(3, 2))
-
 
 # ==============================================================================
 
@@ -301,14 +287,13 @@ class ViewTwoFiveThreeStaticModule(torch.nn.Module):
         None,
         ([3, 5, 2], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 5, 3)
-
 
 @register_test_case(module_factory=lambda: ViewTwoFiveThreeStaticModule())
 def ViewTwoFiveThreeStaticModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(3, 5, 2))
-
 
 # ==============================================================================
 
@@ -321,14 +306,13 @@ class ViewOffsetTestStaticModule(torch.nn.Module):
         None,
         ([2, 3, 2, 2, 5, 6], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 3, 4, 6, 5)
-
 
 @register_test_case(module_factory=lambda: ViewOffsetTestStaticModule())
 def ViewOffsetTestStaticModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 3, 2, 2, 5, 6))
-
 
 # ==============================================================================
 
@@ -341,14 +325,13 @@ class ViewOffsetBackwardTestStaticModule(torch.nn.Module):
         None,
         ([2, 3, 4, 5, 6], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(2, 3, 2, 2, 6, 5)
-
 
 @register_test_case(module_factory=lambda: ViewOffsetBackwardTestStaticModule())
 def ViewOffsetBackwardTestStaticModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 3, 4, 5, 6))
-
 
 # ==============================================================================
 
@@ -361,14 +344,13 @@ class View1DFoldModule(torch.nn.Module):
         None,
         ([-1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(-1)
-
 
 @register_test_case(module_factory=lambda: View1DFoldModule())
 def View1DFoldModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(32))
-
 
 # ==============================================================================
 
@@ -381,14 +363,13 @@ class ViewCollapseInferredDimModule(torch.nn.Module):
         None,
         ([2, 3, 4], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(-1, 4)
-
 
 @register_test_case(module_factory=lambda: ViewCollapseInferredDimModule())
 def ViewCollapseInferredDimModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 3, 4))
-
 
 # ==============================================================================
 
@@ -401,14 +382,13 @@ class ViewExpandInferredDimModule(torch.nn.Module):
         None,
         ([2, 6], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(3, -1, 2)
-
 
 @register_test_case(module_factory=lambda: ViewExpandInferredDimModule())
 def ViewExpandInferredDimModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 6))
-
 
 # ==============================================================================
 
@@ -421,14 +401,13 @@ class ViewExpandDynamicDimModule(torch.nn.Module):
         None,
         ([1, -1, 128], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(16, 1, 128)
-
 
 @register_test_case(module_factory=lambda: ViewExpandDynamicDimModule())
 def ViewExpandDynamicDimModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(1, 16, 128))
-
 
 # ==============================================================================
 
@@ -441,14 +420,13 @@ class ViewFlattenAndExpandModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(a.size(0), a.size(1))
 
-
 @register_test_case(module_factory=lambda: ViewFlattenAndExpandModule())
 def ViewFlattenAndExpandModule_basic(module, tu: TestUtils):
-    module.forward(tu.rand(64, 128))
-
+    module.forward(tu.rand(64,128))
 
 # ==============================================================================
 
@@ -461,14 +439,13 @@ class UnsafeViewExpandModule(torch.nn.Module):
         None,
         ([6, 4], torch.float32, True),
     ])
+
     def forward(self, a):
         return torch.ops.aten._unsafe_view(a, [2, 3, 4])
-
 
 @register_test_case(module_factory=lambda: UnsafeViewExpandModule())
 def UnsafeViewExpandModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(6, 4))
-
 
 # ==============================================================================
 
@@ -481,14 +458,13 @@ class UnsafeViewDynamicExpandModule(torch.nn.Module):
         None,
         ([-1, -1, 30, 384], torch.float32, True),
     ])
-    def forward(self, a):
-        return torch.ops.aten._unsafe_view(a, [2, 4, 5, 6, 12, 32])
 
+    def forward(self, a):
+        return torch.ops.aten._unsafe_view(a,[2, 4, 5, 6, 12, 32])
 
 @register_test_case(module_factory=lambda: UnsafeViewDynamicExpandModule())
 def UnsafeViewDynamicExpandModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 30, 384))
-
 
 # ==============================================================================
 
@@ -501,14 +477,13 @@ class UnsafeViewDynamicExpandWithAtenSizeIntModule(torch.nn.Module):
         None,
         ([-1, -1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return torch.ops.aten._unsafe_view(a, [a.size(0), a.size(1), 12, 32])
-
 
 @register_test_case(module_factory=lambda: UnsafeViewDynamicExpandWithAtenSizeIntModule())
 def UnsafeViewDynamicExpandWithAtenSizeIntModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 384))
-
 
 # ==============================================================================
 
@@ -521,14 +496,13 @@ class UnsafeViewCollapseModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
     ])
-    def forward(self, a):
-        return torch.ops.aten._unsafe_view(a, [8])
 
+    def forward(self, a):
+        return torch.ops.aten._unsafe_view(a,[8])
 
 @register_test_case(module_factory=lambda: UnsafeViewCollapseModule())
 def UnsafeViewCollapseModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4))
-
 
 # ==============================================================================
 
@@ -543,14 +517,13 @@ class UnsafeViewCollapseDynamicWithAtenSizeIntModule(torch.nn.Module):
         ([], torch.int64, True),
         ([], torch.int64, True),
     ])
+
     def forward(self, a, b, c):
         return torch.ops.aten._unsafe_view(a, [a.size(0), int(b), int(c), a.size(3), 384])
-
 
 @register_test_case(module_factory=lambda: UnsafeViewCollapseDynamicWithAtenSizeIntModule())
 def UnsafeViewCollapseDynamicWithAtenSizeIntModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 3, 5, 4, 12, 32), torch.tensor(3), torch.tensor(5))
-
 
 # ==============================================================================
 
@@ -563,14 +536,13 @@ class UnsafeView1DFoldModule(torch.nn.Module):
         None,
         ([-1], torch.float32, True),
     ])
+
     def forward(self, a):
         return torch.ops.aten._unsafe_view(a, [-1])
-
 
 @register_test_case(module_factory=lambda: UnsafeView1DFoldModule())
 def UnsafeView1DFoldModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(32))
-
 
 # ==============================================================================
 
@@ -583,14 +555,13 @@ class ReshapeExpandModule(torch.nn.Module):
         None,
         ([-1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.reshape(12, 32)
-
 
 @register_test_case(module_factory=lambda: ReshapeExpandModule())
 def ReshapeExpandModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(384))
-
 
 # ==============================================================================
 
@@ -603,14 +574,13 @@ class ReshapeCollapseModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return torch.reshape(a, (-1,))
-
 
 @register_test_case(module_factory=lambda: ReshapeCollapseModule())
 def ReshapeCollapseModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4))
-
 
 # ==============================================================================
 
@@ -623,9 +593,9 @@ class ViewNoChange1dModule(torch.nn.Module):
         None,
         ([-1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(6)
-
 
 @register_test_case(module_factory=lambda: ViewNoChange1dModule())
 def ViewNoChange1dModule_basic(module, tu: TestUtils):
@@ -641,9 +611,9 @@ class ViewNoChange2dModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(5, 6)
-
 
 @register_test_case(module_factory=lambda: ViewNoChange2dModule())
 def ViewNoChange2dModule_basic(module, tu: TestUtils):
@@ -659,9 +629,9 @@ class ViewNoChange3dModule(torch.nn.Module):
         None,
         ([-1, -1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(4, 5, 6)
-
 
 @register_test_case(module_factory=lambda: ViewNoChange3dModule())
 def ViewNoChange3dModule_basic(module, tu: TestUtils):
@@ -677,14 +647,13 @@ class ViewNoChangeStaticModule(torch.nn.Module):
         None,
         ([4, 5, 6], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(4, 5, 6)
-
 
 @register_test_case(module_factory=lambda: ViewNoChangeStaticModule())
 def ViewNoChangeStaticModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(4, 5, 6))
-
 
 class ViewNegativeStaticModule(torch.nn.Module):
     def __init__(self):
@@ -695,14 +664,13 @@ class ViewNegativeStaticModule(torch.nn.Module):
         None,
         ([1, 128], torch.float32, True),
     ])
+
     def forward(self, a):
         return a.view(-1, 128)
-
 
 @register_test_case(module_factory=lambda: ViewNegativeStaticModule())
 def ViewNegativeStaticModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(1, 128))
-
 
 # ==============================================================================
 
@@ -717,14 +685,13 @@ class ReshapeAliasExpandModule(torch.nn.Module):
         None,
         ([-1], torch.float32, True),
     ])
+
     def forward(self, a):
         return torch.ops.aten._reshape_alias(a, size=(12, 32), stride=(32, 1))
-
 
 @register_test_case(module_factory=lambda: ReshapeAliasExpandModule())
 def ReshapeAliasExpandModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(384))
-
 
 # ==============================================================================
 
@@ -737,9 +704,9 @@ class ReshapeAliasCollapseModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
     ])
+
     def forward(self, a):
         return torch.ops.aten._reshape_alias(a, (8,), (1,))
-
 
 @register_test_case(module_factory=lambda: ReshapeAliasCollapseModule())
 def ReshapeAliasCollapseModule_basic(module, tu: TestUtils):
