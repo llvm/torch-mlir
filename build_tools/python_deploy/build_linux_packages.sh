66bba85d38a4f12fba55f653cb66c7c661c19e66
Stella Laurenzo <stellaraccident@gmail.com>
2023-11-01 19:18:38 -0700

    Remove CI references to torch-mlir-dialects.

--- build_tools/python_deploy/build_linux_packages.sh
+++ build_tools/python_deploy/build_linux_packages.sh
@@ -234,9 +234,8 @@ function build_in_tree() {
       -DCMAKE_C_COMPILER_LAUNCHER=ccache \
       -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
       -DLLVM_ENABLE_PROJECTS=mlir \
-      -DLLVM_EXTERNAL_PROJECTS="torch-mlir;torch-mlir-dialects" \
+      -DLLVM_EXTERNAL_PROJECTS="torch-mlir" \
       -DLLVM_EXTERNAL_TORCH_MLIR_SOURCE_DIR="/main_checkout/torch-mlir" \
-      -DLLVM_EXTERNAL_TORCH_MLIR_DIALECTS_SOURCE_DIR="/main_checkout/torch-mlir/externals/llvm-external-projects/torch-mlir-dialects" \
       -DLLVM_TARGETS_TO_BUILD=host \
       -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
       -DTORCH_MLIR_ENABLE_LTC=${enable_ltc} \
