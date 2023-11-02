5322c925e5c29b37d991cce138024b27181670b3
Stella Laurenzo <stellaraccident@gmail.com>
2023-11-01 18:40:42 -0700

    Core C++ code build fixup.

--- tools/torch-mlir-lsp-server/torch-mlir-lsp-server.cpp
+++ tools/torch-mlir-lsp-server/torch-mlir-lsp-server.cpp
@@ -9,8 +9,6 @@
 
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
-#include "mlir/InitAllDialects.h"
-#include "mlir/InitAllExtensions.h"
 #include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
 #include "torch-mlir/InitAll.h"
 
@@ -18,8 +16,7 @@ using namespace mlir;
 
 int main(int argc, char **argv) {
   DialectRegistry registry;
-  registerAllDialects(registry);
-  registerAllExtensions(registry);
   mlir::torch::registerAllDialects(registry);
+  mlir::torch::registerOptionalInputDialects(registry);
   return failed(MlirLspServerMain(argc, argv, registry));
 }
