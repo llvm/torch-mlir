823e05134477404a7c7a56804317d6f12994dc7a
Stella Laurenzo <stellaraccident@gmail.com>
2023-11-02 13:28:49 -0700

    Enable pt1 build.

--- projects/pt1/tools/e2e_test.sh
+++ projects/pt1/tools/e2e_test.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 src_dir="$(realpath "$(dirname "$0")"/..)"
+project_dir="$src_dir/../.."
 
 cd "$src_dir"
 
 # Ensure PYTHONPATH is set for export to child processes, even if empty.
 export PYTHONPATH=${PYTHONPATH-}
-source .env
+source $project_dir/.env
 
 python -m e2e_testing.main "$@"
