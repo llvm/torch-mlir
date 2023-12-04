e8de9ad5cbbfdb1468519e9137ee7f05851c3807
saienduri <enduri.sai@gmail.com>
2023-12-04 04:40:15 -0800

    add more ops

--- lib/Conversion/TorchOnnxToTorch/DefaultDomainQtoZ.cpp
+++ lib/Conversion/TorchOnnxToTorch/DefaultDomainQtoZ.cpp
@@ -7,7 +7,15 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Arith/Utils/Utils.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "torch-mlir/Conversion/TorchOnnxToTorch/Patterns.h"
+#include "torch-mlir/Dialect/Torch/IR/TorchOps.h"
+#include "torch-mlir/Dialect/Torch/IR/TorchTypes.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/SmallVector.h"
 
 using namespace mlir;
 using namespace mlir::torch;
@@ -139,4 +147,159 @@ void mlir::torch::onnx_c::populateDefaultDomainQtoZ(
                       binder.op, resultType, operand);
                   return success();
                 });
+  patterns.onOp("Sqrt", 1,
+                [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+                  Torch::ValueTensorType resultType;
+                  Value operand;
+                  if (binder.tensorOperand(operand) ||
+                      binder.tensorResultType(resultType))
+                    return failure();
+                  rewriter.replaceOpWithNewOp<Torch::AtenSqrtOp>(
+                      binder.op, resultType, operand);
+                  return success();
+                });
+  patterns.onOp(
+      "Sub", 1, [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+        Torch::ValueTensorType resultType;
+        Value x;
+        Value y;
+        if (binder.tensorOperands(x, y) || binder.tensorResultType(resultType))
+          return failure();
+        Value const1 = rewriter.create<Torch::ConstantIntOp>(
+            binder.getLoc(), rewriter.getType<Torch::IntType>(),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(64), 1));
+        rewriter.replaceOpWithNewOp<Torch::AtenSubTensorOp>(
+            binder.op, resultType, x, y, const1);
+        return success();
+      });
+  patterns.onOp(
+      "Sum", 1, [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+        Torch::ValueTensorType resultType;
+        Value x;
+        Value y;
+        if (binder.tensorOperands(x, y) || binder.tensorResultType(resultType))
+          return failure();
+        Value const1 = rewriter.create<Torch::ConstantIntOp>(
+            binder.getLoc(), rewriter.getType<Torch::IntType>(),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(64), 1));
+        rewriter.replaceOpWithNewOp<Torch::AtenAddTensorOp>(
+            binder.op, resultType, x, y, const1);
+        return success();
+      });
+  patterns.onOp("Where", 1,
+                [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+                  Torch::ValueTensorType resultType;
+                  Value condition;
+                  Value x;
+                  Value y;
+                  if (binder.tensorOperands(condition, x, y) ||
+                      binder.tensorResultType(resultType))
+                    return failure();
+                  rewriter.replaceOpWithNewOp<Torch::AtenWhereSelfOp>(
+                      binder.op, resultType, condition, x, y);
+                  return success();
+                });
+  patterns.onOp(
+      "Xor", 1, [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+        Torch::ValueTensorType resultType;
+        Value x;
+        Value y;
+        if (binder.tensorOperands(x, y) || binder.tensorResultType(resultType))
+          return failure();
+        rewriter.replaceOpWithNewOp<Torch::AtenLogicalXorOp>(binder.op,
+                                                             resultType, x, y);
+        return success();
+      });
+  patterns.onOp(
+      "Transpose", 1, [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+        Torch::ValueTensorType resultType;
+        Value x;
+        SmallVector<Value> perm;
+        if (binder.tensorOperand(x) || binder.tensorResultType(resultType))
+          return failure();
+        // info for debugging:
+        //
+        // this is how transpose is called:
+        // torch.operator "onnx.Transpose"(%arg0) {torch.onnx.perm = [0 : si64,
+        // 1 : si64, 2 : si64]} : (!torch.vtensor<[2,3,4],f32>) ->
+        // !torch.vtensor<[2,3,4],f32>
+        //
+        // binder.op->getAttr("torch.onnx.perm") is a mlir::Attribute
+        //
+        // crashes when I try following:
+        // llvm::outs() <<
+        // binder.op->getAttr("torch.onnx.perm").cast<TypedAttr>().getType();
+        //
+        // binder.op->getAttr("torch.onnx.perm").getImpl() is 0x565308362e00.
+        // (not sure what this means)
+        //
+        // fails on dyn_cast to ElementsAttr, DenseIntElementsAttr,
+        // DenseResourceElementsAttr, and DenseArrayAttr in following if
+        // statement:
+        //
+        // not sure how to go from mlir::Attribute to Torch list of ints through
+        // casting?
+        if (auto denseAttr = dyn_cast<DenseIntElementsAttr>(
+                binder.op->getAttr("torch.onnx.perm"))) {
+          llvm::outs() << "HERE\n";
+          // for (auto iter = denseAttr.begin(); iter != denseAttr.end();) {
+          //   int64_t elem = (*iter).extractBitsAsZExtValue(64, 0);
+          //   Value constElem = rewriter.create<Torch::ConstantIntOp>(
+          //     binder.getLoc(), rewriter.getType<Torch::IntType>(),
+          //     rewriter.getIntegerAttr(rewriter.getIntegerType(64), elem));
+          //   perm.push_back(constElem);
+          //   ++iter;
+          // }
+          return success();
+        } else {
+          llvm::outs() << "HERE FAILURE\n";
+          return failure();
+        }
+        Value dims = rewriter.create<Torch::PrimListConstructOp>(
+            binder.getLoc(),
+            Torch::ListType::get(Torch::IntType::get(binder.op->getContext())),
+            perm);
+        rewriter.replaceOpWithNewOp<Torch::AtenPermuteOp>(binder.op, resultType,
+                                                          x, dims);
+        return success();
+      });
+  patterns.onOp(
+      "Squeeze", 1, [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+        // TODO: still have to deal with negative dims and sorting the dims
+        // (highest to lowest) so that there are no problems such as: squeezing
+        // dim 0, then trying to squeeze dim 4 (should be dim 3 now)
+        llvm::outs() << "HERE\n";
+        Torch::ValueTensorType resultType;
+        Value data;
+        Value axes;
+        Value result;
+        if (binder.tensorOperands(data, axes) ||
+            binder.tensorResultType(resultType))
+          return failure();
+        Torch::BaseTensorType axesType =
+            axes.getType().cast<Torch::BaseTensorType>();
+        SmallVector<int64_t> selectSizes;
+        selectSizes.push_back(1);
+        Type selectResultType = axesType.getWithSizesAndDtype(
+            llvm::ArrayRef(selectSizes), axesType.getOptionalDtype());
+        auto sizes =
+            dyn_cast<Torch::ValueTensorType>(axes.getType()).getSizes();
+        for (int i = 0; i < sizes[0]; i++) {
+          Value selectAxis = rewriter.create<Torch::ConstantIntOp>(
+              binder.getLoc(), rewriter.getType<Torch::IntType>(),
+              rewriter.getIntegerAttr(rewriter.getIntegerType(64), 0));
+          Value selectIndex = rewriter.create<Torch::ConstantIntOp>(
+              binder.getLoc(), rewriter.getType<Torch::IntType>(),
+              rewriter.getIntegerAttr(rewriter.getIntegerType(64), i));
+          Value extract = rewriter.create<Torch::AtenSelectIntOp>(
+              binder.getLoc(), selectResultType, axes, selectAxis, selectIndex);
+          llvm::outs() << extract;
+          Value dim = rewriter.create<Torch::AtenItemOp>(
+              binder.getLoc(), rewriter.getType<Torch::IntType>(), extract);
+          result = rewriter.create<Torch::AtenSqueezeDimOp>(
+              binder.getLoc(), resultType, data, dim);
+        }
+        rewriter.replaceOp(binder.op, result);
+        return success();
+      });
 }
