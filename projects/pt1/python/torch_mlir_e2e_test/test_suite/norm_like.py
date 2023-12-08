e4bd78af40f9d3841f15345a4dec9d27d5ab7d4c
JianzheXiao <jianzhe.xiao@bytedance.com>
2023-12-08 15:53:56 -0800

    Update norm_like.py

--- projects/pt1/python/torch_mlir_e2e_test/test_suite/norm_like.py
+++ projects/pt1/python/torch_mlir_e2e_test/test_suite/norm_like.py
@@ -246,28 +246,6 @@ def NativeBatchNormNoneWeightModule_basic(module, tu: TestUtils):
 class GroupNormModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.gn = torch.nn.GroupNorm(2, 4)
-        self.gn.eval()
-        self.gn.weight = torch.nn.Parameter(torch.ones(4))
-        self.gn.bias = torch.nn.Parameter(torch.zeros(4))
-
-    @export
-    @annotate_args([
-        None,
-        ([2, 4, 6, 7], torch.float32, True),
-    ])
-    def forward(self, x):
-        return self.gn(x)
-
-@register_test_case(module_factory=lambda: GroupNormModule())
-def GroupNormModule_basic(module, tu: TestUtils):
-    module.forward(tu.rand(2, 4, 6, 7))
-
-# ==============================================================================
-
-class AtenGroupNormModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
 
     @export
     @annotate_args([
@@ -279,8 +257,8 @@ class AtenGroupNormModule(torch.nn.Module):
     def forward(self, x, weight, bias):
         return torch.ops.aten.group_norm(x, 2, weight, bias, 1.0000000000000001e-05, False)
 
-@register_test_case(module_factory=lambda: AtenGroupNormModule())
-def AtenGroupNormModule_basic(module, tu: TestUtils):
+@register_test_case(module_factory=lambda: GroupNormModule())
+def GroupNormModule_basic(module, tu: TestUtils):
     module.forward(tu.rand(2, 4, 6, 7), tu.rand(4), tu.rand(4))
 
 # ==============================================================================
