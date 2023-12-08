906846a927ab2e976d6dc6444de84813c28031a8
Stella Laurenzo <stellaraccident@gmail.com>
2023-12-07 22:04:59 -0800

    Fix version spec

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -21,7 +21,7 @@ LINALG_XFAIL_SET = COMMON_TORCH_MLIR_LOWERING_XFAILS | {
     "IscloseStaticModuleTrue_basic"
 }
 
-if torch_version_for_comparison() > version.parse("2.2.0.dev20231204"):
+if torch_version_for_comparison() >= version.parse("2.2.0.dev20231204"):
     LINALG_XFAIL_SET += {
         "Conv2dWithPaddingDilationStrideStaticModule_grouped",
         "Conv2dWithPaddingDilationStrideStaticModule_grouped_multiplier",
