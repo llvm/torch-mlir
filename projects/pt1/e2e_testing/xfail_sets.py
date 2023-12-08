35b753bf8a769413262b7179bcfe45d55a49604b
Felix Schneider <felix.schneider@fh-dortmund.de>
2023-12-08 11:30:40 +0100

    remove linalg xfails

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -23,14 +23,6 @@ LINALG_XFAIL_SET = COMMON_TORCH_MLIR_LOWERING_XFAILS | {
     "IscloseStaticModuleTrue_basic"
 }
 
-if torch_version_for_comparison() >= version.parse("2.2.0.dev20231204"):
-    LINALG_XFAIL_SET |= {
-        "Conv2dWithPaddingDilationStrideStaticModule_grouped",
-        "Conv2dWithPaddingDilationStrideStaticModule_grouped_multiplier",
-        "ConvolutionModule2DGroups_basic",
-    }
-
-
 TORCHDYNAMO_XFAIL_SET = {
     #### General TorchDynamo/PyTorch errors
 
