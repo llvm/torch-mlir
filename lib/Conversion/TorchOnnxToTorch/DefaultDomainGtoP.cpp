ae1a6e4a5a058170dd7b63001322844d04d12aac
Rob Suderman <suderman@google.com>
2023-12-16 10:47:58 -0800

    [onnx] Lower `onnx.Gemm` to `torch` (#2663)
    General lowering for `onnx.Gemm` to `torch`

--- lib/Conversion/TorchOnnxToTorch/DefaultDomainGtoP.cpp
+++ lib/Conversion/TorchOnnxToTorch/DefaultDomainGtoP.cpp
@@ -107,6 +107,80 @@ void mlir::torch::onnx_c::populateDefaultDomainGtoP(
             binder.op, resultType, data, constAxis, indices, sparseGrad);
         return success();
       });
+  patterns.onOp(
+      "Gemm", 13, [](OpBinder binder, ConversionPatternRewriter &rewriter) {
+        Torch::ValueTensorType resultType;
+        Value a, b, c;
+        float alpha, beta;
+        int64_t transA, transB;
+        if (binder.tensorOperandAtIndex(a, 0) ||
+            binder.tensorOperandAtIndex(b, 1) ||
+            binder.tensorOperandAtIndex(c, 2) ||
+            binder.s64IntegerAttr(transA, "transA", 0) ||
+            binder.s64IntegerAttr(transB, "transB", 0) ||
+            binder.f32FloatAttr(alpha, "alpha", 1.0) ||
+            binder.f32FloatAttr(beta, "beta", 1.0) ||
+            binder.tensorResultType(resultType))
+          return failure();
+
+        Value zero = rewriter.create<Torch::ConstantIntOp>(
+            binder.getLoc(), rewriter.getType<Torch::IntType>(),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(64), 0));
+        Value one = rewriter.create<Torch::ConstantIntOp>(
+            binder.getLoc(), rewriter.getType<Torch::IntType>(),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(64), 1));
+
+        auto transpose = [&](Value m) -> Value {
+          auto tty = m.getType().cast<Torch::ValueTensorType>();
+          auto shape = tty.getOptionalSizes();
+          if (shape.has_value()) {
+            llvm::SmallVector<int64_t> newShape(shape.value());
+            std::reverse(newShape.begin(), newShape.end());
+            shape = std::move(newShape);
+          }
+          auto oty = Torch::ValueTensorType::get(tty.getContext(), shape,
+                                                 tty.getOptionalDtype());
+          return rewriter.create<Torch::AtenTransposeIntOp>(binder.getLoc(),
+                                                            oty, m, zero, one);
+        };
+
+        if (transA) {
+          a = transpose(a);
+        }
+
+        if (transB) {
+          b = transpose(b);
+        }
+
+        Value mm =
+            rewriter.create<Torch::AtenMmOp>(binder.getLoc(), resultType, a, b);
+        if (alpha == 1.0 && beta == 1.0) {
+          rewriter.replaceOpWithNewOp<Torch::AtenAddTensorOp>(
+              binder.op, resultType, mm, c, one);
+          return success();
+        }
+
+        if (alpha != 1.0 && beta != 1.0) {
+          Value constAlpha = rewriter.create<Torch::ConstantFloatOp>(
+              binder.getLoc(), rewriter.getType<Torch::FloatType>(),
+              rewriter.getF64FloatAttr(alpha));
+          mm = rewriter.create<Torch::AtenMulScalarOp>(
+              binder.getLoc(), resultType, mm, constAlpha);
+          alpha = 1.0;
+        }
+
+        if (alpha != 1.0) {
+          std::swap(alpha, beta);
+          std::swap(mm, c);
+        }
+
+        Value constBeta = rewriter.create<Torch::ConstantFloatOp>(
+            binder.getLoc(), rewriter.getType<Torch::FloatType>(),
+            rewriter.getF64FloatAttr(beta));
+        rewriter.replaceOpWithNewOp<Torch::AtenAddTensorOp>(
+            binder.op, resultType, mm, c, constBeta);
+        return success();
+      });
   patterns.onOp("LeakyRelu", 16,
                 [](OpBinder binder, ConversionPatternRewriter &rewriter) {
                   Torch::ValueTensorType resultType;
