ec1db51b2d5c3d4bc1d8e9aa864d8122933330b7
ikalinic <ilija.kalinic@syrmia.com>
2024-01-22 13:11:04 +0000

    Fix ElementwiseLerpScalar test in elementwise.py

--- projects/pt1/python/torch_mlir_e2e_test/test_suite/elementwise.py
+++ projects/pt1/python/torch_mlir_e2e_test/test_suite/elementwise.py
@@ -559,32 +559,14 @@ class ElementwiseLerpScalarFloatModule(torch.nn.Module):
         None,
         ([-1, -1], torch.float32, True),
         ([-1, -1], torch.float32, True),
+        ([], torch.float32, True),
     ])
-    def forward(self, a, b):
-        return torch.ops.aten.lerp(a, b, weight=0.5)
+    def forward(self, a, b, c):
+        return torch.ops.aten.lerp(a, b, weight=c)
 
 @register_test_case(module_factory=lambda: ElementwiseLerpScalarFloatModule())
 def ElementwiseLerpScalarFloatModule_basic(module, tu: TestUtils):
-    module.forward(tu.rand(5,3), tu.rand(5,3))
-
-
-class ElementwiseLerpScalarIntModule(torch.nn.Module):
-
-    def __init__(self):
-        super().__init__()
-
-    @export
-    @annotate_args([
-        None,
-        ([-1, -1], torch.float32, True),
-        ([-1, -1], torch.float32, True),
-    ])
-    def forward(self, a, b):
-        return torch.ops.aten.lerp(a, b, weight=1)
-
-@register_test_case(module_factory=lambda: ElementwiseLerpScalarIntModule())
-def ElementwiseLerpScalarIntModule_basic(module, tu: TestUtils):
-    module.forward(tu.rand(5,3), tu.rand(5,3))
+    module.forward(tu.rand(5,3), tu.rand(5,3), torch.tensor(0.5, dtype=torch.float32))
 
 
 # ==============================================================================
