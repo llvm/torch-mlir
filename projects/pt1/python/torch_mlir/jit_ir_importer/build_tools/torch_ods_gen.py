114d87a6ac5a9aeb5ddcdb395d0f13c05a666d27
LiuYuanqiang <liuyuanqiang.yqliu@bytedance.com>
2024-01-24 11:40:20 +0800

    add another td file

--- projects/pt1/python/torch_mlir/jit_ir_importer/build_tools/torch_ods_gen.py
+++ projects/pt1/python/torch_mlir/jit_ir_importer/build_tools/torch_ods_gen.py
@@ -897,7 +897,8 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
     # 'c10d_functional' namespace.
     # ==========================================================================
 
-    emit("c10d_functional::all_reduce : (Tensor, str, str, int[], int) -> (Tensor)")
+    # uncomment bellow when upstream signature converge.
+    # emit("c10d_functional::all_reduce : (Tensor, str, str, int[], int) -> (Tensor)")
 
 def dump_registered_ops(outfile: TextIO, registry: Registry):
     for _, v in sorted(registry.by_unique_key.items()):
