b4c25ea0a4e5f0167ddf8d2ce8d1d9f912ea26c9
Rob Suderman <rob.suderman@gmail.com>
2024-01-24 15:40:30 -0800

    Add some LTC changes so tests pass

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -1540,4 +1540,5 @@ LTC_XFAIL_SET = {
     "ElementwiseBitwiseAndScalarInt64Module_basic",
     "ElementwiseBitwiseAndScalarInt32Module_basic",
     "ElementwiseBitwiseAndScalarInt8Module_basic",
+    "ElementwiseNanToNumModule_Basic",
 }
