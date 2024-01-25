71d8cf167925bde89edaef50ff98c48fa0d579fa
Rob Suderman <rob.suderman@gmail.com>
2024-01-25 11:50:47 -0800

    missing comma

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -313,7 +313,7 @@ TORCHDYNAMO_XFAIL_SET = {
     "GroupNormNoWeightAndBiasModule_basic",
 
     # Dynamo does not support tracing quantized tensors
-    "ElementwiseDequantizePerChannelModule_basic"
+    "ElementwiseDequantizePerChannelModule_basic",
     "ElementwiseDequantizePerTensorModule_basic",
     "ElementwiseQuantizePerTensorModule_basic",
     "AtenMmQuint8_basic",
