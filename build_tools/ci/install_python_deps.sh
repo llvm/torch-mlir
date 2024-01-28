297650f65b5504a043c06931023cb19fa1b4caa8
Stella Laurenzo <stellaraccident@gmail.com>
2024-01-27 16:45:38 -0800

    Try again

--- build_tools/ci/install_python_deps.sh
+++ build_tools/ci/install_python_deps.sh
@@ -6,22 +6,29 @@ this_dir="$(cd $(dirname $0) && pwd)"
 repo_root="$(cd $this_dir/../.. && pwd)"
 torch_version="${1:-unknown}"
 
-echo ":::: installing llvm python deps"
+echo "::group::installing llvm python deps"
 python -m pip install --no-cache-dir -r $repo_root/externals/llvm-project/mlir/python/requirements.txt
+echo "::endgroup::"
 
 case $torch_version in
   nightly)
-    echo ":::: installing nightly torch"
+    echo "::group::installing nightly torch"
     python3 -m pip install --no-cache-dir -r $repo_root/requirements.txt
     python3 -m pip install --no-cache-dir -r $repo_root/torchvision-requirements.txt
+    echo "::endgroup::"
     ;;
   stable)
-    echo ":::: installing stable torch"
+    echo "::group::installing stable torch"
     python3 -m pip install --no-cache-dir torch torchvision --index-url https://download.pytorch.org/whl/cpu
     python3 -m pip install --no-cache-dir -r $repo_root/build-requirements.txt
+    echo "::endgroup::"
     ;;
   *)
     echo "Unrecognized torch version '$torch_version' (specify 'nightly' or 'stable' with cl arg)"
     exit 1
     ;;
 esac
+
+echo "::group::installing test requirements"
+python -m pip install --no-cache-dir -r $repo_root/test_requirements.txt
+echo "::endgroup::"
