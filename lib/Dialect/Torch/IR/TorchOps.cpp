9af0234c7a14d637c91e0e93d0e540a5ab4bd752
Xida Ren <xida.ren.dev@gmail.com>
2024-02-01 02:09:55 +0000

    remove stray debug cmd

--- lib/Dialect/Torch/IR/TorchOps.cpp
+++ lib/Dialect/Torch/IR/TorchOps.cpp
@@ -2893,7 +2893,6 @@ OpFoldResult AtenItemOp::fold(FoldAdaptor adaptor) {
     if (!tensorType)
       return nullptr;
     if(!tensorType.hasDtype()){
-      tensorType.dump();
       return nullptr;
     }
     auto scalarType = tensorType.getDtype();
