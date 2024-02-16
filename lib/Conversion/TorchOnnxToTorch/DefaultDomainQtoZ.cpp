468c5339424e0f42d474106943c750de5519ff4d
Rob Suderman <suderman@google.com>
2024-02-16 13:04:47 -0800

    [onnx] Fix crash when negative transpose values exist (#2915)
    We are crashing due to indexing into a negative shape. Updated the
    lowering to avoid the crash.

--- lib/Conversion/TorchOnnxToTorch/DefaultDomainQtoZ.cpp
+++ lib/Conversion/TorchOnnxToTorch/DefaultDomainQtoZ.cpp
@@ -1447,7 +1447,17 @@ void mlir::torch::onnx_c::populateDefaultDomainQtoZ(
           current[i] = i;
         }
 
-        // Convert dynamic shape dimension.
+        for (auto &dim : permutations)
+          dim = dim < 0 ? dim + rank : dim;
+
+        // We need to override to the destination if known:
+        if (resultType.hasSizes()) {
+          for (int i = 0; i < rank; ++i) {
+            shape[permutations[i]] = resultType.getSizes()[i];
+          }
+        }
+
+        // Convert dynamic shape dimension:
         for (unsigned i = 0; i < shape.size(); i++) {
           if (shape[i] == ShapedType::kDynamic)
             shape[i] = Torch::kUnknownSize;
