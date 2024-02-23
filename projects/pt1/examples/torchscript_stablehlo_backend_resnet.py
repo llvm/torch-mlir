2984492383115b5d775eeb06fad90e8d04c5111d
VimalWill <vimalwilliam99@gmail.com>
2024-02-23 23:18:44 +0530

    updated examples based on the current init from torch_mlir

--- projects/pt1/examples/torchscript_stablehlo_backend_resnet.py
+++ projects/pt1/examples/torchscript_stablehlo_backend_resnet.py
@@ -1,13 +1,13 @@
 import torch
 import torchvision.models as models
-from torch_mlir import torchscript
+from torch_mlir import compile, OutputType
 
 model = models.resnet18(pretrained=True)
 model.eval()
 data = torch.randn(2,3,200,200)
 out_stablehlo_mlir_path = "./resnet18_stablehlo.mlir"
 
-module = torchscript.compile(model, data, output_type=torchscript.OutputType.STABLEHLO, use_tracing=False)
+module = compile(model, data, output_type=OutputType.STABLEHLO, use_tracing=False)
 with open(out_stablehlo_mlir_path, "w", encoding="utf-8") as outf:
     outf.write(str(module))
 
--- projects/pt1/examples/torchscript_stablehlo_backend_tinybert.py
+++ projects/pt1/examples/torchscript_stablehlo_backend_tinybert.py
@@ -1,5 +1,5 @@
 import torch
-from torch_mlir import torchscript
+from torch_mlir import compile, OutputType
 
 from transformers import BertForMaskedLM
 
@@ -17,7 +17,7 @@ model.eval()
 data = torch.randint(30522, (2, 128))
 out_stablehlo_mlir_path = "./bert_tiny_stablehlo.mlir"
 
-module = torchscript.compile(model, data, output_type=torchscript.OutputType.STABLEHLO, use_tracing=True)
+module = compile(model, data, output_type=OutputType.STABLEHLO, use_tracing=True)
 with open(out_stablehlo_mlir_path, "w", encoding="utf-8") as outf:
     outf.write(str(module))
 
