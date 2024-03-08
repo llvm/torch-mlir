0d50ec2ed5760855ee644740fec99d0b9bd4489d
penguin-wwy <940375606@qq.com>
2024-03-08 17:43:38 +0800

    [dynamo] Verify the default value is passed by kwargs

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -262,9 +262,7 @@ TORCHDYNAMO_XFAIL_SET = {
     "ElementwiseSubScalarFloatModule_basic",
     "ElementwiseSubScalarIntModule_basic",
 
-    # ERROR: Exception: Unsupported: missing default value for argument 0 in schema for aten.div.Tensor_mode
-    "ElementwiseDivRoundingModeFloorModule_basic",
-    "ElementwiseDivRoundingModeTruncModule_basic",
+    # ERROR: 'torch.aten.mul.Tensor' op operand #1 must be Any Torch tensor type, but got '!torch.int'
     "AdaptiveAvgPool1dStaticLargerOutput_basic",
     "AdaptiveAvgPool1dGeneralDynamic_basic",
 
@@ -273,10 +271,6 @@ TORCHDYNAMO_XFAIL_SET = {
     "NumToTensorIntModule_basic",
     "TensorFloatModule_basic",
     "TensorIntModule_basic",
-
-    # ERROR: Exception: Unsupported: missing default value for argument 0 in schema for aten.randn.generator
-    "RandnGeneratorF64Module_basic",
-    "RandnGeneratorModule_basic",
 
     # START tests failing due to: complex floating point ops
     # END tests failing due to: complex floating point ops
