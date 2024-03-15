fb41d55a0bac7f8c1ea63ee1fc9e54b985d58f30
huangma <huangma@chipltech.com>
2024-03-15 14:32:11 +0800

    add type annotation

--- test/python/fx_importer/aten_to_linalg.py
+++ test/python/fx_importer/aten_to_linalg.py
@@ -1,6 +1,7 @@
 import os
 from torch_mlir import fx
 import torch
+from torch import Tensor
 
 from torch_mlir.compiler_utils import run_pipeline_with_repro_report
 
@@ -22,14 +23,17 @@ def save_mlir(module, name, ir):
         f.write(module_strs)
 
 class Transform:
-    def __init__(self, f, args):
+    def __init__(self, f, *args, **kwargs):
         self.f = f
         self.kernel_name = f.__class__.__name__
         self.args = args
+        self.kwargs = kwargs
         self.module = None
     
     def get_torchIR(self):
-        self.module = fx.export_and_import(self.f, self.args)
+        self.module = fx.export_and_import(self.f, *self.args, **self.kwargs)
+        print("aten ir:")
+        print(self.module)
         save_mlir(self.module, self.kernel_name, "torch-aten")
 
     def lower_linalg(self):
@@ -43,27 +47,52 @@ class Transform:
             "Lowering TorchFX IR -> Linalg IR",
             enable_ir_printing=False,
         )
+        print("linalg ir:")
+        print(self.module)
         save_mlir(self.module, self.kernel_name, "linalg-ir")
     
     def run(self):
         self.get_torchIR()
         self.lower_linalg()
-        return self.module
+        # return self.module
 
 ################################################################
 #  Add torch kernel example
 ################################################################
+        
+# error TODO
+# python exception: Failure while executing pass pipeline:
+# error: unknown: failed to legalize operation 'torch.constant.int'
+# note: unknown: see current operation: %0 = "torch.constant.int"() <{value = 1 : i64}> : () -> !torch.int
+# @run
+def test_index_add():
+    class Index_add(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+        def forward(self, x:Tensor, dim, index:Tensor, source:Tensor) -> Tensor:
+            return x.index_add_(dim, index, source)
+    
+    index_add = Transform(
+        Index_add(), 
+        torch.randn(128, 128, dtype=torch.float), 
+        0,
+        torch.tensor([8, 16, 32, 64, 127, 48, 72, 96], dtype=torch.int),
+        torch.randn(8, 128, dtype=torch.float),
+        )
+    index_add.run()
+
 @run
 def test_sigmoid():
     class Sigmoid(torch.nn.Module):
         def __init__(self):
             super().__init__()
 
-        def forward(self, x):
+        def forward(self, x:Tensor) -> Tensor:
             return torch.sigmoid(x)
 
-    sigmoid = Transform(Sigmoid(), torch.randn(8, 128))
-    module = sigmoid.run()
+    sigmoid = Transform(Sigmoid(), torch.randn(128, 128))
+    sigmoid.run()
 
 @run
 def test_softmax():
@@ -71,9 +100,8 @@ def test_softmax():
         def __init__(self):
             super().__init__()
 
-        def forward(self, x):
-            return torch.nn.functional.softmax(x, -1, torch.float32)
-    softmax = Transform(Softmax(), torch.randn(8, 128))
-    module = softmax.run()
-    # print("====================================")
-    # print(module)
+        def forward(self, x:Tensor, dim:int) -> Tensor:
+            return torch.nn.functional.softmax(x, dim, torch.float32)
+    softmax = Transform(Softmax(), torch.randn(128, 128), 1)
+    softmax.run()
+
