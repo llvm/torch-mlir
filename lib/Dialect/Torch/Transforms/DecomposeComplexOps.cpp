9e0c455e2af4db412c60354b3f613266d4a44929
Xida Ren <xida.ren.dev@gmail.com>
2024-04-22 19:50:45 +0000

    prioritize getting shapes from output shapes and use kUnknown when no shape is available

--- lib/Dialect/Torch/Transforms/DecomposeComplexOps.cpp
+++ lib/Dialect/Torch/Transforms/DecomposeComplexOps.cpp
@@ -1073,30 +1073,28 @@ public:
         rewriter.getIntegerType(/*width=*/64, /*isSigned=*/true));
     auto si64Type = IntegerType::get(context, 64, IntegerType::Signed);
 
-    int64_t n;
-    Type rangeNType;
-    if (matchPattern(op.getN(), m_TorchConstantInt(&n))) {
-      rangeNType = outType.getWithSizesAndDtype(std::nullopt, si64Type);
-    } else {
-      if (n < 0)
-        return rewriter.notifyMatchFailure(op,
-                                           "n must be greater or equal to 0");
-      rangeNType = outType.getWithSizesAndDtype(llvm::ArrayRef(n), si64Type);
+    int64_t n = kUnknownSize;
+    int64_t m = kUnknownSize;
+    // prioritize getting shape from output shape
+    if (outType.hasSizes()) {
+      n = outType.getSizes().front();
+      m = outType.getSizes().back();
     }
+    // if output shape is not available, try to get shape from input
+    if (n == kUnknownSize) {
+      matchPattern(op.getN(), m_TorchConstantInt(&n));
+    }
+    if (m == kUnknownSize) {
+      matchPattern(op.getM(), m_TorchConstantInt(&m));
+    }
+    Type rangeNType = outType.getWithSizesAndDtype(
+        n == kUnknownSize ? std::nullopt : llvm::ArrayRef(n), si64Type);
     Value rangeN = rewriter.create<AtenArangeOp>(
         loc, rangeNType, op.getN(), /*dtype=*/int64Dtype, /*layout=*/none,
         /*device=*/op.getDevice(), /*pin_memory=*/none);
 
-    int64_t m;
-    Type rangeMType;
-    if (matchPattern(op.getM(), m_TorchConstantInt(&m))) {
-      rangeMType = outType.getWithSizesAndDtype(std::nullopt, si64Type);
-    } else {
-      if (m < 0)
-        return rewriter.notifyMatchFailure(op,
-                                           "m must be greater or equal to 0");
-      rangeMType = outType.getWithSizesAndDtype(llvm::ArrayRef(m), si64Type);
-    }
+    Type rangeMType = outType.getWithSizesAndDtype(
+        m == kUnknownSize ? std::nullopt : llvm::ArrayRef(m), si64Type);
     Value rangeM = rewriter.create<AtenArangeOp>(
         loc, rangeMType, op.getM(), /*dtype=*/int64Dtype, /*layout=*/none,
         /*device=*/none, /*pin_memory=*/none);
