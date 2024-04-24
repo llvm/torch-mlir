627095b7beab4ab70948a30c780e7bfa57887b56
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2024-04-24 16:49:19 +0000

    Disable LTC build

--- build_tools/ci/build_posix.sh
+++ build_tools/ci/build_posix.sh
@@ -50,7 +50,6 @@ cmake -S "$repo_root/externals/llvm-project/llvm" -B "$build_dir" \
   -DLLVM_EXTERNAL_TORCH_MLIR_SOURCE_DIR="$repo_root" \
   -DLLVM_TARGETS_TO_BUILD=host \
   -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
-  -DTORCH_MLIR_ENABLE_LTC=ON
 echo "::endgroup::"
 
 echo "::group::Build"
