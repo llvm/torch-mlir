52d6f3e61f0f4b8b4c3cc932adaf83ed26ff43b2
Xida Ren <cedar.ren@gmail.com>
2024-04-30 17:45:59 +0000

    black

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -2565,10 +2565,9 @@ ONNX_XFAIL_SET = {
     # Failure - onnx_import
     # Failure - onnx_lowering: onnx.AveragePool
     "AdaptiveAvgPool1dGeneralDynamicNoBatches_basic",
-  
     # these diagonal modules are currently failing due to dynamic shape.
     # We are currently testing aten.diagonal using DiagonalWithStaticShapeModule instead.
-    # when the issue is fixed, please remove DiagonalWithStaticShapeModule as well as the xfails here. 
+    # when the issue is fixed, please remove DiagonalWithStaticShapeModule as well as the xfails here.
     "DiagonalModule_basic",
     "DiagonalModule_nonsquare",
     "DiagonalModule_transposed",
