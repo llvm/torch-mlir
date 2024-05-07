c3bd850951c0e84c94f9ec2e7f14605d9c7104ea
penguin_wwy <940375606@qq.com>
2024-05-07 20:58:50 +0800

    [FxImporter] Add backend lowering to Fx API (#3288)

--- python/torch_mlir/fx.py
+++ python/torch_mlir/fx.py
@@ -16,11 +16,50 @@ from torch_mlir.extras.fx_importer import FxImporter, FxImporterHooks
 from torch_mlir import ir
 from torch_mlir.dialects import torch as torch_d
 from torch_mlir.extras.fx_decomp_util import get_decomposition_table
+from torch_mlir.compiler_utils import (
+    OutputType,
+    run_pipeline_with_repro_report,
+    lower_mlir_module,
+)
+
+
+def _module_lowering(
+    verbose,
+    output_type,
+    torch_mod,
+    backend_legal_ops=None,
+    extra_library_file_name=None,
+):
+
+    if output_type == OutputType.TORCH:
+        if verbose:
+            print(torch_mod)
+        return torch_mod
+    # TODO: pass backend_legal_ops/extra_library_file_name by caller
+    if backend_legal_ops is None:
+        backend_legal_ops = []
+    if extra_library_file_name is None:
+        extra_library_file_name = ""
+    option_string = (
+        "{backend-legal-ops="
+        + ",".join(backend_legal_ops)
+        + " extra-library="
+        + extra_library_file_name
+        + "}"
+    )
+    run_pipeline_with_repro_report(
+        torch_mod,
+        f"builtin.module(torch-function-to-torch-backend-pipeline{option_string})",
+        "Lowering TorchFX IR -> Torch Backend IR",
+        enable_ir_printing=verbose,
+    )
+    return lower_mlir_module(verbose, output_type, torch_mod)
 
 
 def export_and_import(
     f: Union[nn.Module, ExportedProgram],
     *args,
+    output_type: Union[str, OutputType] = OutputType.TORCH,
     fx_importer: Optional[FxImporter] = None,
     dynamic_shapes: Optional[Union[Dict[str, Any], Tuple[Any]]] = None,
     experimental_support_mutation: bool = False,
@@ -28,6 +67,7 @@ def export_and_import(
     decomposition_table: Optional[Dict[torch._ops.OperatorBase, Callable]] = None,
     func_name: str = "main",
     enable_graph_printing: bool = False,
+    enable_ir_printing: bool = False,
     **kwargs,
 ):
     context = ir.Context()
@@ -52,15 +92,19 @@ def export_and_import(
     else:
         fx_importer.import_frozen_program(prog, func_name=func_name)
 
-    return fx_importer.module
+    return _module_lowering(
+        enable_ir_printing, OutputType.get(output_type), fx_importer.module
+    )
 
 
 def stateless_fx_import(
     gm: torch.fx.GraphModule,
+    output_type: Union[str, OutputType] = OutputType.TORCH,
     fx_importer: Optional[FxImporter] = None,
     hooks: Optional[FxImporterHooks] = None,
     model_name: str = "main",
     enable_graph_printing: bool = False,
+    enable_ir_printing: bool = False,
 ):
     if enable_graph_printing:
         gm.print_readable()
@@ -69,4 +113,6 @@ def stateless_fx_import(
     if fx_importer is None:
         fx_importer = FxImporter(context=context, hooks=hooks)
     fx_importer.import_stateless_graph(gm.graph, func_name=model_name)
-    return fx_importer.module
+    return _module_lowering(
+        enable_ir_printing, OutputType.get(output_type), fx_importer.module
+    )
