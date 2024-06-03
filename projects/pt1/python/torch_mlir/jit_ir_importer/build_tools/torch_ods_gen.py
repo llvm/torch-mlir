30285efc149751b6fe1737c5e6f0dded6e93ad55
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2024-06-03 16:03:32 +0000

    [ONNX] Add OnnxToTorch Lowering for MaxUnpool op
    This commit also adds the Torch declaration for aten.max_unpool2d
    and aten.max_unpool3d op. The TorchToLinalg lowering for the same
    will be added in a follow-up commit.
    
    Signed-Off By: Vivek Khandelwal <vivekkhandelwal1424@gmail.com>

--- projects/pt1/python/torch_mlir/jit_ir_importer/build_tools/torch_ods_gen.py
+++ projects/pt1/python/torch_mlir/jit_ir_importer/build_tools/torch_ods_gen.py
@@ -597,6 +597,7 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
     )
     emit("aten::max_pool1d : (Tensor, int[], int[], int[], int[], bool) -> (Tensor)")
     emit("aten::max_pool2d : (Tensor, int[], int[], int[], int[], bool) -> (Tensor)")
+    emit("aten::max_unpool2d : (Tensor, Tensor, int[]) -> (Tensor)")
     emit(
         "aten::max_pool2d_with_indices : (Tensor, int[], int[], int[], int[], bool) -> (Tensor, Tensor)",
         has_canonicalizer=True,
@@ -605,6 +606,7 @@ def emit_ops(emitter_td: TextEmitter, registry: Registry):
         "aten::max_pool2d_with_indices_backward : (Tensor, Tensor, int[], int[], int[], int[], bool, Tensor) -> (Tensor)"
     )
     emit("aten::max_pool3d : (Tensor, int[], int[], int[], int[], bool) -> (Tensor)")
+    emit("aten::max_unpool3d : (Tensor, Tensor, int[], int[], int[]) -> (Tensor)")
     emit(
         "aten::max_pool3d_with_indices : (Tensor, int[], int[], int[], int[], bool) -> (Tensor, Tensor)"
     )
