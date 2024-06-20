77f4eeade2d8007b13689890baa784b696cb6c19
Ian Wood <ianwood2024@u.northwestern.edu>
2024-06-20 17:31:24 +0000

    remove extract slice from grid sample

--- lib/Conversion/TorchToLinalg/Uncategorized.cpp
+++ lib/Conversion/TorchToLinalg/Uncategorized.cpp
@@ -7,6 +7,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "torch-mlir/Conversion/TorchToLinalg/TorchToLinalg.h"
 
 #include "PopulatePatterns.h"
@@ -2380,7 +2381,6 @@ public:
     Type floatType = rewriter.getF32Type();
     Value zeroIndex = rewriter.create<arith::ConstantIndexOp>(loc, 0);
     Value oneIndex = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-    Value twoIndex = rewriter.create<arith::ConstantIndexOp>(loc, 2);
     Value zeroFloat = rewriter.create<arith::ConstantOp>(
         loc, rewriter.getFloatAttr(floatType, 0.0));
     Value oneFloat = rewriter.create<arith::ConstantOp>(
@@ -2389,7 +2389,6 @@ public:
         loc, rewriter.getFloatAttr(floatType, 2.0));
     Value input = adaptor.getInput();
     auto inputType = cast<RankedTensorType>(input.getType());
-    auto inputShape = inputType.getShape();
     Value innerDim0a = rewriter.create<tensor::DimOp>(loc, input, 2);
     Value innerDim1a = rewriter.create<tensor::DimOp>(loc, input, 3);
     Value innerDim0b =
@@ -2410,43 +2409,12 @@ public:
         rewriter.create<arith::DivFOp>(loc, innerDim1d, twoFloat);
     Value grid = adaptor.getGrid();
     auto gridType = cast<RankedTensorType>(grid.getType());
-    auto gridShape = gridType.getShape();
     auto gridRank = gridType.getRank();
-    SmallVector<Value> extractGridOffsets0(gridRank, zeroIndex);
-    SmallVector<Value> extractGridShape = getTensorSizes(rewriter, loc, grid);
-    SmallVector<Value> extractGridStride(gridRank, oneIndex);
-    int64_t lastGridDim = gridRank - 1;
-    extractGridShape[lastGridDim] = oneIndex;
-    extractGridStride[lastGridDim] = twoIndex;
-    SmallVector<Value> extractGridOffsets1(gridRank, zeroIndex);
-    extractGridOffsets1[lastGridDim] = oneIndex;
-    SmallVector<int64_t> gridShapeExtracted(gridShape);
-    gridShapeExtracted.back() = 1;
-    SmallVector<int64_t> gridShapeCollapsed{gridShape[0], gridShape[1],
-                                            gridShape[2]};
-    auto grid0 = rewriter.create<tensor::ExtractSliceOp>(
-        loc, grid, extractGridOffsets0, extractGridShape, extractGridStride);
-    auto grid1 = rewriter.create<tensor::ExtractSliceOp>(
-        loc, grid, extractGridOffsets1, extractGridShape, extractGridStride);
-    SmallVector<ReassociationIndices> associations{ReassociationIndices{0},
-                                                   ReassociationIndices{1},
-                                                   ReassociationIndices{2, 3}};
-    auto gridCollapsed0 =
-        rewriter.create<tensor::CollapseShapeOp>(loc, grid0, associations);
-    auto gridCollapsed1 =
-        rewriter.create<tensor::CollapseShapeOp>(loc, grid1, associations);
-    AffineMap gridMap = AffineMap::get(4, 0,
-                                       {rewriter.getAffineDimExpr(0),
-                                        rewriter.getAffineDimExpr(2),
-                                        rewriter.getAffineDimExpr(3)},
-                                       op->getContext());
-    SmallVector<AffineMap> gridMaps{gridMap, gridMap,
-                                    rewriter.getMultiDimIdentityMap(gridRank)};
+    SmallVector<AffineMap> gridMaps{
+        rewriter.getMultiDimIdentityMap(inputType.getRank())};
     SmallVector<utils::IteratorType> gridIterators(
         gridRank, utils::IteratorType::parallel);
-    SmallVector<int64_t> resultShape{inputShape[0], inputShape[1], gridShape[1],
-                                     gridShape[2]};
-    auto lambdaExtract = [](OpBuilder &b, Location loc, Value input, Value idxA,
+    auto createExtract = [](OpBuilder &b, Location loc, Value input, Value idxA,
                             Value idxB, Value idxC, Value idxD) -> Value {
       SmallVector<Value> index{idxA, idxB, idxC, idxD};
       Value result = b.create<tensor::ExtractOp>(loc, input, index);
@@ -2486,25 +2454,30 @@ public:
 
     auto resultType = cast<RankedTensorType>(
         getTypeConverter()->convertType(op.getResult().getType()));
-    SmallVector<Value> resultSize{};
-    if (resultType.isDynamicDim(0))
-      resultSize.push_back(rewriter.create<tensor::DimOp>(loc, input, 0));
-    if (resultType.isDynamicDim(1))
-      resultSize.push_back(rewriter.create<tensor::DimOp>(loc, input, 1));
-    if (resultType.isDynamicDim(2))
-      resultSize.push_back(rewriter.create<tensor::DimOp>(loc, grid, 1));
-    if (resultType.isDynamicDim(3))
-      resultSize.push_back(rewriter.create<tensor::DimOp>(loc, grid, 2));
     Value alignCorners = adaptor.getAlignCorners();
     Value interMode = adaptor.getInterpolationMode();
-    Value resultFinal =
-        rewriter.create<tensor::EmptyOp>(loc, resultType, resultSize);
+    SmallVector<Value> dynamicSizes{};
+    if (resultType.isDynamicDim(0))
+      dynamicSizes.push_back(rewriter.create<tensor::DimOp>(loc, input, 0));
+    if (resultType.isDynamicDim(1))
+      dynamicSizes.push_back(rewriter.create<tensor::DimOp>(loc, input, 1));
+    if (resultType.isDynamicDim(2))
+      dynamicSizes.push_back(rewriter.create<tensor::DimOp>(loc, grid, 1));
+    if (resultType.isDynamicDim(3))
+      dynamicSizes.push_back(rewriter.create<tensor::DimOp>(loc, grid, 2));
+    tensor::EmptyOp emptyOp =
+        rewriter.create<tensor::EmptyOp>(loc, resultType, dynamicSizes);
     auto sGrid = rewriter.create<linalg::GenericOp>(
-        loc, TypeRange{resultType}, ValueRange{gridCollapsed0, gridCollapsed1},
-        ValueRange(resultFinal), gridMaps, gridIterators,
-        [&](OpBuilder &b, Location loc, ValueRange args) {
-          Value gr0 = args[1];
-          Value gr1 = args[0];
+        loc, TypeRange{resultType}, ValueRange(), ValueRange(emptyOp), gridMaps,
+        gridIterators, [&](OpBuilder &b, Location loc, ValueRange args) {
+          Value iterIdxZero = b.create<linalg::IndexOp>(loc, 0);
+          Value iterIdxTwo = b.create<linalg::IndexOp>(loc, 2);
+          Value iterIdxThree = b.create<linalg::IndexOp>(loc, 3);
+
+          Value gr1 = createExtract(b, loc, grid, iterIdxZero, iterIdxTwo,
+                                    iterIdxThree, zeroIndex);
+          Value gr0 = createExtract(b, loc, grid, iterIdxZero, iterIdxTwo,
+                                    iterIdxThree, oneIndex);
           Value gr0Half = b.create<arith::DivFOp>(loc, gr0, twoFloat);
           Value gr1Half = b.create<arith::DivFOp>(loc, gr1, twoFloat);
           Value gr0HalfSelect =
@@ -2561,22 +2534,22 @@ public:
               b.create<arith::IndexCastOp>(loc, b.getIndexType(), upperValid1);
           Value N = b.create<linalg::IndexOp>(loc, 0);
           Value C = b.create<linalg::IndexOp>(loc, 1);
-          Value result00 = lambdaExtract(b, loc, input, N, C, lw0, lw1);
+          Value result00 = createExtract(b, loc, input, N, C, lw0, lw1);
           Value result00a = b.create<arith::SelectOp>(loc, checkLowerBound0,
                                                       zeroFloat, result00);
           Value result00b = b.create<arith::SelectOp>(loc, checkLowerBound1,
                                                       zeroFloat, result00a);
-          Value result01 = lambdaExtract(b, loc, input, N, C, lw0, up1);
+          Value result01 = createExtract(b, loc, input, N, C, lw0, up1);
           Value result01a = b.create<arith::SelectOp>(loc, notValidUpper1,
                                                       zeroFloat, result01);
           Value result01b = b.create<arith::SelectOp>(loc, checkLowerBound0,
                                                       zeroFloat, result01a);
-          Value result10 = lambdaExtract(b, loc, input, N, C, up0, lw1);
+          Value result10 = createExtract(b, loc, input, N, C, up0, lw1);
           Value result10a = b.create<arith::SelectOp>(loc, notValidUpper0,
                                                       zeroFloat, result10);
           Value result10b = b.create<arith::SelectOp>(loc, checkLowerBound1,
                                                       zeroFloat, result10a);
-          Value result11 = lambdaExtract(b, loc, input, N, C, up0, up1);
+          Value result11 = createExtract(b, loc, input, N, C, up0, up1);
           Value result11a = b.create<arith::SelectOp>(loc, notValidUpper0,
                                                       zeroFloat, result11);
           Value result11b = b.create<arith::SelectOp>(loc, notValidUpper1,
