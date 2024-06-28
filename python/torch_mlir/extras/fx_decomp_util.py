6fece25ff3203bbc538756beb83fd513c19bcd7d
Aart Bik <ajcbik@google.com>
2024-06-28 10:18:36 -0700

    [torch-mlir][sparse] add decomposition features to sparse compiler (#3505)
    Fixes https://github.com/llvm/torch-mlir/issues/3499

--- python/torch_mlir/extras/fx_decomp_util.py
+++ python/torch_mlir/extras/fx_decomp_util.py
@@ -49,6 +49,7 @@ DEFAULT_DECOMPOSITIONS = [
     torch.ops.aten.nan_to_num.default,
     torch.ops.aten.unbind,
     torch.ops.aten._scaled_dot_product_flash_attention_for_cpu,
+    torch.ops.aten.diag,
 ]
 
 
