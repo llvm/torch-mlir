0624fd4f93a2c4e04489088002e1528b21ad3f61
Rob Suderman <rob.suderman@gmail.com>
2024-08-05 09:44:37 -0700

    fix build error

--- lib/Conversion/TorchOnnxToTorch/DefaultDomainQtoZ.cpp
+++ lib/Conversion/TorchOnnxToTorch/DefaultDomainQtoZ.cpp
@@ -1610,8 +1610,8 @@ void mlir::torch::onnx_c::populateDefaultDomainQtoZ(
 
         int64_t start = 0;
         int64_t end = -1;
-        if (binder.optionalS64IntegerAttr(start, "start", 0) ||
-            binder.optionalS64IntegerAttr(end, "end", -1))
+        if (binder.optionalS64IntegerAttr(start, "start") ||
+            binder.optionalS64IntegerAttr(end, "end"))
           return failure();
 
         if (start == 0 && end == -1) {
