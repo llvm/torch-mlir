8ab7076521720ddeb6fbe74d12ce4baa0a885a79
Vimal Patel <vimal@polymagelabs.com>
2024-08-22 10:48:49 +0530

    [FxImporter] Fix sympy_int_to_int utility
    New sympy type is introduced to represent integer infinity in upstream
    PyTorch repo. Subsequently, sympy.oo is no longer used to represent
    infinity upper bound for dynamic dimensions where the upper bound is
    unknown. Instead `int_oo` is used to represent integer infinity. This
    commit updates the `_sympy_int_to_int` utility in light of this change.

--- python/torch_mlir/extras/fx_importer.py
+++ python/torch_mlir/extras/fx_importer.py
@@ -78,6 +78,15 @@ except ModuleNotFoundError:
     # conditional.
     ml_dtypes = None
 
+try:
+    from torch.utils._sympy.numbers import IntInfinity, NegativeIntInfinity
+except ModuleNotFoundError:
+    # This commit on PyTorch repo introduced IntInfinity and NegativeIntInfinity:
+    # https://github.com/pytorch/pytorch/commit/2229884102ac95c9dda0aeadbded1b04295d892e
+    # Required module may not be present in the stable version of PyTorch.
+    IntInfinity = None
+    NegativeIntInfinity = None
+
 from torch.fx.node import (
     Argument as NodeArgument,
 )
@@ -272,6 +281,11 @@ PY_BUILTIN_TO_TORCH_OP = {
 # torch with cuda has a __version__ that looks like  "2.1.0+cu113",
 # so split by + and 0 index will always give the base version
 _IS_TORCH_2_1_OR_EARLIER = torch.__version__.split("+")[0] <= "2.1.0"
+
+# The value here is chosen to match that of sys.maxint (or sys.maxsize in case
+# of Python3.x). This is to maintain compatibility with older version of PyTorch.
+MLIR_INT_INFINITY = (2**63 - 1) - 1
+MLIR_NEGATIVE_INT_INFINITY = -MLIR_INT_INFINITY - 1
 
 # The following are maps from symbolic ops to their non symbolic equivalents.
 # In <=2.1.0, imported fx graphs come with a type inspecific torch.ops.aten.sym_size
@@ -1167,20 +1181,26 @@ class ContextCache:
 
         def _sympy_int_to_int(val: sympy.Expr, adjust_func: Callable):
             # Convert simple sympy Integers into concrete int
-            if val == sympy.oo:
-                return math.inf
-            if val == -sympy.oo:
-                return -math.inf
+            if IntInfinity is not None and isinstance(val, IntInfinity):
+                return MLIR_INT_INFINITY
+            if NegativeIntInfinity is not None and isinstance(val, NegativeIntInfinity):
+                return MLIR_NEGATIVE_INT_INFINITY
             if isinstance(val, sympy.Integer):
                 return int(val)
+            if val == sympy.oo:
+                return MLIR_INT_INFINITY
+            if val == -sympy.oo:
+                return MLIR_NEGATIVE_INT_INFINITY
             # TODO: Remove this adjustment when fractional ranges are removed
             return adjust_func(val)
 
         contains_symbolic_ints = False
         for val in prog.range_constraints.values():
             if (
-                isinstance(val.lower, sympy.Integer)
-                and isinstance(val.upper, sympy.Integer)
+                isinstance(val.lower, (sympy.Integer, IntInfinity, NegativeIntInfinity))
+                and isinstance(
+                    val.upper, (sympy.Integer, IntInfinity, NegativeIntInfinity)
+                )
                 and not val.is_bool
             ):
                 contains_symbolic_ints = True
