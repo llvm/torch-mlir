80c3d354bd61a9b98fc4bbbaa1d38d252e6bf50e
Xida Ren <cedar.ren@gmail.com>
2024-08-26 16:09:09 +0000

    fix copypaste artifact

--- lib/Dialect/Torch/Transforms/RestructureNonConstantAxes.cpp
+++ lib/Dialect/Torch/Transforms/RestructureNonConstantAxes.cpp
@@ -1,4 +1,5 @@
-//===- LowerToBackendContract.cpp --------------------------------*- C++-*-===//
+//===- RestructureNonConstantAxes.cpp --------------------------------*-
+// C++-*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
