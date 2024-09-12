4bb8b4a674e0e8ebbb28903f2e3cdb69ab466dbd
saienduri <77521230+saienduri@users.noreply.github.com>
2024-09-12 15:29:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -225,8 +225,10 @@ EXT_MODULES = [
 ]
 NAME = "torch-mlir-core"
 
+print("TEST")
 # If building PyTorch extensions, customize.
 if not TORCH_MLIR_ENABLE_ONLY_MLIR_PYTHON_BINDINGS:
+    print("HEREE")
     import torch
 
     NAME = "torch-mlir"
