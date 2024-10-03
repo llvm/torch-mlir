a39aa5a959a76ae7807b25f97cbea574fe9a74e6
Rob Suderman <rob.suderman@gmail.com>
2024-10-03 10:26:10 -0700

    disable on stablehlo fx importer

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -687,6 +687,7 @@ FX_IMPORTER_STABLEHLO_XFAIL_SET = {
     "DivFloatModule_basic",
     "DivIntModule_basic",
     "ElementwiseAddScalar_NumToTensorFloat_Module_basic",
+    "ElementwiseCreateComplexModule",
     "ElementwiseDequantizePerChannelModule_basic",
     "ElementwiseDequantizePerTensorModule_basic",
     "ElementwiseErfIntModule_basic",
