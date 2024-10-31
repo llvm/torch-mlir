c17184ba0d33719e0209c4df314c1f795d30727e
yyp0 <shuhuiyao@bytedance.com>
2024-10-31 10:25:37 +0800

    update xfail

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -564,6 +564,7 @@ FX_IMPORTER_STABLEHLO_XFAIL_SET = {
     "ElementwiseRemainderTensorModule_Int_Float_NegativeDivisor_basic",
     "ElementwiseRemainderTensorModule_Int_NegativeDividend_basic",
     "ElementwiseRemainderTensorModule_Int_NegativeDivisor_basic",
+    "Exp2StaticModule_basic",
     "MaxPool1dCeilModeTrueModule_basic",
     "MaxPool1dStaticCeilModeTrueModule_basic",
     "MaxUnpool3dModulePad0_basic",
