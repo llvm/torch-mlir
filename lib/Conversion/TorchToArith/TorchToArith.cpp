44985690a74123dead46b68afaf3068bfb105f9e
Giacomo Serafini <179146510+giacs-epic@users.noreply.github.com>
2024-11-27 17:23:35 +0100

    [Torch Dialect] Emit `torch.aten.mul.float_int`, add folder and conversion to Arith. (#3750)
    Folder is required to simplify the shape calculation of
    `torch.aten.__interpolate.size_list_scale_list`:
    
    https://github.com/llvm/torch-mlir/blob/5eab669c4ab0c3aab3dab5b95d0172ab0a8395b8/lib/Dialect/Torch/Transforms/AbstractInterpLibrary.cpp#L6900-L6907
    
    (I've re-run `build_tools/update_abstract_interp_lib.sh`)
    
    ---------
    
    Co-authored-by: zjgarvey <47986913+zjgarvey@users.noreply.github.com>

--- lib/Conversion/TorchToArith/TorchToArith.cpp
+++ lib/Conversion/TorchToArith/TorchToArith.cpp
@@ -74,7 +74,8 @@ public:
                   ConversionPatternRewriter &rewriter) const override {
     Value a = adaptor.getA();
     Value b = adaptor.getB();
-    if (llvm::is_one_of<AtenOp, AtenAddFloatIntOp>::value)
+    if (llvm::is_one_of<AtenOp, AtenAddFloatIntOp>::value ||
+        llvm::is_one_of<AtenOp, AtenMulFloatIntOp>::value)
       b = convertScalarToDtype(rewriter, op.getLoc(), b, a.getType());
     if (llvm::is_one_of<AtenOp, AtenMulIntFloatOp>::value)
       a = convertScalarToDtype(rewriter, op.getLoc(), a, b.getType());
@@ -492,7 +493,8 @@ public:
     target.addIllegalOp<AtenNegIntOp>();
     patterns.add<ConvertAtenNegIntOp>(typeConverter, context);
     target.addIllegalOp<AtenAddIntOp, AtenAddFloatIntOp, AtenSubIntOp,
-                        AtenMulIntOp, AtenRemainderIntOp, AtenMulIntFloatOp>();
+                        AtenMulIntOp, AtenRemainderIntOp, AtenMulIntFloatOp,
+                        AtenMulFloatIntOp>();
     patterns.add<ConvertAtenBinaryOp<AtenAddIntOp, arith::AddIOp>>(
         typeConverter, context);
     patterns.add<ConvertAtenBinaryOp<AtenRemainderIntOp, arith::RemSIOp>>(
@@ -504,6 +506,8 @@ public:
     patterns.add<ConvertAtenBinaryOp<AtenMulIntOp, arith::MulIOp>>(
         typeConverter, context);
     patterns.add<ConvertAtenBinaryOp<AtenMulIntFloatOp, arith::MulFOp>>(
+        typeConverter, context);
+    patterns.add<ConvertAtenBinaryOp<AtenMulFloatIntOp, arith::MulFOp>>(
         typeConverter, context);
     target.addIllegalOp<AtenSubFloatOp, AtenMulFloatOp>();
     patterns.add<ConvertAtenBinaryOp<AtenSubFloatOp, arith::SubFOp>>(
