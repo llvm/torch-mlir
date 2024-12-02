a941f73547b7c6222c14c58a98ca310ba69172c4
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2024-12-02 12:10:23 +0000

    Update xfail_set

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -514,9 +514,6 @@ FX_IMPORTER_XFAIL_SET = {
     "ElementwiseCopysignModule_basic",
     "BernoulliFloatModule_basic",
     "BernoulliTensorModule_basic",
-    "MobilenetV3Module_basic",
-    "ResNet18Module_basic",
-    "ResNet18StaticModule_basic",
     "UniformModule_basic",
     "UniformStaticShapeModule_basic",
 }
@@ -848,6 +845,8 @@ FX_IMPORTER_STABLEHLO_XFAIL_SET = {
     "ScatterValueFloatModule_basic",
     "ScatterValueIntModule_basic",
     "SliceOutOfLowerBoundEndIndexModule_basic",
+    "SortIntListReverse_basic",
+    "SortIntList_basic",
     "SortTensorDescending_basic",
     "SortTensorInteger_basic",
     "SortTensorNegativeDimension_basic",
