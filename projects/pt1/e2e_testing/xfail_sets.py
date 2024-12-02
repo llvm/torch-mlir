36fe2e2db5c61d9c8b31d47601110ece1ee4b3c5
Sayan Saha <sayans@mathworks.com>
2024-12-02 07:58:27 -0500

    Update xfail_sets.py

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -3011,7 +3011,6 @@ ONNX_XFAIL_SET = {
     "MaxPool1dCeilModeTrueModule_basic",
     "MaxPool1dModule_basic",
     "MaxPool2dCeilModeTrueModule_basic",
-    "MaxPool2dStaticCeilModeTrueReduceOutputModule_basic",
     "MaxPool2dModule_basic",
     "MaxPool2dWithIndicesAllOnesModule_basic",
     "MaxPool2dWithIndicesBackwardDynamic3DModule_basic",
@@ -3381,6 +3380,13 @@ if torch_version_for_comparison() >= version.parse("2.5.0.dev"):
     ONNX_XFAIL_SET = ONNX_XFAIL_SET | {
         # ERROR: value (Tensor with shape=[2, 3, 8, 20], dtype=torch.float32, min=+nan, max=+nan, mean=+nan) is not close to golden value (Tensor with shape=[2, 3, 8, 20], dtype=torch.float32, min=-2.394, max=+2.454, mean=-0.02828)
         "ScaledDotProductAttentionBoolMaskModule_basic",
+    }
+
+if torch_version_for_comparison() > version.parse("2.5.1"):
+    ONNX_XFAIL_SET = ONNX_XFAIL_SET | {
+        # error: 'memref.cast' op operand type 'memref<2x6x4x3xf32>' and result type 'memref<2x6x5x3xf32>' are cast incompatible
+        # torch.onnx.export produces onnx.MaxPool op with incorrect output shape of 2x6x5x3 instead of 2x6x4x3
+        "MaxPool2dStaticCeilModeTrueReduceOutputModule_basic",
     }
 
 if torch_version_for_comparison() < version.parse("2.4.0.dev"):
@@ -4495,7 +4501,6 @@ ONNX_TOSA_XFAIL_SET = {
     "MaxPool1dCeilModeTrueModule_basic",
     "MaxPool1dModule_basic",
     "MaxPool2dCeilModeTrueModule_basic",
-    "MaxPool2dStaticCeilModeTrueReduceOutputModule_basic",
     "MaxPool2dModule_basic",
     "MaxPool2dWithIndicesAllNegativeValuesModule_basic",
     "MaxPool2dWithIndicesAllOnesModule_basic",
@@ -4936,3 +4941,10 @@ ONNX_TOSA_XFAIL_SET = {
     "_LogSoftmaxModule_basic",
     "_SoftmaxModule_basic",
 }
+
+if torch_version_for_comparison() > version.parse("2.5.1"):
+    ONNX_TOSA_XFAIL_SET = ONNX_TOSA_XFAIL_SET | {
+        # error: 'memref.cast' op operand type 'memref<2x6x4x3xf32>' and result type 'memref<2x6x5x3xf32>' are cast incompatible
+        # torch.onnx.export produces onnx.MaxPool op with incorrect output shape of 2x6x5x3 instead of 2x6x4x3
+        "MaxPool2dStaticCeilModeTrueReduceOutputModule_basic",
+    }
