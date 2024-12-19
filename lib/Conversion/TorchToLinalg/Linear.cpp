061bbc5e1bc4f7880bb565e404a6709f97396818
Rob Suderman <rob.suderman@gmail.com>
2024-12-19 10:55:15 -0800

    [torch] Update `torch.bmm` to use accumulator type (#3924)
    Batch matmul was using the result type as the accumulator. Updated to
    use the preferred accumulator based on input type.

--- lib/Conversion/TorchToLinalg/Linear.cpp
+++ lib/Conversion/TorchToLinalg/Linear.cpp
@@ -727,15 +727,21 @@ public:
     // Check the matrixs shapes are valid for mulplication.
     checkDimEqualHelper(rewriter, loc, lhsDim2, rhsDim1);
 
+    Type accumulatorDType = getDefaultAccType(rewriter, resultElementType);
     Value initTensor0 = createZeroInitTensor(
-        rewriter, loc, ValueRange{lhsDim0, lhsDim1, rhsDim2},
-        resultElementType);
+        rewriter, loc, ValueRange{lhsDim0, lhsDim1, rhsDim2}, accumulatorDType);
 
     Value bmm =
         rewriter
             .create<linalg::BatchMatmulOp>(loc, initTensor0.getType(),
                                            ValueRange{lhs, rhs}, initTensor0)
             .getResult(0);
+
+    if (accumulatorDType != resultElementType) {
+      bmm = torch_to_linalg::convertTensorToElementType(rewriter, loc, bmm,
+                                                        resultElementType);
+    }
+
     rewriter.replaceOpWithNewOp<tensor::CastOp>(op, newResultType, bmm);
     return success();
   }
