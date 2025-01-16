523cd45fb034649851ead69ca208c6c71e3a32a1
Giacomo Serafini <179146510+giacs-epic@users.noreply.github.com>
2025-01-16 15:39:20 +0000

    Remove most onnx calls in import-onnx-main

--- projects/onnx_c_importer/import-onnx-main.cpp
+++ projects/onnx_c_importer/import-onnx-main.cpp
@@ -12,10 +12,7 @@
 // they can be included in foreign projects more easily.
 
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/InitLLVM.h"
-#include "llvm/Support/Path.h"
-#include "llvm/Support/raw_ostream.h"
 
 #include "OnnxImporter.h"
 
@@ -53,23 +50,6 @@ static cl::opt<bool> clearDomainArg(
              " in the onnx graph before performing shape inference."),
     cl::init(false));
 
-static cl::opt<bool> keepTempsArg("keep-temps",
-                                  cl::desc("Keep intermediate files"),
-                                  cl::init(false));
-
-static cl::opt<std::string> tempDirArg(
-    "temp-dir",
-    cl::desc(
-        "Pre-existing directory in which to create temporary files."
-        " For example, to place temporaries under the directory \"foo/bar\""
-        " specify --temp-dir=foo/bar.  \"foo/bar\" must already exist."
-        " Defaults to the directory of the input file."),
-    cl::value_desc("directory"), cl::init("-"));
-
-// NOTE: -data-dir
-//       unfortunately it's not possible to specify a different base directory
-//       for external data due to limitations of the onnx::checker API
-
 static cl::opt<int> opsetVersionArg(
     "opset-version",
     cl::desc(
@@ -83,51 +63,11 @@ static cl::opt<bool> disableFunctionExpansionAllowlistArg(
              " allowing non-allowlisted functions to be expanded."),
     cl::init(false));
 
-// NOTE: this function may actually fail without returning failure: since
-// exceptions are disabled, onnx utilities will just print errors to stderr and
-// keep executing. It's unclear what the returned ModelProto will contain.
+//  NOTE: this function may actually fail without returning failure: since
+//  exceptions are disabled, onnx utilities will just print errors to stderr and
+//  keep executing. It's unclear what the returned ModelProto will contain.
+//  TODO: Rely on python's load_onnx_model() and expose OnnxImporter via pybind.
 FailureOr<onnx::ModelProto> loadOnnxModel() {
-  // Do shape inference two ways.  First, attempt in-memory to avoid redundant
-  // loading and the need for writing a temporary file somewhere.  If that
-  // fails, typically because of the 2 GB protobuf size limit, try again via
-  // files.  See
-  // https://github.com/onnx/onnx/blob/main/docs/PythonAPIOverview.md#shape-inference-a-large-onnx-model-2gb
-  // for details about the file-based technique.
-
-  // Make a temp dir for all the temp files we'll be generating as a side
-  // effect of infering shapes.  For now, the only file is a new .onnx holding
-  // the revised model with shapes.
-  //
-  // TODO: If the program temp_dir is None, we should be using an ephemeral
-  // temp directory instead of a hard-coded path in order to avoid data races
-  // by default.
-  SmallString<512> inputFile;
-  inputFile = inputFilenameArg.getValue();
-  if (!sys::fs::make_absolute(inputFile)) {
-    errs() << "Invalid input file path: " << inputFilenameArg << "\n";
-    return llvm::failure();
-  }
-  if (sys::fs::is_directory(inputFile)) {
-    errs() << "Input file path is a directory: " << inputFilenameArg << "\n";
-    return llvm::failure();
-  }
-  llvm::StringRef inputDir = sys::path::parent_path(inputFile);
-
-  SmallString<512> tempDir;
-  if (tempDirArg == "-") {
-    tempDir = inputDir;
-  } else {
-    tempDir = tempDirArg;
-  }
-  sys::path::append(tempDir, "onnx-importer-temp");
-  if (!sys::fs::remove_directories(tempDir, /*IgnoreErrors=*/true)) {
-    errs() << "Couldn't clean temp directory: " << tempDir << "\n";
-    return llvm::failure();
-  }
-  if (!sys::fs::create_directory(tempDir)) {
-    errs() << "Couldn't create temp directory: " << tempDir << "\n";
-    return llvm::failure();
-  }
 
   onnx::ModelProto mp;
   {
@@ -161,41 +101,8 @@ FailureOr<onnx::ModelProto> loadOnnxModel() {
     onnx::shape_inference::InferShapes(mp, onnx::OpSchemaRegistry::Instance(),
                                        opts);
   } else {
-    // Model is too big for in-memory inference: do file-based shape inference
-    // to a temp file.
-
-    SmallString<512> tempInferredFile = tempDir;
-    sys::path::append(tempInferredFile, "inferred.onnx");
-    std::string tempInferredFileStr = std::string(tempInferredFile);
-    // First save intermediate to file
-    {
-      std::fstream output(tempInferredFileStr,
-                          std::ios::out | std::ios::trunc | std::ios::binary);
-      std::string model_string;
-      mp.SerializeToString(&model_string);
-      output << model_string;
-    }
-
-    onnx::shape_inference::InferShapes(tempInferredFileStr, tempInferredFileStr,
-                                       onnx::OpSchemaRegistry::Instance(),
-                                       opts);
-
-    {
-      std::ifstream inputStream(tempInferredFileStr,
-                                std::ios::in | std::ios::binary);
-      mp.Clear();
-      if (!mp.ParseFromIstream(&inputStream)) {
-        errs() << "Failed to parse ONNX ModelProto \n";
-        return llvm::failure();
-      }
-    }
-  }
-
-  if (!keepTempsArg) {
-    if (!sys::fs::remove_directories(tempDir, /*IgnoreErrors=*/true)) {
-      errs() << "Couldn't clean temp directory: " << tempDir << "\n";
-      return llvm::failure();
-    }
+    errs() << "ModelProto too big. Missing feature.\n";
+    return llvm::failure();
   }
 
   return mp;
@@ -226,7 +133,14 @@ int main(int argc, char **argv) {
     outputStream = allocatedOutputStream.get();
   }
 
-  Status status = OnnxImporter::Import(std::move(model).value(), outputStream);
+  Config config;
+  config.no_verify = noVerifyArg;
+  if (disableFunctionExpansionAllowlistArg) {
+    config.function_expansion_allowlists_by_domain = std::nullopt;
+  }
+
+  Status status =
+      OnnxImporter::Import(std::move(model).value(), outputStream, config);
   if (failed(status))
     return 1;
 
