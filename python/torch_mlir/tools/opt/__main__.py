4be5b00e09975d4be26f2331c3ea29989bc4c2f2
Marius Brehler <marius.brehler@amd.com>
2025-01-17 13:30:00 +0000

    Include `torch-mlir-opt` in Python wheels
    This adds the `torch-mlir-opt` tool to the Python wheels, which allows
    to use the commandline tool via a pip installed package instead of
    having to compile the torch-mlir project yourself.
    The executable is still installed to the deault location and copied over
    via the `setup.py` to be included in the Python wheel. This could be
    refactored and handled within CMake in a follow-up.

---
+++ python/torch_mlir/tools/opt/__main__.py
@@ -0,0 +1,30 @@
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Also available under a BSD-style license. See LICENSE.
+
+import os
+import platform
+import subprocess
+import sys
+
+from typing import Optional
+
+
+def _get_builtin_tool(exe_name: str) -> Optional[str]:
+    if platform.system() == "Windows":
+        exe_name = exe_name + ".exe"
+    this_path = os.path.dirname(__file__)
+    tool_path = os.path.join(this_path, "..", "..", "_mlir_libs", exe_name)
+    return tool_path
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    exe = _get_builtin_tool("torch-mlir-opt")
+    return subprocess.call(args=[exe] + args)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
