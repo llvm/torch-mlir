b6672005d9d47464afca3817a305ad1ac9903c6e
Sayan Saha <sayans@mathworks.com>
2025-07-09 16:57:54 -0400

    Add canonicalizer in torch_ods_gen.py + update error message

--- lib/Dialect/Torch/IR/TorchOps.cpp
+++ lib/Dialect/Torch/IR/TorchOps.cpp
@@ -5827,8 +5827,8 @@ struct CanonicalizeAvgPoolWithSingleIntTuple
     // Attempt to expand params if necessary.
     if (failed(expandPoolParams(op, numSpatialDims, rewriter, kernel, stride,
                                 pad, dilations)))
-      return rewriter.notifyMatchFailure(op,
-                                         "Failed to expand params for pooling");
+      return rewriter.notifyMatchFailure(
+          op, "Failed to expand params for AvgPooling");
 
     rewriter.replaceOpWithNewOp<AvgPoolOpT>(
         op, op.getResult().getType(), op.getSelf(), kernel, stride, pad,
@@ -5855,8 +5855,8 @@ struct CanonicalizeMaxPoolWithSingleIntTuple
     // Attempt to expand params if necessary.
     if (failed(expandPoolParams(op, numSpatialDims, rewriter, kernel, stride,
                                 pad, dilations)))
-      return rewriter.notifyMatchFailure(op,
-                                         "Failed to expand params for pooling");
+      return rewriter.notifyMatchFailure(
+          op, "Failed to expand params for MaxPooling");
 
     rewriter.replaceOpWithNewOp<MaxPoolOpT>(op, op.getResult().getType(),
                                             op.getSelf(), kernel, stride, pad,
