0501684858a3535a3b2d14e869879f39ce9ff02f
Vivek Khandelwal <vivekkhandelwal1424@gmail.com>
2025-07-15 10:02:35 +0530

    Update Tosa xfail set

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -3977,6 +3977,17 @@ FX_IMPORTER_TOSA_XFAIL_SET = {
     "ReplicationPad3dModuleSingleIntPad_basic",
     "AtenAsStridedModule_basic",
     "AtenAsStridedNoStorageOffsetModule_basic",
+    "ChunkListUnpackDynamic_Module_basic",
+    "ChunkListUnpackUnevenDynamic_Module_basic",
+    "ChunkListUnpackUneven_Module_basic",
+    "ChunkListUnpack_Module_basic",
+    "NativeGroupNormModule_basic",
+    "SplitTensorGetItem_Module_basic",
+    "SplitTensorLastSmallerModule_basic",
+    "SplitTensorListUnpackModule_basic",
+    "SplitTensorNegativeDimModule_basic",
+    "SplitWithSizesListUnpackModule_basic",
+    "SplitWithSizes_Module_basic",
 }
 
 ONNX_TOSA_CRASHING_SET = {
